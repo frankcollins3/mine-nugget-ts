d52cc88d1d1f0c85afc1ae81b6c456c56f19caf2
Frank Collins <fwc3rd@gmail.com>
2022-11-15 18:51:49 -0500

    add 2 joined classes stored in variable [5:55pm]

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+// import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
+
 import Axios from 'axios'
 import APIcall from '../utility/APIcall'
 import Random from '../utility/Randomizer'
+
 
 import React,  { useEffect, useState} from 'react'
 
@@ -18,8 +21,12 @@ export default function Home() {
       let specify = await APIcall('specify', 'wedding cake', setCurrentStrain)
       let randomstrain = await APIcall('random', null, null)      
     }
+  const classList = [styles.Container, styles.Column].join(" ")
+
   return (
-    <div className={styles.container}>
+    <div className={classList}>
+    {/* // <div className={styles[styles.Container, styles.Column-Center]}> */}
+    {/* <div className={styles.container styles.Column-Center} {styles.container, styles.Column-Center}> */}
     <main className={styles.main}>
         <h1>         
           {currentStrain || 'hey'}
