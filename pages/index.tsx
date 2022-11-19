61c0b048a61e05a8f91bc2547a826b21cd0ffdc9
Frank Collins <fwc3rd@gmail.com>
2022-11-18 20:37:56 -0500

    [index] prisma, getStaticProps() [8:37pm]
    db connected 
    
    used #psql to INSERT INTO (psql)
    
    have to restart [ npm build | next run dev ]
    console.log() goes to terminal

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,43 @@ import Head from 'next/head'
 import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 import styles from '../styles/Home.module.scss'
-
 import Axios from 'axios'
 import APIcall from '../utility/APIcall'
 import Random from '../utility/Randomizer'
-
-
+import Client from '../utility/Prisma'
+// import QueryDB from '../index'
 import React,  { useEffect, useState} from 'react'
+import { PrismaClient } from '@prisma/client'
+import { collapseTextChangeRangesAcrossMultipleVersions, createNoSubstitutionTemplateLiteral } from 'typescript'
 
 
-export default function Home() {
-  
+
+export async function getStaticProps(context:any) {
+let allstrains:any = await APIcall('all', null, null)
+console.log('allstrains')
+console.log(allstrains)
+console.log("oh jeez");
+  return {
+    props: { allstrains }, // will be passed to the page component as props
+  }
+}
+
+
+
+
+export default function Home(allstrains:any) {
   const [pokemon, setPokemon ] = useState('')
   const [currentStrain, setCurrentStrain] = useState('')
+  const [savedStrains, setSavedStrains] = useState('')
 
-  const checkAPI = async () => {    
-      let predata: any[] = await [APIcall('all', null, setCurrentStrain)]
-      let specify = await APIcall('specify', 'wedding cake', setCurrentStrain)
-      let randomstrain = await APIcall('random', null, null)      
+  const checkAPI = async () => {  
+    let predata: any[] = await [APIcall('all', null, setCurrentStrain)]
+    
+
+
+    // let specify = await APIcall('specify', 'wedding cake', setCurrentStrain)
+    let randomstrain = await APIcall('random', null, setCurrentStrain)      
+    
     }
   const classList = [styles.Container, styles.Column].join(" ")
 
@@ -31,9 +50,13 @@ export default function Home() {
         <h1>         
           {currentStrain || 'hey'}
         </h1>
+        
+
         <button onClick={checkAPI} type="button"> </button>
     </main>
 
     </div>
   )
 }
+
+
