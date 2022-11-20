8d995fd3ee61d0daa9f8b07931d5a35625132157
Frank Collins <fwc3rd@gmail.com>
2022-11-19 23:29:00 -0500

    [allStrains] API link prisma browser err gone [11:26pm]
    $.ajax({
          method: 'post',
          url: '/api/allStrain',
          data: {
           key: 'all'
          }
        }).then( (msg) => {
          console.log('msg we are in the .then() statement')
          console.log(msg)
        })
    
    ^ ^ this code above is client side [index.tsx] This example should make full stack easy to understand:
    
    client side there is a:
    .then( (msg) => console.log(msg) that is the asynchronous return of $.ajax( method: 'post' )=>
    
    at first the request is handled server side 
    you cannot see any of those logs in the google chrome dev tools inspect.
    
    likewise...
    
    server-side:
    if there is no 
    res.json( { success: 'success-message' } ) 
    
    if there is no response... there is no code coming back client side since there is no response from server side
    client

--- pages/api/allStrain.ts
+++ pages/api/allStrain.ts
@@ -4,19 +4,35 @@ import APIcall from '../../utility/APIcall'
 const prisma = new PrismaClient();
 
 export default async function (req:any, res:any) {
-  if (req.method === 'POST') {
+    let allstrainspost:any = await APIcall('all', null, null)
+  if (req.method === 'POST') {      
+      let status:string = req.status
+      console.log('status')
+      console.log(status)
+
       const { body } = req;
       console.log('body')
       console.log(body)
-      if (req.body.key === 'all') {
           console.log("we are in the post method and req.body.key === all")
-        let allstrainspost:any = await APIcall('all', null, null)
         console.log('allstrainspost')
         console.log(allstrainspost)
-    }
         
-    // const movie = await prisma.strains.create({ data: JSON.parse(body) });
-    // res.json(movie);
+    // const strains = await prisma.strains.create({ data: JSON.parse(body) });
+    // res.json(strains);
   }
-  
+  try {
+      res.json( { successObject: allstrainspost})
+  }
+  catch (err) {
+    console.log(err)
+  }
+//   console.log('res.status')
+//   console.log(res.status)
+//   console.log(res.status)
+//   if (res.status(403)) {
+//     console.log('we have failure')
+//     // can also redirect to an error page.
+//   } else {
+//     res.status(200).json( { successobject: allstrainspost})
+//   }
 }
