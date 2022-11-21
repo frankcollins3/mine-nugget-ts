b5f7a2baa331c317a5c4aad246fbf95e54f81247
Frank Collins <fwc3rd@gmail.com>
2022-11-21 05:47:24 -0500

    successfully return data from db server to client [5:47am]
    successfully return all data from the database and prevent duplicate creations if the functions ran again
    
    the response is on line 77:     res.json( { successObject: dbstrainlist, length: dbstrainlist.length})
    
    this code below is generated client side on the .then( msg) => console.log(msg) in the .then() block of the ajax call that accesses the POST request 
    {successObject: Array(8), length: 8}
    8
    successObject
    Array(8)
    0
    {id: 23, strainId: 1, strain: 'wedding cake', dominant: 'indica', funfact: 'leafly strain of year 2019', …}
    1
    {id: 37, strainId: 7, strain: 'pineapple express', dominant: 'sativa', funfact: 'name comes from meteorology: hawaiian water vapor, shares name with a movie', …}
    2
    {id: 39, strainId: 1, strain: 'wedding cake', dominant: 'indica', funfact: 'leafly strain of year 2019', …}
    3
    {id: 40, strainId: 4, strain: 'mimosa', dominant: 'sativa-hybrid', funfact: 'nutrient greedy: needs encouragement to grow to full potential', …}
    4
    {id: 41, strainId: 2, strain: 'GorillaGlue#4', dominant: 'sativa', funfact: 'sticks to fingers and scissors, #1 cann cup 2014', …}
    5
    {id: 42, strainId: 6, strain: 'white widow', dominant: 'balanced hybrid', funfact: 'passed genes to white rhino & white russian, growers prefer original', …}
    6
    {id: 38, strainId: 3, strain: 'Do-Si-Dos', dominant: 'indica-hybrid', funfact: 'gender-fluid, warm or humid climate, in or outdoors', …}
    7
    {id: 43, strainId: 5, strain: 'cherry pie', dominant: 'indica hybrid', funfact: 'effects in minutes, lasts for hours, pungent: use discretion', …}
    Array(0)
    Object

--- pages/api/allStrain.ts
+++ pages/api/allStrain.ts
@@ -1,25 +1,80 @@
 import { PrismaClient } from '@prisma/client';
 import APIcall from '../../utility/APIcall'
+import Random from '../../utility/Randomizer'
+
+
 
 const prisma = new PrismaClient();
 
 export default async function (req:any, res:any) {
+  
     const { body } = req;      
+    // console.log(body)
     let allstrainspost:any = await APIcall(body.key, null, null)
+    let dbstrainlist = await prisma.strains.findMany()
+    let strains = prisma.strains
+
+    
+    const createStrains = async () => {
+      console.log("we are here");
+          if (dbstrainlist.length < 3) {
+            console.log("we are ALSO HERE!!");                
+            
+          allstrainspost.map( (mapitem:any, index:number) => {
+            let strainId:number = index + 1
+            let strain:string = mapitem.strain
+            let dominant:string = mapitem.dominant
+            let parents:string = mapitem.parents
+            let funfact:string = mapitem.funfact
+            
+            console.log('strain')
+            console.log(strain)
+            
+            strains.create({
+              data: {
+                strainId: strainId,
+                strain: strain,
+                dominant: dominant,
+                parents: parents,
+                funfact: funfact
+              }
+            }).then( (createdstrains:any) => {
+              console.log('createdstrains')
+              console.log(createdstrains)
+            })              
+        })
+    }
+    }
+      createStrains()
+
+  
+    // const updateId = async () => {      
+    // }
+
+    const checkdb = () => {
+      console.log('were in the checkdb')
+      console.log(dbstrainlist)
+    }
+    checkdb()
+
+
+
   if (req.method === 'POST') {      
       let status:string = req.status
       console.log('status')
       console.log(status)
+            
+      let dblength = dbstrainlist.length
+      if (dbstrainlist.length) {
 
-          console.log("we are in the post method and req.body.key === all")
-        console.log('allstrainspost')
-        console.log(allstrainspost)
-        
+      }
+      
+    
     // const movie = await prisma.strains.create({ data: JSON.parse(body) });
     // res.json(movie);
   }
   try {
-      res.json( { successObject: allstrainspost})
+      res.json( { successObject: dbstrainlist, length: dbstrainlist.length})
   }
   catch (err) {
     console.log(err)
