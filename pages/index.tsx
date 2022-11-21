59c69936ea0da63434e8e9d560c7f3f127af8449
Frank Collins <fwc3rd@gmail.com>
2022-11-21 04:11:56 -0500

    [index] change from db to API [4:11am]
    access API instead of db 
    specify types in map for (mapitem, index)

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,12 +10,20 @@ export default async function (req:any, res:any) {
     let allstrainspost:any = await APIcall(body.key, null, null)
     let dbstrainlist = await prisma.strains.findMany()
     let strains = prisma.strains
-   
-    dbstrainlist.forEach( (strainitem) => {
-        console.log('strainitem')  
-        console.log(strainitem)  
 
+    
+      allstrainspost.map( (mapitem:any, index:number) => {
+      let strainId = mapitem.strainId
+      let strain = mapitem.strain
+      let dominant = mapitem.dominant
+      let parents = mapitem.parents
+      let funfact = mapitem.funfact
+            
+      
     })
+        // id, strain, strainId, dominant, funfact, parents
+        //  id | strainId | strain |   dominant    |                            funfact                             |         parents
+    
 
   if (req.method === 'POST') {      
       let status:string = req.status
@@ -37,7 +45,9 @@ export default async function (req:any, res:any) {
   catch (err) {
     console.log(err)
   }
-
+//   console.log('res.status')
+//   console.log(res.status)
+//   console.log(res.status)
 //   if (res.status(403)) {
 //     console.log('we have failure')
 //     // can also redirect to an error page.
