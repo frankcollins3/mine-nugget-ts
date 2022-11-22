302e5701124c156173d34565282bb0273e264d16
Frank Collins <fwc3rd@gmail.com>
2022-11-21 20:47:08 -0500

    [strain] drilling props for now [8:46pm]

--- pages/strain.tsx
+++ pages/strain.tsx
@@ -1,19 +1,29 @@
 // import modulecss from '../styles/Strain.module.scss'
 import styles from '../styles/Strain.module.scss'
-// import '../styles/globals.css'
-// import globals from '../styles/globals.css'
+import AllStrainContainer from '../components/AllStrainContainer'
+
+import React, { useEffect, useState } from 'react'
+// import getAllStrain from './api/getAllStrain'
 
 
-export default function Strain (){
-    const classList = [styles.Page, 'Column'].join(" ")
-    const textClasses = [styles.FontSizeTest, styles.BorderTest].join(" ");
+
+export default function Strain (props:any) {
+
+    console.log("right now we are in the strain function");
+    const classList:string = [styles.Page, 'Column'].join(" ")
+    const textClasses:string = [styles.FontSizeTest, styles.BorderTest].join(" ");
     // const classList = [modulecss.Parent-Cont, modulecss.Test-Border].join(" ")
 
 
     return (
     // let doubleClass = [modulecss.Parent-Cont, ]
         <div className={classList}>
-            <h1> yeah buddy </h1>
+            <AllStrainContainer
+                currentStrain={props.currentStrain} setCurrentStrain={props.setCurrentStrain}
+                url={props.url} setUrl={props.setUrl}
+                allStrains={props.allStrains} setAllStrains={props.setAllStrains}
+             />
+
             <p className={textClasses}> quick look </p>
         </div>
     )
