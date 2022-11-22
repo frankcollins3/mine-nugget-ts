97da1db2c9099468ade68132d023bc0637ad9824
Frank Collins <fwc3rd@gmail.com>
2022-11-21 19:46:52 -0500

    [app] using createContext in root level of app[7:44pm]
    instead of learning by using it as an exported expression
    
    now to test if there's a reference of the value={theme}
    if there is reference of the value within the <ThemeContext / createContext/> component
    
    a conditional can be created around that value and state can be enmeshed for toggling behavior.
    
    this app is currently calling for: 
    toggle state and context to change the main container contents

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,46 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from "next/head";
+import React, { useState, useContext, createContext } from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
+// import { themes, ThemeContext} from '.././utility/Context'
+// import globalContext from   '.././utility/Context'
+
+let themes = {
+// export const themes = {
+  light: {
+    foreground: '#000000',
+    background: '#eeeeee',
+  },
+  dark: {
+    foreground: '#ffffff',
+    background: '#222222',
+    border: '10px solid orange'
+  },
+};
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  
+  const [theme, setTheme] = useState(themes.dark)
+  const ThemeContext = createContext(theme)
+  // globalContext()
+  // const appContext = createContext({
+  //   default
+  // })
+  const contextstate = () => {
+    
+  }
+
   return (
     <>
       <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-    <Component {...pageProps} 
-   />
+
+    <ThemeContext.Provider value={theme}>
+    <Component {...pageProps} />
+   </ThemeContext.Provider>
    </>
     
   )
