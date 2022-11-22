90551d32bdbc7f1ded219b0c9d5ff08ac1a69e08
Frank Collins <fwc3rd@gmail.com>
2022-11-21 20:22:36 -0500

    good old fashioned passing-props [8:21pm]
    was looking into redux or context API
    still would like to iterate context api before this app is completed.
    
    i wasn't sure if this would work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,31 +6,19 @@ import 'bootstrap/dist/css/bootstrap.css'
 // import { themes, ThemeContext} from '.././utility/Context'
 // import globalContext from   '.././utility/Context'
 
-let themes = {
-// export const themes = {
-  light: {
-    foreground: '#000000',
-    background: '#eeeeee',
-  },
-  dark: {
-    foreground: '#ffffff',
-    background: '#222222',
-    border: '10px solid orange'
-  },
-};
-
 
 export default function App({ Component, pageProps }: AppProps) {
   
-  const [theme, setTheme] = useState(themes.dark)
-  const ThemeContext = createContext(theme)
+  // const [theme, setTheme] = useState(themes.dark)      this is for context in the example.
+  const [url, setUrl] = useState('')    // url:string
+  const [ currentStrain, setCurrentStrain ] = useState('')
+
+  // const ThemeContext = createContext(theme)
   // globalContext()
   // const appContext = createContext({
   //   default
   // })
-  const contextstate = () => {
-    
-  }
+  
 
   return (
     <>
@@ -38,9 +26,13 @@ export default function App({ Component, pageProps }: AppProps) {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-    <ThemeContext.Provider value={theme}>
-    <Component {...pageProps} />
-   </ThemeContext.Provider>
+    {/* <ThemeContext.Provider value={theme}> */}
+
+    <Component {...pageProps}
+     currentStrain={currentStrain} setCurrentStrain={setCurrentStrain}
+     url={url} setUrl={setUrl}
+     />
+   {/* </ThemeContext.Provider> */}
    </>
     
   )
