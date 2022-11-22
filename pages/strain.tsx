56c57adea77931eeea514d5e64da59200e439544
Frank Collins <fwc3rd@gmail.com>
2022-11-21 22:09:52 -0500

    webpage rdy: render UI based on state[10:09pm]
    couldn't get that IntrinsicTypes error to clear. got it.

--- pages/strain.tsx
+++ pages/strain.tsx
@@ -8,8 +8,11 @@ import React, { useEffect, useState } from 'react'
 
 
 export default function Strain (props:any) {
+    console.log('props')
+    console.log(props)
 
-    console.log("right now we are in the strain function");
+
+    
     const classList:string = [styles.Page, 'Column'].join(" ")
     const textClasses:string = [styles.FontSizeTest, styles.BorderTest].join(" ");
     // const classList = [modulecss.Parent-Cont, modulecss.Test-Border].join(" ")
@@ -18,10 +21,11 @@ export default function Strain (props:any) {
     return (
     // let doubleClass = [modulecss.Parent-Cont, ]
         <div className={classList}>
-            <AllStrainContainer
-                currentStrain={props.currentStrain} setCurrentStrain={props.setCurrentStrain}
+            <AllStrainContainer 
+                // dummyprop={'dummyprop'}
                 url={props.url} setUrl={props.setUrl}
                 allStrains={props.allStrains} setAllStrains={props.setAllStrains}
+                currentStrain={props.currentStrain} setCurrentStrain={props.setCurrentStrain}            
              />
 
             <p className={textClasses}> quick look </p>
