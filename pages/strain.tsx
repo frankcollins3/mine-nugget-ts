048cfca25aa7a14cc08a67437b69e67ed209d495
Frank Collins <fwc3rd@gmail.com>
2022-11-23 05:20:46 -0500

    [strain] establish relative path to window [5:20am]
    getStaticProps() returning a:
    
              {err: 'needs absolute path'  } error
    this is to access prisma-utilizing-getStrainsFromDb-route: 
    pages/api/getAllStrain.ts

--- pages/strain.tsx
+++ pages/strain.tsx
@@ -1,17 +1,55 @@
+import $ from 'jquery'
 // import modulecss from '../styles/Strain.module.scss'
+
+// import { $ }  from 'react-jquery-plugin'
+
 import styles from '../styles/Strain.module.scss'
+import getAllStrain from './api/getAllStrain'
+
 import AllStrainContainer from '../components/AllStrainContainer'
 
 import React, { useEffect, useState } from 'react'
 // import getAllStrain from './api/getAllStrain'
 
+export async function getStaticProps(context:any) {
+    console.log('server side! in the strain!')
+    let currentdirectory = process.cwd()
+    console.log('currentdirectory')
+    console.log(currentdirectory)
+    
+    // let getdbALL = await fetch('/pages/api/allStrain.ts')
+    // console.log('getdbALL')
+    // console.log(getdbALL)
+
+    // const protocol = req.headers['x-forwarded-proto'] || 'http'
+    // const baseUrl = req ? `${protocol}://${req.headers.host}` : ''
+
+    // let strains = await fetch(`/Users/medium/Desktop/mine-nugget-ts/pages/api/getAllStrain.ts`)
+
+    // console.log('strains')
+    // console.log(strains)
+
+    // $.ajax({
+    //     method: 'get',
+    //     url: '/api/getAllStrains',
+    //     data: 'json'    
+    // }).then( (data) => {
+    //     console.log('data')
+    //     console.log(data)
+    // })
+    return {
+        props: {
+            
+        }
+    }
 
 
-export default function Strain (props:any) {
-    console.log('props')
-    console.log(props)
+}
 
-
+export default function Strain (props:any,) {
+    
+    
+    
     
     const classList:string = [styles.Page, 'Column'].join(" ")
     const textClasses:string = [styles.FontSizeTest, styles.BorderTest].join(" ");
@@ -21,8 +59,7 @@ export default function Strain (props:any) {
     return (
     // let doubleClass = [modulecss.Parent-Cont, ]
         <div className={classList}>
-            <AllStrainContainer 
-                // dummyprop={'dummyprop'}
+            <AllStrainContainer                 
                 url={props.url} setUrl={props.setUrl}
                 allStrains={props.allStrains} setAllStrains={props.setAllStrains}
                 currentStrain={props.currentStrain} setCurrentStrain={props.setCurrentStrain}            
