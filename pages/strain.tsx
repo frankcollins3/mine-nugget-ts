47294f8f1ff0eed1e79d09b357c69f831e5e80f3
Frank Collins <fwc3rd@gmail.com>
2022-11-24 08:11:55 -0500

    [strain] serverside-Props -> webpage -> component [8:05am]
    dbdata prisma/psql data passed through parent webpage sent from getStaticProps behind the scenes server-to-client.
    AllStrainContainer.tsx?8985:35 
    {getdata: Array(8)} 
    {id: 23, strainId: 1, strain: 'wedding cake', dominant: 'indica', funfact: 'leafly strain of year 2019', …} 
    {id: 37, strainId: 7, strain: 'pineapple express', dominant: 'sativa', funfact: 'name comes from meteorology: hawaiian water vapor, shares name with a movie', …}
    {id: 39, strainId: 1, strain: 'wedding cake', dominant: 'indica', funfact: 'leafly strain of year 2019', …} 
    {id: 40, strainId: 4, strain: 'mimosa', dominant: 'sativa-hybrid', funfact: 'nutrient greedy: needs encouragement to grow to full potential', …} 
    {id: 41, strainId: 2, strain: 'GorillaGlue#4', dominant: 'sativa', funfact: 'sticks to fingers and scissors, #1 cann cup 2014', …}
    {id: 42, strainId: 6, strain: 'white widow', dominant: 'balanced hybrid', funfact: 'passed genes to white rhino & white russian, growers prefer original', …}
    {id: 38, strainId: 3, strain: 'Do-Si-Dos', dominant: 'indica-hybrid', funfact: 'gender-fluid, warm or humid climate, in or outdoors', …}
    {id: 43, strainId: 5, strain: 'cherry pie', dominant: 'indica hybrid', funfact: 'effects in minutes, lasts for hours, pungent: use discretion', …}
    ****** THIS DATA ABOVE COMES FROM CHROME->DEV-TOOLS-INSPECT-> console.log(dbData) **** THIS DATA IS CLIENT SIDE CANT SEE IN TERMINAL

--- pages/strain.tsx
+++ pages/strain.tsx
@@ -1,51 +1,24 @@
- import $ from 'jquery'
+// import $ from 'jquery'
 // import modulecss from '../styles/Strain.module.scss'
 // import { $ }  from 'react-jquery-plugin'
 import styles from 'styles/Strain.module.scss'
 import getAllStrain from 'pages/api/getAllStrain'
 import AllStrainContainer from 'components/AllStrainContainer'
+import Random from 'utility/Randomizer'
 import React, { useEffect, useState, useContext } from 'react'
-import ReturnUrl from '.././utility/ReturnUrl'
+import ReturnUrl from 'utility/ReturnUrl'
 import Axios from 'axios';
 
-// import useSWR from 'swr'
-// import getAllStrain from './api/getAllStrain'
+    const access = () => {
+        // console.log("heyy") tested pokeAPI on/off here.
+    }
 
-const access = () => {
-    console.log('access')
-    $.ajax({
-        method: 'get',
-        url: '/api/getAllStrain',
-        // data: {
-        //     key: 'all'
-        // },        
-        // http://localhost:3000/pages/api/getAllStrain?key=all      look at keyall how it ends up in the url
-    }).then( (clientdata) => {
-        console.log('data')
-        console.log(clientdata)
-    })
-
-}
-
-
-export async function getServerSideProps(context:any) {
-    console.log("in the serverside props function")
-
-    let url:any = await ReturnUrl(context);
-    // let url = '/pages/api/getAllStrain'
-    // let url = 'https://pokeapi.co/api/v2/pokemon/2'
-
-    const baseURL = 'http://localhost'
-    let pokeurl = `https://pokeapi.co/api/v2/pokemon/`
-    // fetch(new URL(url, baseURL))
-
-    // let finallydata = await fetch(new URL(pokeurl))
-    let predata = await fetch(new URL(`${url}/api/getAllStrain`))
-    let serverdata = await predata.json()
-    console.log('this is my data')
-    console.log('data')
-    console.log(serverdata)
-
+export async function getServerSideProps(context:any) {            
+        let url:any = await ReturnUrl(context);    
+        let pokeurl = `https://pokeapi.co/api/v2/pokemon/`    
+        let relativepath = `pages/api/getAllStrain.ts`
+        let predata = await fetch(new URL(`${url}/api/getAllStrain`))
+        let serverdata = await predata.json()
     return {
       props: {
         serverdata    
@@ -53,28 +26,22 @@ export async function getServerSideProps(context:any) {
     };
   }
 
-export default function Strain ( props:any  ) {  
-    console.log('props')
-    console.log(props)
-    
-    
+export default  function Strain ( props:any ) {          
+    console.log('props.serverdata')
+    console.log(props.serverdata)
 
     const classList:string = [styles.Page, 'Column'].join(" ")
     const textClasses:string = [styles.FontSizeTest, styles.BorderTest].join(" ");
-    // const classList = [modulecss.Parent-Cont, modulecss.Test-Border].join(" ")
-
 
     return (
-    // let doubleClass = [modulecss.Parent-Cont, ]
         <div className={classList}>
-            <AllStrainContainer                 
+            <AllStrainContainer           
+                serverdata={props.serverdata}      
                 url={props.url} setUrl={props.setUrl}
                 allStrains={props.allStrains} setAllStrains={props.setAllStrains}
                 currentStrain={props.currentStrain} setCurrentStrain={props.setCurrentStrain}            
              />
-
-            <p className={textClasses}> quick look </p>
-            <button onClick={access}>   </button>
+            <button onClick={access}> </button>            
         </div>
     )
 }
