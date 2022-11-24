58670f1f0ecd36faae747a90b7b98910bcb55812
Frank Collins <fwc3rd@gmail.com>
2022-11-24 08:20:59 -0500

    [StrainContainer] db -> server-> client [8:15am]
    looking into global state but have officially passed data from:
    psql database -> through next.js serverside getStaticProps functionality -> into the client-facing "front-end" of this application

--- components/AllStrainContainer.tsx
+++ components/AllStrainContainer.tsx
@@ -5,78 +5,26 @@ import APIcall from '../utility/APIcall'
 import React, { useEffect, useState} from 'react';  
 import Alert from 'react-bootstrap/Alert';
 import Container from 'react-bootstrap/Container'
-import $ from 'jquery' // import * as $ from 'jquery'
-
-const getStrains = async () => {
-
-    $.ajax({
-        method: 'get',
-        url: './api/getAllStrain',
-          data: {
-            key: 'GETall'
-          }
-        }).then( (resFromAPI) => {
-          console.log('resFromAPI')
-          console.log(resFromAPI)
-          let res = resFromAPI || 'hey wats up'
-          // *  this is client side 
-          return  {
-              props: {strains: res}
-            }
-        })
-    
-}
+import $, { data } from 'jquery' // import * as $ from 'jquery'
 
 
-export async function getStaticProps() {
-    // console.log("hey were in the get static props") dont write code that you expect to see in your browser. this runs at build time. can only fetch and return data as props.
-    let allstrains:any = await APIcall('all', null, null)
-    // $.ajax({
-    //     method: 'get',
-    //     url: './api/getAllStrain',
-    //       data: {
-    //         key: 'GETall'
-    //       }
-    //     }).then( (resFromAPI) => {
-    //       console.log('resFromAPI')
-    //       console.log(resFromAPI)
-    //       let res = resFromAPI || 'hey wats up'
-    //       // *  this is client side 
-    //       return  {
-    //           props: {strains: res}
-    //         }
-    //     })
-    }
-
-
-export default  function AllStrainContainer({strains}:any, props:any) {    
-// export default  function AllStrainContainer(props:any) {    
-    console.log('strains')
-    console.log( { strains })
-    
-    // let allDb = props.allStrains
-    console.log('props')
+export default  function AllStrainContainer(props:any) {    
+    console.log('props from the container')
     console.log(props)
-    
 
-
-    
-
+    let db:string = props.serverdata
+    console.log('db')
+    console.log(db)
     
 
     // tried page:string .... page:any works.
     let allstrains: any[] = [   APIcall('any', null, null)]
-
-
-
     // const classList = [modulecss.Parent-Cont, modulecss.Test-Border].join(" ")
-
-
     return (
     // let doubleClass = [modulecss.Parent-Cont, ]
         <>
         {/* <ThemeContext> */}
-        <button type="button" onClick={getStrains}></button>
+        <button type="button"></button>
         <Container fluid className={styles.ColumnCenter}>
 
         </Container>
