dd4a03f4fe877a00a6e0c87fb04bcc59fd77b6cc
Frank Collins <fwc3rd@gmail.com>
2022-11-25 10:13:59 -0500

    update imports from tsConfig baseUrl = '/' [10:13am]
    {
      "compilerOptions": {
        "baseUrl": "."
      },
    
    baseurl means that the root of the file isn't relative to the scope of the file the functionality is in.
    if we are pulling functions from /pages/ or /src/components/container.js (i.e.)  all of our functions will be able to express imports relative to top level 
    }

--- components/AllStrainContainer.tsx
+++ components/AllStrainContainer.tsx
@@ -1,47 +1,50 @@
-import Strain from '../pages/strain'
-import styles from '../styles/AllStrainContainer.module.scss'
-import APIcall from '../utility/APIcall'
+import Strain from 'pages/strain'
+import styles from 'styles/AllStrainContainer.module.scss'
 // import getAllStrain from '../pages/api/getAllStrain'
 import React, { useEffect, useState} from 'react';  
 import Alert from 'react-bootstrap/Alert';
 import Container from 'react-bootstrap/Container'
 import $, { data } from 'jquery' // import * as $ from 'jquery'
 
+import APIcall from 'utility/APIcall'
+import MasterListStlye from 'utility/MasterListStyle'
+
+
 
 export default  function AllStrainContainer(props:any) {    
-    console.log('props from the container')
-    console.log(props)
 
+    const grabClasses = async () => {
+        let flexbottom = await styles.FlexBottom
+        console.log('flexbottom')        
+        console.log(flexbottom)        
+    }
+
+
+    
     let db:any = props.serverdata.getdata
-    let strainmap = db.map( (item:any, index:number) => {
-        console.log('item')
-        console.log(item)
-        console.log(item.strain)
+    let strainmap = db.map( (item:any, index:number) => {        
         let strain = item.strain
         let id:number = item.id
-        return (
-            // <ul key={index}> this would create a ul for every element that we are mapping through.
-                <li className={styles.li} key={id}> {strain} </li>
-            // </ul>
-        )
+        return (        
+            <div key={'column' + index} className="Column">
+            <img key={`id ${strain} `} src=""/>
+            <li className={styles.li} key={id}> {strain} </li>
 
+            </div>            
+        )
     })
 
 
 
 
-
-    // tried page:string .... page:any works.
-    let allstrains: any[] = [   APIcall('any', null, null)]
-    // const classList = [modulecss.Parent-Cont, modulecss.Test-Border].join(" ")
-    return (
-    // let doubleClass = [modulecss.Parent-Cont, ]
-        
+    
+    let allstrains: any[] = [   APIcall('any', null, null)]    
+    return (            
         <>
         <Container 
             style={{ overflowY: 'scroll' }}
-            fluid className={styles.ColumnCenter}>
-            <ul>
+            className={styles.ColumnCenter}>
+            <ul className={styles.ul}>
             {strainmap}
             </ul>
         </Container>
