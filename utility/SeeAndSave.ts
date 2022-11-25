4275ec9dd7c3d23e149827f8454482536b4a6aed
Frank Collins <fwc3rd@gmail.com>
2022-11-25 12:50:41 -0500

    strainClicking function [12:50pm]
    accept an element which will be the strain element represented by a gold bar that is clicked upon on AllStrainContainer.tsx
    
    the exported func will accept state to change and the state changing method to do that.
    
    this function will progress through the API that corresponds to the clicked on strain.
    The very last state that will be set will be a button to save the strain that doesn't need to be clicked.
    
    A user can click through all the api endpoints and save the psql data appended without saving their userId onto UsersStrains that associates userId and StrainId

---
+++ utility/SeeAndSave.ts
@@ -0,0 +1,5 @@
+export default async function SeeAndSave(element:string, textState:string, setTextState:any ) {
+    // element to change, the string-data-textState that will be the display text, setTextState which will setState for textState()
+    console.log(element)
+
+}
