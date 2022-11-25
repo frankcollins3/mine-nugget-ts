94e6981be8c8dd382e87df58077858391cb12848
Frank Collins <fwc3rd@gmail.com>
2022-11-25 08:17:04 -0500

    [DataCall]  [8:05am]

--- utility/DataCall.ts
+++ utility/DataCall.ts
@@ -1,26 +1,74 @@
 import $ from 'jquery'
 import Axios from 'axios'
 
-export default async function DataCall ( method:string, url:string, data:(string|null)) { 
-    const ajaxCall = async () => {
-        if (method === 'ajax') {
-            let ajax = $.ajax({
+// export default async function DataCall ( method:string, url:string, data:(string|null)) { 
+//     const ajaxCall = async () => {
+//         if (method === 'ajax') {
+//             let ajax = $.ajax({
+//                 method: 'get',
+//                 url: url,
+//                 data: 'json',
+//                 success: (ajaxdata) => {
+//                     console.log('ajaxdata')
+//                     console.log(ajaxdata)
+//                     return ajaxdata
+//                 }
+//             })
+//             return ajax
+//         }
+//     }
+//         return ajaxCall()
+
+//         if (method === 'axios') {
+
+//         }    
+
+//         }
+
+export default async function DataCall (method:string, url:string, data:(string|null)) {
+    console.log('url')
+    console.log(url)
+
+    class Call {
+
+        constructor(url:any) {
+
+        }      
+    // method getters
+    get ajax() {
+        return this.ajaxcall()
+    }
+
+    // methods 
+    ajaxcall() {
+           return $.ajax({
+            // let ajax = $.ajax({
                 method: 'get',
                 url: url,
                 data: 'json',
-                success: (ajaxdata) => {
-                    console.log('ajaxdata')
-                    console.log(ajaxdata)
-                    return ajaxdata
-                }
-            })
-            return ajax
-        }
+                // success: (ajaxdata) => {
+                //     console.log('ajaxdata')
+                //     console.log(ajaxdata)
+                //     return ajaxdata
+            }).then( (data) => {
+                console.log('data')
+                console.log(data)
+                return data            
+            })            
     }
-        return ajaxCall()
-
-        if (method === 'axios') {
-
-        }    
-
+}   // ajax call ending 
+    
+        if (method === 'ajax') {
+            const returnajaxcall:any = new Call(url).ajax
+            console.log('returnajaxcall')
+            console.log(returnajaxcall)
+            // console.log(returnajaxcall.responseJSON)
+            // let actualdata = returnajaxcall.responseJSON.getdata
+            // console.log('actualdata')
+            // console.log(actualdata)
         }
+
+
+
+
+}       // function end
