40e6a1fd0309366d34fda30876e920e9f5997610
Frank Collins <fwc3rd@gmail.com>
2022-11-26 09:16:52 -0500

    /pages/strain & <StrainContainer/> state linked b/w parent child 9:16am]
    move function to have access to state
    
    state linked between parents and children
    click the:          <AllStrainContainer <div> that holds the <li> />... 
    
    👍  this uses event.target.innerText endpoint to retrieve which was strain clicked on and change state with that string data 
    
    then click the test-button in the parent container that checks local parent state to find it changed and can be changed again corresponding always
     only to the "innertext" of last element clicked

--- pages/strain.tsx
+++ pages/strain.tsx
@@ -11,14 +11,6 @@ import Axios from 'axios';
 let relativepath = `/api/getAllStrain.ts`
 
 
-    const access = async (context:any) => {        
-        let url:string = await ReturnUrl(context);  
-        let ajaxstraindata = await DataCall('axios', `${url}/api/getAllStrain`, null) // /pages/api/getAllStrains
-        console.log('ajaxstraindata')
-        console.log(ajaxstraindata)
-        // let ajaxstraindata = await AjaxCall(`${url}/api/getAllStrain`, null, null) // /pages/api/getAllStrains
-        
-    }
 
 export async function getServerSideProps(context:any) {            
         let url:any = await ReturnUrl(context);    
@@ -40,6 +32,17 @@ export default  function Strain ( props:any ) {
     const classList:string = [styles.Page, 'Column'].join(" ")
     const textClasses:string = [styles.FontSizeTest, styles.BorderTest].join(" ");
 
+    const access = async (context:any) => {       
+      console.log('clickedStrain') 
+      console.log(clickedStrain) 
+      let url:string = await ReturnUrl(context);  
+      let ajaxstraindata = await DataCall('axios', `${url}/api/getAllStrain`, null) // /pages/api/getAllStrains
+      console.log('ajaxstraindata')
+      console.log(ajaxstraindata)
+      // let ajaxstraindata = await AjaxCall(`${url}/api/getAllStrain`, null, null) // /pages/api/getAllStrains        
+  }
+
+
     return (
         <div className={classList}>
             <AllStrainContainer    
@@ -49,7 +52,7 @@ export default  function Strain ( props:any ) {
                 allStrains={props.allStrains} setAllStrains={props.setAllStrains}
                 currentStrain={props.currentStrain} setCurrentStrain={props.setCurrentStrain}            
              />
-            {/* <button onClick={access}> </button>             */}
+            <button onClick={access}> </button>            
         </div>
     )
 }
