7dd0e892f78600ba021a8d754bddf1be38dfd0a6
Frank Collins <fwc3rd@gmail.com>
2022-11-25 22:23:38 -0500

    import/invocation v.s. hit-route return res.json() [10:23pm]
    import & function-invocation style v.s. ajax/axios hitting post route and returning res without sending out arguments
    
    for now going to use (:any) &&  typeof to separate between:
    import getSpecifiedStrain from 'api/getSpecifiedStrain' and invoking getSpecifiedStrain(targetStrainAsObject, [res])
    
    vs hitting that route:
    
    $.ajax({
    method: 'get' url: 'api/getSpecifiedStrain'    
    
    // this way you don't need to send data you can just do data: { 'key' } and it will pass anything and give you the res.json() 
    but res.json() will return an error if you're using the invocation method so to have some way to distinguish them...

--- pages/api/getSpecifiedStrain.ts
+++ pages/api/getSpecifiedStrain.ts
@@ -2,13 +2,16 @@ import Random from '../../utility/Randomizer'
 import { PrismaClient } from '@prisma/client';
 
 async function getSpecifiedStrain(req:any, res:any|null) {          // changing or null. if you're invoking the function you don't want a value. its the ajax call that needs the value. this req can be the strain.strain/name
-        console.log('in the get specified strain function')
-        console.log('req')
-        console.log(req)
+         if (typeof req === 'object') {
+            console.log("allow the object to be for when the request is the object of the strain being sent import/invocation style")
+
+         } else if (typeof req === 'string') {
+            console.log("this should be the ajax string data of doing things")
+             res.json( { getdata: {hi: 'heyy'} })        
+         }
 
            
 
-         res.json( { getdata: {hi: 'heyy'} })        
 }
 
 export default getSpecifiedStrain
