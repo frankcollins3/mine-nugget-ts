ad1651e6e19933eb488e3362a8e5c1a7d8e4348d
Frank Collins <fwc3rd@gmail.com>
2022-11-25 22:18:59 -0500

    [getSpecifiedStrain] change 2nd param [10:18pm]
    {hi'} testing object
    
    revamping this function to allow null for the second argument.
    
    i'm noticing that ajax call allows the response to be returned but it doesn't actually invoke the function and allow for the console.logs() to go off
    
    while the ajax can get the response object with nothing in it, the invocation way requires that the strain is sent as a parameter because 
    the function will need a return statement not a res.json() statement

--- pages/api/getSpecifiedStrain.ts
+++ pages/api/getSpecifiedStrain.ts
@@ -1,11 +1,21 @@
-export default async function specifyDbStrain (req:string, res:string) {
-    console.log('req')
-    console.log(req)
-    try {
+import Random from '../../utility/Randomizer'
+import { PrismaClient } from '@prisma/client';
 
-    }
-    catch (err) {
-        
-    }
+async function getSpecifiedStrain(req:any, res:any|null) {          // changing or null. if you're invoking the function you don't want a value. its the ajax call that needs the value. this req can be the strain.strain/name
+        console.log('in the get specified strain function')
+        console.log('req')
+        console.log(req)
 
+           
+
+         res.json( { getdata: {hi: 'heyy'} })        
 }
+
+export default getSpecifiedStrain
+
+
+//    const strain = await prisma.strains.findUnique({
+        //     where: {
+        //       strain: req.body,
+        //     },
+        //   })
