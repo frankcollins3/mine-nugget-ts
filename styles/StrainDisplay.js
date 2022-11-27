9a69e048e9d0dab20fecc2c6f542e7f6852e8601
Frank Collins <fwc3rd@gmail.com>
2022-11-27 01:44:46 -0500

    state based changes [1:44am]
    this isn't exactly working as needed. the above code works
    
    👎  opacity: clickedStrain.length > 5 ? 1.0 : 0.0
    
    but this length based existence-checking expression doesn't work as .length returns an error.
    
    since this works then just setting a boolean true/false that triggers opacity toggling based on an API/info-display being interacted with.

--- styles/StrainDisplay.js
+++ styles/StrainDisplay.js
@@ -1,9 +1,13 @@
 // Button.js
 import styled from 'styled-components';
+
 const Display = styled.div`
+    
+    opacity: ${props => props.clickedStrain ? "1.0" : "0.1"};
+    margin-top: 1.5em;
     border: 5px solid hotpink;
     height: 200px;
-    width: 800px;    
+    width: 400px;    
   }
 `
 export default Display;
