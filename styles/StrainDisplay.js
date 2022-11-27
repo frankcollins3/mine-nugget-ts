cf30925beba14c8b1d0d15b416185ce519297560
Frank Collins <fwc3rd@gmail.com>
2022-11-27 01:07:15 -0500

    styled components for display container [1:07am]
    want to possibly set up props based CSS

---
+++ styles/StrainDisplay.js
@@ -0,0 +1,9 @@
+// Button.js
+import styled from 'styled-components';
+const Display = styled.div`
+    border: 5px solid hotpink;
+    height: 200px;
+    width: 800px;    
+  }
+`
+export default Display;
