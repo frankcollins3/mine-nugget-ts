0f07c61f0852e800bec2d1975fd5ecea0a12dee5
Frank Collins <fwc3rd@gmail.com>
2022-11-26 23:52:52 -0500

    prisma returning db, then loop [11:52pm]
    am checking for index to see if theres an order to the strains and if they correspond to the order of the psql table
    will probably check for duplicates by string name

--- pages/api/strains/update.ts
+++ pages/api/strains/update.ts
@@ -1,11 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient()
 export default async function update (req:any, res:any) {              // res:string doesn't work
-    console.log('we are hitting the update route')
-    console.log('req')
-    console.log(req)
+    // console.log('we are hitting the update route')
+    // console.log('req')
+    // console.log(req)
 
-    
+    let dbstrains = await prisma.strains.findMany()
+    dbstrains.forEach( (data:string|object) => {
+        console.log('data in the dbstrains foreach')
+        console.log(data)
+    })
+
     // console.log(strains)
-    res.json(  {ayoo: 'hi'}  )
+    res.json(  {watsup: 'hi'}  )
 }
