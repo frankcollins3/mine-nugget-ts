354fbcafa30cac91203a4fcc009d7d018a1b236e
Frank Collins <fwc3rd@gmail.com>
2022-11-27 02:54:53 -0500

    [strainDisplay] conditional logic now works [2:54pm]

--- styles/StrainDisplay.js
+++ styles/StrainDisplay.js
@@ -2,22 +2,15 @@ import styled from 'styled-components';
 
 
 
-const Display = styled("div")`
-${props => {
-    console.log('props firing props from the styled component')
-    console.log(props)
-    console.log(props.clickedStrain)
-    if (props.clickedStrain === undefined ) {
-        console.log("props.clickedstrain = undefined")
-    }
-    props.clickedStrain && `
-    display: inline;
-    padding-top: 10px;
-    padding-right: 30px;    
-    border: ${props.clickedStrain === undefined ? '5px solid hotpink' : '3px solid blue'}
-    `}
-}
-`;
+const Display = styled.div`    
+    opacity: ${props => props.clickedStrain === undefined ? "1.0" : "0.1"};
+    border: 5px solid hotpink;
+    min-height: 10vh;
+    min-width: 10vw;
+`
+
+
+
 
 
 export default Display
