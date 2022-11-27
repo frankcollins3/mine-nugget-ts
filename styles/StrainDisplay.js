d003c343e67f47caec409d95e39a03cfccaeddb5
Frank Collins <fwc3rd@gmail.com>
2022-11-27 17:01:28 -0500

    [styled-component] restore defaults
    was also trying to use jquery to target the children within an expression but opted for inline state based styling.

--- styles/StrainDisplay.js
+++ styles/StrainDisplay.js
@@ -1,19 +1,30 @@
 import styled from 'styled-components';
+import $ from 'jquery'
 
 
 
 const Display = styled.div`        
-        opacity: ${props => props.clickedStrain === undefined ? "0.1" : "1.0"};        
+        opacity: ${props => props.clickedStrain === undefined ? "0.1" : "1.0"};       
         
+        
+        display: flex;
+        flex-flow: column wrap;
+        justify-content: center;
+        align-items: center;
         margin-top: 2em;
-        color: papayawhip;
+        // color: papayawhip;
         min-height: 8vh;
         min-width: 50vw;
-        `
+        `        
         
         const CardStyle = styled(Display)`
+        // background-color: ${props => props.bgToggle === 'new' ? "rgb(62, 50, 32)" : "transparent"};       
+        // background-color: ${props => props.bgToggle === 'new' ? "red" : "transparent"};       
         box-shadow: 10px 10px 10px rgb(247, 208, 32);    
         border: 3px solid rgb(247, 208, 36);
+    
+        
+        
         
      
 `
