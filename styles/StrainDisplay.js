77d485ec43fad073bffeb8c0696b350ef9ac5251
Frank Collins <fwc3rd@gmail.com>
2022-11-27 14:48:38 -0500

    invoke style method: reference above component and store [2:48pm]
    const CardStyle = styled(Display)`

--- styles/StrainDisplay.js
+++ styles/StrainDisplay.js
@@ -4,13 +4,25 @@ import styled from 'styled-components';
 
 const Display = styled.div`        
         opacity: ${props => props.clickedStrain === undefined ? "0.1" : "1.0"};        
-        border: 5px solid hotpink;
-        min-height: 10vh;
-        min-width: 10vw;
+        
+        margin-top: 2em;
+        color: papayawhip;
+        min-height: 8vh;
+        min-width: 50vw;
+        `
+        
+        const CardStyle = styled(Display)`
+        box-shadow: 10px 10px 10px rgb(247, 208, 32);    
+        border: 3px solid rgb(247, 208, 36);
+        
+     
 `
 
 
 
 
 
-export default Display
+
+
+export default CardStyle
+// export default [Display, CardStyle]
