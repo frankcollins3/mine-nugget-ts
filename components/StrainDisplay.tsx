1718bac5d4ac0f2d2844e9a73b03f2d1bcc972f9
Frank Collins <fwc3rd@gmail.com>
2022-11-27 13:07:40 -0500

    [ts-nocheck] not most guilt-free option [1:06pm]
    // @ts-nocheck
    oh wow
    
    No overload matches this call.
      Overload 1 of 2, '(props: { slot?: string | undefined; style?: CSSProperties | undefined; title?: string | undefined; ref?: ((instance: HTMLDivElement | null) => void) | RefObject<...> | null | undefined; ... 254 more ...; onTransitionEndCapture?: TransitionEventHandler<...> | undefined; } & { ...; } & { ...; }): ReactElement<...>', gave the following error.
        Type '{ children: Element; setClickedStrain: any; onClick: any; clickedStrain: any; }' is not assignable to type 'IntrinsicAttributes & { slot?: string | undefined; style?: CSSProperties | undefined; title?: string | undefined; ref?: ((instance: HTMLDivElement | null) => void) | RefObject<...> | null | undefined; ... 254 more ...; onTransitionEndCapture?: TransitionEventHandler<...> | undefined; } & { ...; } & { ...; }'.
          Property 'setClickedStrain' does not exist on type 'IntrinsicAttributes & { slot?: string | undefined; style?: CSSProperties | undefined; title?: string | undefined; ref?: ((instance: HTMLDivElement | null) => void) | RefObject<...> | null | undefined; ... 254 more ...; onTransitionEndCapture?: TransitionEventHandler<...> | undefined; } & { ...; } & { ...; }'.
      Overload 2 of 2, '(props: StyledComponentPropsWithAs<"div", any, {}, never, "div", "div">): ReactElement<StyledComponentPropsWithAs<"div", any, {}, never, "div", "div">, string | JSXElementConstructor<...>>', gave the following error.
        Type '{ children: Element; setClickedStrain: any; onClick: any; clickedStrain: any; }' is not assignable to type 'IntrinsicAttributes & { slot?: string | undefined; style?: CSSProperties | undefined; title?: string | undefined; ref?: ((instance: HTMLDivElement | null) => void) | RefObject<...> | null | undefined; ... 254 more ...; onTransitionEndCapture?: TransitionEventHandler<...> | undefined; } & { ...; } & { ...; }'.
          Property 'setClickedStrain' does not exist on type 'IntrinsicAttributes & { slot?: string | undefined; style?: CSSProperties | undefined; title?: string | undefined; ref?: ((instance: HTMLDivElement | null) => void) | RefObject<...> | null | undefined; ... 254 more ...; onTransitionEndCapture?: TransitionEventHandler<...> | undefined; } & { ...; } & { ...; }'.ts(2769)
    No quick fixes available
    
    i am not using setTimeout in the styled components file to apply functionality onto the bootstrap component
    
    ill be testing the setState() prop that is triggering the error. if setState() works it looks like the error is only annoying and not breaking anything 
    doesn't hurt to ignore with code?  
    
    *****
    just getting in the rep of pulling typescript code and looking up the questions in a way that the answers have relevant utility to the task.current()

--- components/StrainDisplay.tsx
+++ components/StrainDisplay.tsx
@@ -1,16 +1,18 @@
+// @ts-nocheck
 import styled from 'styled-components'
 import Display from 'styles/StrainDisplay'
 import $ from 'jquery'
 import CSS from 'utility/CSStool'
 
-const changeMe = async (event) => {
-    console.log('hey im being clicked')
-    let target = $(event.target)
-    CSS(target, 'border', '5px solid hotpink')
-}
-export default function StrainDisplay (props, {changeMe}) {
-    console.log('props')
-    console.log(props)
+// const changeMe = async (event) => {
+//     console.log('hey im being clicked')
+//     let target = $(event.target)
+//     // $(event.target).css('height', '400px')
+//     CSS($(event.target), 'border', '5px solid white')
+// }
+
+export default function StrainDisplay (props) {
+    
     return (
     // return as any (
         // clickedStrain={props.clickedStrain}
@@ -18,12 +20,11 @@ export default function StrainDisplay (props, {changeMe}) {
         // 
         <Display
         setClickedStrain={props.setClickedStrain}       
-        onClick={changeMe}
-        onMouseEnter={changeMe}
+        onClick={props.onClick}        
         clickedStrain={props.clickedStrain} 
         >
         <div>
-                      
+            {/* <button onClick={changeMe}></button>               */}
         </div>
          </Display>
     )
