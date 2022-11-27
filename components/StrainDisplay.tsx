8747df1d9a42ffe4bc8e7909af9f9cb6a16591da
Frank Collins <fwc3rd@gmail.com>
2022-11-27 02:00:35 -0500

    display container [1:58am]
    this component shares clickedStrain state and appends the api information from the corresponding click item from its sister component <AllStrainContainer/>

---
+++ components/StrainDisplay.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+import Display from 'styles/StrainDisplay'
+
+export default function StrainDisplay () {
+    return (
+        <Display>
+
+        <div>
+            <p> yeah sure </p>            
+        </div>
+        </Display>
+    )
+}
