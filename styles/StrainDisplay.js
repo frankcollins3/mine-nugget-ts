fb7aa7252adcc18c8fd1e7d21fe8e8548aa5b6e2
Frank Collins <fwc3rd@gmail.com>
2022-11-27 14:26:13 -0500

    [StrainDisplay] fixed. wasn't redeclaring props [2:26pm]
    also noticing:
    you would ideally create a /stylecomponent/StrainDisplay.js          and a separate /sass  directory 
    [styled component directory && sass directory distinguishing files]

--- styles/StrainDisplay.js
+++ styles/StrainDisplay.js
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 
 
 
-const Display = styled.div`    
-    opacity: ${props => props.clickedStrain === undefined ? "1.0" : "0.1"};
-    border: 5px solid hotpink;
-    min-height: 10vh;
-    min-width: 10vw;
+const Display = styled.div`        
+        opacity: ${props => props.clickedStrain === undefined ? "0.1" : "1.0"};        
+        border: 5px solid hotpink;
+        min-height: 10vh;
+        min-width: 10vw;
 `
 
 
