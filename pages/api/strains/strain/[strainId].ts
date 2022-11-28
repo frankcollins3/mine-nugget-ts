19bb5327bb016ee3981702d93100b2a9cd7a3340
Frank Collins <fwc3rd@gmail.com>
2022-11-28 08:27:47 -0500

    dynamic route includes distinguishing key [8:27am]
    before i couldn't get res.json to work in an if block but this is working. 
    this code is parsing the key that is included in the ajax data call.
    
    Another function hitting this route elsewhere within the app will have it accessing this same (specific strain) data
    and rendering a few elements based on the same API it spits back out when you hit this route this way with the:
    
    .includes('no') from nokey which is included in the url params with the strain.strainId that can be used to access API              
    this is the endpoint we access during the fetch on client side:              api/strains/strain/nokey${strainId}

--- pages/api/strains/strain/[strainId].ts
+++ pages/api/strains/strain/[strainId].ts
@@ -8,40 +8,40 @@ let prisma = new PrismaClient()
 export default async function (req:any, res:any) {              // res:string doesn't work
     let strainbucket:(string|number|object)[] = new Array() || []
 
-    console.log('req')
-    console.log(req)
+    
     // let reqStr = req.query
     let twostringkeys = req.query.strainId
-    console.log('twostringkeys')
-    console.log(twostringkeys)
 
-    let reqstr = await Regex(twostringkeys, 'numreturn')
-    console.log('reqstr')
-    console.log(reqstr)
-    // console.log(typeof reqStr)
-
-    // let reqStr = parseInt(req.query.strainId)        
-    let dbstrain = await prisma.strains.findMany()
-
+    if (twostringkeys.includes('no')) {
+        console.log("it includes no!!!!")
+        
+        let reqstr = await Regex(twostringkeys, 'numreturn')
+        console.log('reqstr')
+        console.log(reqstr)
+        // console.log(typeof reqStr)
     
-
-    dbstrain.forEach( (strain) => {
-        console.log('typeof strain.strainId')
-        console.log(typeof strain.strainId)
-        let strainstring:any = strain.strainId
-
-        // if (strainstring.toString() === reqStr) {
-        //     console.log("strain is equal to param")            
-        //     console.log('strain here')
-        //     console.log(`strain ${strain}`)
-        //     strainbucket.push({ strain })
-        // }
-    })
+        // let reqStr = parseInt(req.query.strainId)        
+        let dbstrain = await prisma.strains.findMany()
     
-    // strains.forEach( (strain:object|string) => {
-    //     console.log('strain in the /strain/[strainId] dynamic route')
-    //     console.log(strain)
-    // })
-    // console.log(strains)
-    res.json(  strainbucket  )
+        
+    
+        dbstrain.forEach( (strain) => {
+            console.log('typeof strain.strainId')
+            console.log(typeof strain.strainId)
+            let strainstring:any = strain.strainId
+    
+            if (strainstring.toString() === reqstr) {
+                console.log("strain is equal to param")            
+                console.log('strain here')
+                console.log(`strain ${strain}`)
+                strainbucket.push({ strain })
+            }
+        })
+        
+        
+        res.json(  strainbucket  )
+    } else if (!twostringkeys.includes('no')) {
+        console.log("the data doesn't have that so this same function should return undefined.")
+        
+    }
 }
