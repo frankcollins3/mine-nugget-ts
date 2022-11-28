cb92c2a5d6555b033e1248a5621de0750384240c
Frank Collins <fwc3rd@gmail.com>
2022-11-28 10:37:01 -0500

    [StrainDisplay] successfully append db.strData [10:35am]

--- components/StrainDisplay.tsx
+++ components/StrainDisplay.tsx
@@ -10,13 +10,17 @@ import CSS from 'utility/CSStool'
 
 
 export default function StrainDisplay (props) {
+    console.log('props')
+    console.log(props)
+
+    let stateString:string = props.textState
+
     let columnclass = 'Column'
     let card = 'card'   // i wonder if doing this likes this takes the string data out of the scope of being tied to bootstrap 
     // bootstrap might only apply to inline styling. these 2 might be blended like a normal string.
     let doubleCardClass = [card, columnclass].join(' ')
 
     return (
-
     // return as any (
 <>
         <CardStyle
@@ -29,7 +33,9 @@ export default function StrainDisplay (props) {
                 >            
                 <p
                 style={{ backgroundColor: props.bgToggle === 'new' ? 'transparent' : 'rgb(62, 50, 32)'}}
-                 className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                 className="card-text">
+                    {stateString || ''}
+                 </p>
             </div>
     
          </CardStyle>            
