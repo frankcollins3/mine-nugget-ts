5e97b9202a431b5350e440b9c8fbee2d56d3b4aa
Frank Collins <fwc3rd@gmail.com>
2022-11-27 23:50:37 -0500

    key created to split this route up [11:50pm]
    twostringkeys
    nokey1                                                       // no key is what is wanted. 1 extra step to get the same id as before now there are distinguishing characters 
    were in the numreturn if block!
    getNumbers
    1
    reqstr
    1
    
    distinguishing characters to create a condition-logic-enforced gate.
    
    this route will utilize the same set of data for both of its uses (which is the data specified by id which is retrieved by clicking the element)
    
    1) one use is to just return object endpoint data
    2) is to create a very simple [ /strain/${id} ] landing page where you hit the address-url and see that data based on url-params

--- pages/api/strains/strain/[strainId].ts
+++ pages/api/strains/strain/[strainId].ts
@@ -1,13 +1,25 @@
 
 import APIcall from 'utility/APIcall'
 import { PrismaClient } from '@prisma/client';
+import Regex from 'utility/MasterRegex'
 let prisma = new PrismaClient()
+
+
 export default async function (req:any, res:any) {              // res:string doesn't work
     let strainbucket:(string|number|object)[] = new Array() || []
-    let reqStr = req.query.strainId
-    console.log('reqStr')
-    console.log(reqStr)
-    console.log(typeof reqStr)
+
+    console.log('req')
+    console.log(req)
+    // let reqStr = req.query
+    let twostringkeys = req.query.strainId
+    console.log('twostringkeys')
+    console.log(twostringkeys)
+
+    let reqstr = await Regex(twostringkeys, 'numreturn')
+    console.log('reqstr')
+    console.log(reqstr)
+    // console.log(typeof reqStr)
+
     // let reqStr = parseInt(req.query.strainId)        
     let dbstrain = await prisma.strains.findMany()
 
@@ -17,12 +29,13 @@ export default async function (req:any, res:any) {              // res:string do
         console.log('typeof strain.strainId')
         console.log(typeof strain.strainId)
         let strainstring:any = strain.strainId
-        if (strainstring.toString() === reqStr) {
-            console.log("strain is equal to param")
-            console.log('strain')
-            console.log(strain)
-            strainbucket.push({ yeah: 'sure'!})
-        }
+
+        // if (strainstring.toString() === reqStr) {
+        //     console.log("strain is equal to param")            
+        //     console.log('strain here')
+        //     console.log(`strain ${strain}`)
+        //     strainbucket.push({ strain })
+        // }
     })
     
     // strains.forEach( (strain:object|string) => {
