550fb20fc2d7b557e1325f7ca1d56ced318a6c62
Frank Collins <fwc3rd@gmail.com>
2022-11-27 22:49:06 -0500

    [getSpecifiedStrain] fix array w/ no types [10:48pm]
    the regular javascript array syntax wasn't messing up this function 
    but was interfering with the return of the data in the other fetching-data-function

--- pages/api/strains/getSpecifiedStrain.ts
+++ pages/api/strains/getSpecifiedStrain.ts
@@ -3,46 +3,35 @@ import Random from '../../../utility/Randomizer'
 import $ from 'jquery'
 import { PrismaClient } from '@prisma/client';
 let prisma = new PrismaClient()
-let strainbucket = new Array() || []
+// let strainbucket = new Array() || []
+let strainbucket:(string|number|object)[] = []
 
 async function getSpecifiedStrain(req:any, res:any|null,) {          // changing or null. if you're invoking the function you don't want a value. its the ajax call that needs the value. this req can be the strain.strain/name
-        strainbucket.splice(0, strainbucket.length) 
+strainbucket.splice(0, strainbucket.length) 
 
 
-        let prisma = new PrismaClient()
-        // console.log('req SERVER SIDE')
-       const query = req.query
-       let strain:string = query.strain
-       let alldbstrains = await prisma.strains.findMany()
-        
-        await alldbstrains.forEach(async(item) => {
-//        let specifiedstrain = await alldbstrains.forEach( (item) => {
-//        alldbstrains.forEach( (item:object|string) => {                
-                if (item.strain === strain) {
-                        let strain:string = item.strain
+let prisma = new PrismaClient()
+// console.log('req SERVER SIDE')
+const query = req.query
+let strain:string = query.strain
+let alldbstrains = await prisma.strains.findMany()
 
-                        console.log(`i just clicked on the ${strain}`)
-                        console.log('item in the if block')
-                        console.log(item)
-                        // return item
 
-                        let onestrain = await APIcall('specify', strain, null)
-                        console.log('onestrain')
-                        console.log(onestrain)
-
-                        await strainbucket.push(item)
-                }
-                
-                //        prisma.strains.findUnique({
-                        //         where: {
+await alldbstrains.forEach(async(item) => {
+        if (item.strain === strain) {
+                let strain:string = item.strain                        
+                // let onestrain = await APIcall('specify', strain, null)                        
+                        strainbucket.push(item)
+        }
 })
-        await res.json( strainbucket )
+
+await res.json( strainbucket )
 //                 strain: 'white widow'
 //         }
 //        })
 
-        // return  { getdata: 'return statement instead of res.json'}
-        //     return statement
+// return  { getdata: 'return statement instead of res.json'}
+//     return statement
 
 }
 
@@ -50,7 +39,7 @@ export default getSpecifiedStrain
 
 
 //    const strain = await prisma.strains.findUnique({
-        //     where: {
-        //       strain: req.body,
-        //     },
-        //   })
+//     where: {
+//       strain: req.body,
+//     },
+//   })
