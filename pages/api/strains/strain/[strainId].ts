bcc092561b1bd97779601d7ee27ccad8bb3c41c4
Frank Collins <fwc3rd@gmail.com>
2022-11-27 22:14:19 -0500

    linked and returning data [10:14pm]
    forgot that the data key in the ajax call had nothing to do with anything, and that everything relied on the req.query from the dynamic route query string
    data: { strain: text }

--- pages/api/strains/strain/[strainId].ts
+++ pages/api/strains/strain/[strainId].ts
@@ -1,8 +1,34 @@
 import APIcall from 'utility/APIcall'
+import { PrismaClient } from '@prisma/client';
+let prisma = new PrismaClient()
 export default async function (req:any, res:any) {              // res:string doesn't work
-    let reqStr:string = req.query.strainId    
-    // let strains = await APIcall('specify', 'white widow', null)
-    let strains = await APIcall('specify', reqStr, null)
+    let reqStr = req.query.strainId
+    console.log('reqStr')
+    console.log(reqStr)
+    console.log(typeof reqStr)
+    // let reqStr = parseInt(req.query.strainId)        
+    let dbstrain = await prisma.strains.findMany()
+
+    console.log('reqStr')
+    console.log(reqStr)
+
+    dbstrain.forEach( (strain) => {
+        console.log('typeof strain.strainId')
+        console.log(typeof strain.strainId)
+
+        if (strain.strainId.toString() === reqStr) {
+            console.log("strain is equal to param")
+            console.log('strain')
+            console.log(strain)
+        }
+    })
+
+    
+    
+    // strains.forEach( (strain:object|string) => {
+    //     console.log('strain in the /strain/[strainId] dynamic route')
+    //     console.log(strain)
+    // })
     // console.log(strains)
     res.json(  reqStr  )
 }
