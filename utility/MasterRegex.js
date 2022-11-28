2e04c46a04644926bf2cf0b4e730947b1302c35c
Frank Collins <fwc3rd@gmail.com>
2022-11-27 23:31:06 -0500

    bulletproof class [11:31pm]
    anti-define-error && type-safety

--- utility/MasterRegex.js
+++ utility/MasterRegex.js
@@ -1,18 +1,23 @@
-export default async function Regex(url) {      // already forgot its not TS file: !(url:string)
-    class Exp {
-        constructor(url) {
-            this.url = url
-        }
-
+export default async function Regex(url, action ) {      // already forgot its not TS file: !(url:string)
+    if (url !== null || url !== undefined && typeof url === 'string') {
+        class Exp {
+            constructor(url) {
+                this.url = url
+            }
+            
             get numreturn () {
                 return this.numberexp()
             }
-
+            
             numberexp () {
                 if (typeof url === 'string') {
                     let onlynumbers = url.replace(/[/\a-z]/g, '')
                     return onlynumbers
                 }
             }
+        }
+        
+
     }
 }
+
