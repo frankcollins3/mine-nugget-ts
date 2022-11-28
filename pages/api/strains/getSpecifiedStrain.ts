8760252374fa0433fde6ff41908ab0cc1c78dda0
Frank Collins <fwc3rd@gmail.com>
2022-11-27 21:22:40 -0500

    return object from the psql database [9:22pm]
    call2
    AllStrainContainer.tsx?8985:87 
    [{…}]
    dominant
    "balanced hybrid"
    funfact
    "passed genes to white rhino & white russian, growers prefer original"
    id
    45
    parents
    "south indian indica, brazilian sativa landrace"
    strain
    "white widow"
    strainId
    6
    Array(0)
    
    glad the res.json returns the array and there's no async error waiting for the loop.

--- pages/api/strains/getSpecifiedStrain.ts
+++ pages/api/strains/getSpecifiedStrain.ts
@@ -3,34 +3,36 @@ import Random from '../../../utility/Randomizer'
 import $ from 'jquery'
 import { PrismaClient } from '@prisma/client';
 let prisma = new PrismaClient()
+let strainbucket = new Array() || []
 
 async function getSpecifiedStrain(req:any, res:any|null,) {          // changing or null. if you're invoking the function you don't want a value. its the ajax call that needs the value. this req can be the strain.strain/name
         let prisma = new PrismaClient()
         // console.log('req SERVER SIDE')
-
        const query = req.query
        let strain:string = query.strain
-        
        let alldbstrains = await prisma.strains.findMany()
-       
-       alldbstrains.forEach( (item) => {
+        
+        alldbstrains.forEach( (item) => {
+//        let specifiedstrain = await alldbstrains.forEach( (item) => {
 //        alldbstrains.forEach( (item:object|string) => {                
                 if (item.strain === strain) {
                         let strain:string = item.strain
 
                         console.log(`i just clicked on the ${strain}`)
-                        console.log('strain')
-                        console.log(strain)
+                        console.log('item in the if block')
+                        console.log(item)
+                        // return item
+                        strainbucket.push(item)
                 }
-       })
-
-//        prisma.strains.findUnique({
-//         where: {
+                
+                //        prisma.strains.findUnique({
+                        //         where: {
+})
+                        res.json( strainbucket )
 //                 strain: 'white widow'
 //         }
 //        })
 
-        res.json( { strain })
         // return  { getdata: 'return statement instead of res.json'}
         //     return statement
 
