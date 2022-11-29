ff436c9fd4bf7133b6ffab94c63cba1e1e9ed78b
Frank Collins <fwc3rd@gmail.com>
2022-11-28 21:42:07 -0500

    [See API save strain to db file] state changed [9:42pm]
    this state changes and renders "how are you" on the <displayContainer/>
    this functionality is initiated from <AllStrainContainer/> by clicking on the strains of the container.
    
    now to parse the element and return the name of the strain

--- utility/SeeAndSave.ts
+++ utility/SeeAndSave.ts
@@ -1,5 +1,10 @@
-export default async function SeeAndSave(element:string, textState:string, setTextState:any ) {
+export default async function SeeAndSave(element, textState:string, setTextState:any ) {
     // element to change, the string-data-textState that will be the display text, setTextState which will setState for textState()
+    console.log('element')
     console.log(element)
 
+    console.log('textState')
+    console.log(textState)
+
+    setTextState('hey how are you')
 }
