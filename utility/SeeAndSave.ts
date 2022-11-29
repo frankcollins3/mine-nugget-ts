084829524d57b54317f0e07a72ad91d154e909a0
Frank Collins <fwc3rd@gmail.com>
2022-11-28 21:51:10 -0500

    whats going on more easily understood now [9:48pm]
    changed param 'element' to api (i was originally going to create state and send over object-endpoint state from $('') jquery targeting the display div container.
    
    theres an additional parameter called length. hoping to not need this but:
    the length will help to restart when we reach the end of the api. array-indexing may make need for [index + 1] (and will probably need state to do so) 
    
    thats why i was going to pass an element through as well, to get the innerText of that element and continually loop through the API and do [i + 1] 
    whenever the loop reaches the current active innerText so it progresses onto the next endpoint.

--- utility/SeeAndSave.ts
+++ utility/SeeAndSave.ts
@@ -1,9 +1,9 @@
-export default async function SeeAndSave(element, textState:string, setTextState:any ) {
+export default async function SeeAndSave(api, apilength, textState:string, setTextState:any ) {
     // element to change, the string-data-textState that will be the display text, setTextState which will setState for textState()
-    console.log('element')
-    console.log(element)
+    console.log('api element')
+    console.log(api)
 
-    let strain:string = element[0].strain
+    let strain:string = api[0].strain
     console.log('strain API functions')
     console.log(strain)
 
