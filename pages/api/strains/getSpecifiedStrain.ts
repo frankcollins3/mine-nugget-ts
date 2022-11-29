fba7a51217ca03f3363fb099de3f97835e6d80a2
Frank Collins <fwc3rd@gmail.com>
2022-11-28 22:05:36 -0500

    switch to consuming & using API data vs prisma-db data [10:05pm]
    onestrain
    [
      {
        strain: 'wedding cake',
        dominant: 'indica',
        funfact: 'leafly strain of year 2019',
        parents: 'triangle kush, animal mints',
        taste: 'sour-tang, earthy-pepper, creamy, vanilla',
        smell: 'vanilla, frosting',
        gold: 'pain, insomnia, appetite',
        nugget: 'short, dense',
        thc: '25%',
        cbd: '0.1%'
      }
    ]
    
    heres the strain API data up above this text
    
    and below this text is the data from the psql database that is taken from the API. just proof-of-concept using both back and forth.
    the data is fetched from APIcall with jquery and the server side retrieval of the database data is done with await fetch 
    
    {id: 45, strainId: 6, strain: 'white widow', dominant: 'balanced hybrid', funfact: 'passed genes to white rhino & white russian, growers prefer original', …}
    dominant
    "balanced hybrid"
    funfact
    "passed genes to white rhino & white russian, growers prefer original"
    parents
    "south indian indica, brazilian sativa landrace"
    strain
    "white widow"
    strainId
    6
    Object

--- pages/api/strains/getSpecifiedStrain.ts
+++ pages/api/strains/getSpecifiedStrain.ts
@@ -9,51 +9,33 @@ let strainbucket:(string|number|object)[] = []
 
 async function getSpecifiedStrain(req:any, res:any|null,) {          // changing or null. if you're invoking the function you don't want a value. its the ajax call that needs the value. this req can be the strain.strain/name
 strainbucket.splice(0, strainbucket.length) 
-
-console.log('req')
-console.log(req)
-
 let prisma = new PrismaClient()
-// console.log('req SERVER SIDE')
 const query = req.query
-console.log('query')
-console.log(query)
-
 let prestrain:string = query.strain
-
-
 let strainq:string = query.strain
-// let strainq:string = await Regex(prestrain, 'numreturn')
-console.log('strainq')
-console.log(strainq)
+
+
 
 
 let alldbstrains = await prisma.strains.findMany()
 
 // strain:(obect|string)
         // let strains = await APIcall('all', null, null)
-        // await strains.forEach(async(strain) => {          
-        //         console.log('strain in the forEach loop')
-        //         console.log(strain)
-        //         if (strain.string === strainq) {
-        //                 console.log('strainq they equal each other!s')
-        //                 console.log(strainq)
-        //                 console.log(strain)
+        // await strains.forEach(async(strain) => {                  
+        //         if (strain.string === strainq) {        
         //                 await strainbucket.push(strain)
         //         }
         // })
-
-// let onestrain = await APIcall('specify', strain, null)                        
-
+        let onestrain = await APIcall('specify', strainq, null)                        
                         await alldbstrains.forEach(async(item) => {
                                 if (item.strain === strainq) {
                                         let strain:string = item.strain                        
-                                console.log('strain')
-                                console.log(strain)
+                                // console.log('strain')
+                                // console.log(strain)
                                         strainbucket.push(item)
                         }
                 })
 
-await res.json( strainbucket )
+await res.json( onestrain )
 }
 export default getSpecifiedStrain
