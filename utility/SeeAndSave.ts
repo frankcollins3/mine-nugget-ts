9638c82bc209e1f8130426168fd87a6bfe55aa9f
Frank Collins <fwc3rd@gmail.com>
2022-11-29 22:37:54 -0500

    [SeeAndSave] param.filter()  [10:37am]

--- utility/SeeAndSave.ts
+++ utility/SeeAndSave.ts
@@ -1,14 +1,55 @@
+=import Strain from "pages/strain"
+
 export default async function SeeAndSave(api, apilength, textState:string, setTextState:any ) {
     // element to change, the string-data-textState that will be the display text, setTextState which will setState for textState()
-    console.log('api element')
+    console.log('api element and length')
+    console.log('api')
     console.log(api)
 
-    let strain:string = api[0].strain
-    console.log('strain API functions')
-    console.log(strain)
+    api.filter(data => {
+        console.log('data')
+        console.log(data)
+        if (data !== 'parents') {
+            console.log('data without parents!!!! ')
+            console.log(data)
+        }
+    })    
 
-    console.log('textState')
-    console.log(textState)
+// cbd
+// "0.1%"
+// dominant
+// "balanced hybrid"
+// funfact
+// "passed genes to white rhino & white russian, growers prefer original"
+// gold
+// "euphoria, conversation, energy"
+// nugget
+// "white trichomes, frosty"
+// parents
+// "south indian indica, brazilian sativa landrace"
+// smell
+// "tropical, fruity"
+// strain
+// "white widow"
+// taste
+// "not very tasty, earthy, piney, smooth, disappointing to fruity fans"
+// "15%, 20%"
+            // filter(callbackFn, thisArg)
 
-    setTextState(strain)
+    // let strain:string = api[0].strain
+    // console.log('apilength')
+    // console.log(apilength)
+
+    // if (apilength === api.length) {
+    //     console.log('apilength is equal to each other')
+    //     console.log(apilength)
+    //     console.log(`api length ${api.length}`)
+    // }
+
+    setTextState('hey')
+
+    
+
+    
+
 }
