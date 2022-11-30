131211ca0d70631ae4aa80fb62937511eeb4f5ba
Frank Collins <fwc3rd@gmail.com>
2022-11-29 23:22:41 -0500

    keybucket [11:22pm]
    this is the Left side container that will hold the object.keys()
    
    the Right side Container and original only dispilayContainer under the <AllStrainContainer/> that container is <StrainDisplay/>
    
    now have to make the same state that applies to that conatiner and rename and reapply to this one

---
+++ components/strainDisplayvalue.tsx
@@ -0,0 +1,14 @@
+export default async function StrainDisplayValue() {
+    let columnclass = 'Column'
+    let card = 'card' 
+    let doubleCardClass = [card, columnclass].join(' ')
+    // sister container to the original underdisplay of the rendererd strains
+    // now [Left: Object.Keys(i.e. 'strain')] && [Right: Object.Values(i.e. Do-Si-Dos)]
+    // 
+
+    return (
+        <div className={doubleCardClass}>
+            
+         </div>
+    )
+}
