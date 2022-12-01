6f694d4e216a0d3dbdd0d380b483975873348bdf
Frank Collins <fwc3rd@gmail.com>
2022-11-30 23:02:14 -0500

    keys & vals in sync with filters each side [11pm]
    was going to loop through keys to find the index of the parents but don't have that object currently available when invoking the vals
     because object.values() would be in place of object.keys()
    
    was thinking about about using .slice to distinguish the parent strain data from the other endpoints with its comma,
    but other endpoints like taste can have a comma as well

--- utility/SeeAndSave.ts
+++ utility/SeeAndSave.ts
@@ -1,33 +1,73 @@
 import Strain from "pages/strain"
 let i = 0; 
 export default async function SeeAndSave(api, apilength, textState:string, setTextState:any ) {
+    // element to change, the string-data-textState that will be the display text, setTextState which will setState for textState()
+        console.log('api')
+        console.log(api)
+
     
-    let myfilteredData = api.filter(data => {        
-        if (data !== 'parents') {
-            return data                    
-        }
-    })                
+
     if (api.includes('strain') && api.includes('dominant') && api.includes ('taste')) {
+    // if (api.includes('strain' || 'parents' || 'cbd' || 'thc')) {
         try {
-            let datalength = myfilteredData.length             
+            console.log('atleast were trying')
+            let myfilteredData = api.filter(data => {        
+                if (data !== 'parents') {
+                    return data                    
+                }
+            })                
+            let datalength = myfilteredData.length            
+          
+            // if (textState === null || undefined) {
+                
                 if (textState.length < 2) {
+                    console.log('were over here in the 3')
                     await setTextState(myfilteredData[0])
                 } 
                 else if (textState.length > 2) {
+                // else if (textState.length > 6) {
                     for (let i = 0; i < myfilteredData.length; i++) {                        
+                        console.log(myfilteredData[i])
                         if (myfilteredData[i] === textState) {
-                            let newindex:string = myfilteredData[i] + 1                        
+                            let indexVar:string = myfilteredData[i + 1]                        
                             await setTextState('')
-                            await setTextState(newindex)                            
+                            await setTextState(myfilteredData[i + 1])
+                            // if(i > apilength) setTextState('y')
                         }   else if (textState === 'thc')  setTextState('')
-                    }                                       
+                    }
+                    
+                        // await setTextState(myfilteredData[i + 1])
+                    // }
+                    console.log('were in here now')
                 }
+                
+                    
+            
 
-        } catch (err) { console.log(err) // res.redirect(error)
+        } catch (err) { console.log(err) // redirect to error page.}
     }
     
 } else {
-    console.log("our api does not include that stuff")
+    console.log("that stuff isn't in the API")
+    // get the index of parents get 
+    // declare that index of api as a variable. 
+    // filter variable
+
+    console.log('api')
+    console.log(api)
+    
+    let elsefilter= api.filter(data => {        
+        if (data !== api[3]) {
+            console.log('data')
+            console.log(data)
+            // returning no the parents
+        }
+    })  
+    
+
+    // console.log('elsefilter')
+    // console.log(elsefilter)
+
 }
 
 }  
