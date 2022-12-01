0fd75e949d4a5ebc227b774510bde9b1601facb2
Frank Collins <fwc3rd@gmail.com>
2022-11-30 23:25:48 -0500

    R: [strainDisplay] props [11:25pm]

--- components/strainDisplayvalue.tsx
+++ components/strainDisplayvalue.tsx
@@ -1,14 +1,18 @@
-export default async function StrainDisplayValue() {
+import styled from 'styled-components'
+
+export default async function StrainDisplayValue(props) {
     let columnclass = 'Column'
     let card = 'card' 
     let doubleCardClass = [card, columnclass].join(' ')
     // sister container to the original underdisplay of the rendererd strains
     // now [Left: Object.Keys(i.e. 'strain')] && [Right: Object.Values(i.e. Do-Si-Dos)]
-    // 
+    console.log('props')
+    console.log(props)
+    
 
     return (
         <div className={doubleCardClass}>
-            
+            <p> right side container </p>
          </div>
     )
 }
