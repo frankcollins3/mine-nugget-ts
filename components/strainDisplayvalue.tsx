9d8e050123e53486832c3533ea9b1f7b8cc3f163
Frank Collins <fwc3rd@gmail.com>
2022-12-01 00:04:03 -0500

    [strainDisplayValue] server reconfig  [12:03am]
    overload matches call error and next.js.config

--- components/strainDisplayvalue.tsx
+++ components/strainDisplayvalue.tsx
@@ -1,26 +1,29 @@
+// @ts-nocheck
 import styled from 'styled-components'
 import CardStyle from 'styles/StrainDisplay'
 
 
-export default async function StrainDisplayValue(props) {
+export default function StrainDisplayValue(props) {
+    console.log("hello from strain display values!")
     let columnclass = 'Column'
     let card = 'card' 
     let doubleCardClass = [card, columnclass].join(' ')
     // sister container to the original underdisplay of the rendererd strains
     // now [Left: Object.Keys(i.e. 'strain')] && [Right: Object.Values(i.e. Do-Si-Dos)]
-    console.log('props')
+    console.log('props in the strain display value')
     console.log(props)
 
 
     return (
-
+        <>        
         <CardStyle        
         bgToggle={props.bgToggle} setBgToggle={props.setBgToggle}
         clickedStrain={props.clickedStrain} setClickedStrain={props.setClickedStrain}                   
         >
         <div className={doubleCardClass}>
-            <p> right side container </p>
-         </div>
+            {props.displayText}
+        </div>
         </CardStyle> 
+        </>
     )
 }
