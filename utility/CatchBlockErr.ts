d25192b0ee417a04170ca510510e79255890298b
Frank Collins <fwc3rd@gmail.com>
2022-12-02 10:58:21 -0500

    accessing err from other file instead of errobj param

--- utility/CatchBlockErr.ts
+++ utility/CatchBlockErr.ts
@@ -1,7 +1,8 @@
+// @ts-nocheck
 import Axios from 'axios'
 export default async function ERROR (errorobj:(object|string), errorState, setErrorState) {                                
-        if (errorobj) {     // also saying if (typeof errorobj === 'object || string')            
-            let errorstatus:string = err.response.status              
+        if (errorobj) {     // also saying if (typof errorobj === 'object || string')            
+            let errorstatus:string = errobj.response.status              
             return errorstatus
         }
 }
