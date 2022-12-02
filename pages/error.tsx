4b0827f7cd8dd5db8520450fc0a8185f2dceab7f
Frank Collins <fwc3rd@gmail.com>
2022-12-02 17:20:38 -0500

    barriers and dynamite instead of no digging sign. [5:20pm]
    seeing dynamite on an error page makes sense because everything blew up.
    was going to use flex for the .row layouting bootstrap instead.
    
    i also want to make a master api to access images. Ideally put into global state or redux but just moving along now.
    putting the images in a central store and cutting off redundancy of saying /public/image and declare that root within an exported utility function

--- pages/error.tsx
+++ pages/error.tsx
@@ -8,7 +8,15 @@ export default function Error () {
         // <Container>
                 <Container className="Column">
             <ErrorCont>
-              <p> error </p>
+                {/* time for an image bank! */}
+
+              <img src="/img/barrier.png"></img>
+              <img src="/img/dynamite.png"></img>
+              <img src="/img/barrier.png"></img>
+              {/* <img src="/img/pick.png"/> */}
+              {/* i was going to use a no-digging sign */}
+              {/* barrier dynamite barrier */}
+              {/* everything blew up! */}
             </ErrorCont>
                 </Container>
         //  </Container> 
