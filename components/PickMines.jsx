25d8b426078b891a4f3ee6e17fd78c35fcface16
Frank Collins <fwc3rd@gmail.com>
2022-12-01 22:00:15 -0500

    [PickMines] styled component [10:00pm]

--- components/PickMines.jsx
+++ components/PickMines.jsx
@@ -1,12 +1,26 @@
 import Container from 'react-bootstrap/Container'
 
+import styled from 'styled-components'
+import CardStyle from 'styles/StrainDisplay'
+
+let ptag = 'PickMineP' 
+let column = 'Column'
+
+
+let double = [column, ptag].join(' ')
+// let tripleclass = [column, ptag, CardStyle].join(' ')
 
 
 export default function PickMines() {
     return (
+
+
         <Container fluid="md">
         {/* <div >             */}
-        <p className="PickMineP Column Card"> test text </p>
+        <CardStyle>
+        <p className={double}> test text </p>
+
+        </CardStyle>
         {/* </div> */}
         </Container>
     )
