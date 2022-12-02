f7456574a704aefa1e347c7b88af386ad3e0b890
Frank Collins <fwc3rd@gmail.com>
2022-12-02 11:01:40 -0500

    [CatchBlock] now returns the status [11:01am]
    this now returns the error status.

--- utility/CatchBlockErr.ts
+++ utility/CatchBlockErr.ts
@@ -1,8 +1,8 @@
 // @ts-nocheck
 import Axios from 'axios'
-export default async function ERROR (errorobj:(object|string), errorState, setErrorState) {                                
+export default async function ERROR (errorobj, errorState, setErrorState) {                                
         if (errorobj) {     // also saying if (typof errorobj === 'object || string')            
-            let errorstatus:string = errobj.response.status              
+            let errorstatus:string = errorobj.response.status              
             return errorstatus
         }
 }
