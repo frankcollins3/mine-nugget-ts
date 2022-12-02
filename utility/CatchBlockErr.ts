73a1b92d1211c485bf81aef812fadab4673bbb8d
Frank Collins <fwc3rd@gmail.com>
2022-12-02 11:06:55 -0500

    setState: in util function [11:06am]
    setState declared as the 2nd parameter in this instance
    setState working to return the UI <p> {errorState} </p>
    
    state is not changed within the component that the UI page/element renders the state changed.

--- utility/CatchBlockErr.ts
+++ utility/CatchBlockErr.ts
@@ -1,8 +1,11 @@
 // @ts-nocheck
 import Axios from 'axios'
-export default async function ERROR (errorobj, errorState, setErrorState) {                                
+export default async function ERROR (errorobj, setErrorState) {                                
+// export default async function ERROR (url, errorobj, errorState, setErrorState)
+
         if (errorobj) {     // also saying if (typof errorobj === 'object || string')            
             let errorstatus:string = errorobj.response.status              
-            return errorstatus
+            setErrorState(errorstatus)
+            // return errorstatus
         }
 }
