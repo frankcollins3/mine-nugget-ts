5f6bc680111c42d086d7576a93099dd65c900bd5
Frank Collins <fwc3rd@gmail.com>
2022-12-02 10:22:37 -0500

    create test err func [10:21pm]
    AxiosError {message: 'Request failed with status code 404', name: 'AxiosError', code: 'ERR_BAD_REQUEST', config: {…}, request: XMLHttpRequest, …}
    code
    : 
    "ERR_BAD_REQUEST"
    config
    : 
    {transitional: {…}, transformRequest: Array(1), transformResponse: Array(1), timeout: 0, adapter: ƒ, …}
    message
    : 
    "Request failed with status code 404"
    name
    : 
    "AxiosError"
    request
    : 
    XMLHttpRequest {onreadystatechange: null, readyState: 4, timeout: 0, withCredentials: false, upload: XMLHttpRequestUpload, …}
    response
    : 
    {data: '<!DOCTYPE html><html><head><style data-next-hide-f…p":true,"scriptLoader":[]}\x3C/script></body></html>', status: 404, statusText: 'Not Found', headers: AxiosHeaders, config: {…}, …}
    stack
    : 
    "AxiosError: Request failed with status code 404\n    at settle (webpack-internal:///./node_modules/axios/lib/core/settle.js:24:12)\n    at XMLHttpRequest.onloadend (webpack-internal:///./node_modules/axios/lib/adapters/xhr.js:117:66)"
    [[Prototype]]\
    Error
    
    within this Axios provided error object, 
    👍 should be able to find the status of an error and use this as state for future redirecting to error pages
    
    still considering global state just moving along.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,64 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Head from "next/head";
-import React, { useState, useContext, createContext } from 'react'
-import 'bootstrap/dist/css/bootstrap.css'
-import styled from 'styled-components'
-// import { themes, ThemeContext} from '.././utility/Context'
-// import globalContext from   '.././utility/Context'
+  import '../styles/globals.css'
+  import type { AppProps } from 'next/app'
+  import Head from "next/head";
+  import React, { useState, useContext, createContext } from 'react'
+  import 'bootstrap/dist/css/bootstrap.css'
+  import styled from 'styled-components'
+  import Axios from 'axios';
+import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript';
+  // import { themes, ThemeContext} from '.././utility/Context'
+  // import globalContext from   '.././utility/Context'
+
+  const testError = async () => {
+    try {
+      // let testfetch = await Axios.get('https://pokeapi.co/api/v2/pokemon')
+      let test = await Axios.get('hi')
+      // console.log('testfetch')
+      // console.log(testfetch)
+    } 
+    catch(err) {
+      console.log('err')
+      console.log(err)
+    }
+  }
 
 
-export default function App({ Component, pageProps }: AppProps) {
-  
-  // const [theme, setTheme] = useState(themes.dark)      this is for context in the example.
-  const [url, setUrl] = useState('')    // url:string
-  const [currentStrain, setCurrentStrain ] = useState('')
-  const [allStrains, setAllStrains] = useState([])
-  const [error, setError] = useState()
+  export default function App({ Component, pageProps }: AppProps) {
     
-  // const ThemeContext = createContext(theme)
-  // globalContext()
-  // const appContext = createContext({
-  //   default
-  // })
-  
-  return (
-    <>
-      <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+    // const [theme, setTheme] = useState(themes.dark)      this is for context in the example.
+    const [url, setUrl] = useState('')    // url:string
+    const [currentStrain, setCurrentStrain ] = useState('')
+    const [allStrains, setAllStrains] = useState([])
+    const [error, setError] = useState()
+      
 
-    {/* <ThemeContext.Provider value={theme}> */}
-
-    <Component {...pageProps}
-     error={error} setError={setError}
-     currentStrain={currentStrain} setCurrentStrain={setCurrentStrain}
-     url={url} setUrl={setUrl}
-     allStrains={allStrains} setAllStrains={setAllStrains}
-     />
-   {/* </ThemeContext.Provider> */}
-   </>
+    // const ThemeContext = createContext(theme)
+    // globalContext()
+    // const appContext = createContext({
+    //   default
+    // })
     
-  )
 
-}
+    
+    return (
+      <>
+        <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+
+      {/* <ThemeContext.Provider value={theme}> */}
+      <button onClick={testError}>
+        ERROR TEST 
+      </button>
+      <Component {...pageProps}
+      error={error} setError={setError}
+      currentStrain={currentStrain} setCurrentStrain={setCurrentStrain}
+      url={url} setUrl={setUrl}
+      allStrains={allStrains} setAllStrains={setAllStrains}
+      />
+    {/* </ThemeContext.Provider> */}
+    </>
+      
+    )
+
+  }
