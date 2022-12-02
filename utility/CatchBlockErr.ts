d19cfdf620c2c626585111489613663671690166
Frank Collins <fwc3rd@gmail.com>
2022-12-02 13:28:43 -0500

    [error function] location.replace() works

--- utility/CatchBlockErr.ts
+++ utility/CatchBlockErr.ts
@@ -1,11 +1,13 @@
 // @ts-nocheck
 import Axios from 'axios'
+let url = '/error'
 export default async function ERROR (errorobj, setErrorState) {                                
 // export default async function ERROR (url, errorobj, errorState, setErrorState)
-
         if (errorobj) {     // also saying if (typof errorobj === 'object || string')            
             let errorstatus:string = errorobj.response.status              
             setErrorState(errorstatus)
-            // return errorstatus
+        location.replace(url) || location.attr('href','/error')        
+        } else {
+            return ''
         }
-}
+}       
