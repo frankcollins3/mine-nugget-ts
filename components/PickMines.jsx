70332738b5f9cdc39cdb1f6b47032d5905595b0a
Frank Collins <fwc3rd@gmail.com>
2022-12-01 21:56:24 -0500

    PickMines  [9:44pm]

--- components/PickMines.jsx
+++ components/PickMines.jsx
@@ -1,10 +1,12 @@
 import Container from 'react-bootstrap/Container'
 
+
+
 export default function PickMines() {
     return (
         <Container fluid="md">
         {/* <div >             */}
-        <p> test text </p>
+        <p className="PickMineP Column Card"> test text </p>
         {/* </div> */}
         </Container>
     )
