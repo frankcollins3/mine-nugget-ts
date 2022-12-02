0af65483ff95b992e428dc3426b449b4df779a0b
Frank Collins <fwc3rd@gmail.com>
2022-12-02 13:41:08 -0500

    this works with APP's: modular jq based attribute func [1:37pm]
    can also make a tool specifically for handling location functions like i did in the last app 
    
    but I'm going to use NextRouting and <Link/> like react router DOM for regular navbar based redirection (excluding error page invoked within catch block)

--- utility/CatchBlockErr.ts
+++ utility/CatchBlockErr.ts
@@ -1,13 +1,19 @@
 // @ts-nocheck
 import Axios from 'axios'
+import $ from 'jquery'
+import AttrTool from 'utility/JqAttr'
 let url = '/error'
+
+
 export default async function ERROR (errorobj, setErrorState) {                                
 // export default async function ERROR (url, errorobj, errorState, setErrorState)
         if (errorobj) {     // also saying if (typof errorobj === 'object || string')            
             let errorstatus:string = errorobj.response.status              
             setErrorState(errorstatus)
         location.replace(url) || location.attr('href','/error')        
+        // location.replace(url) || location.attr('href','/error')        
+        AttrTool(location, href, url)
         } else {
             return ''
         }
-}       
+}
