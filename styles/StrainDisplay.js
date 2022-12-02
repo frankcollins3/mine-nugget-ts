facddf7fd12835ffcf143b42267943a4b610a250
Frank Collins <fwc3rd@gmail.com>
2022-12-02 08:59:22 -0500

    [strainDisplay] restore  [8:58am]
    thought you could possibly invoke expressionability within the 
    
    opacity: ${props => {
     props.clickedStrain === undefined ? "0.1" : "1.0"};    
    $(' ') cant use jquery in here removing unneeded modules
    }
    
    tried going through the props to check for an error that didn't exist (white text on white container)

--- styles/StrainDisplay.js
+++ styles/StrainDisplay.js
@@ -1,42 +1,20 @@
 import styled from 'styled-components';
-import $ from 'jquery'
-
-
 
 const Display = styled.div`        
         opacity: ${props => props.clickedStrain === undefined ? "0.1" : "1.0"};       
         color: ${props => props.bgToggle === 'new' ? 'black' : 'white'};        
-        // backgroundColor: ${props => props.bgToggle === 'old' ? 'transparent' : 'blue'};
-        // backgroundColor: props.bgToggle === 'old' ? 'transparent' : 'blue', 
-
         display: flex;
         flex-flow: column wrap;
         justify-content: center;
         align-items: center;
         margin-top: 2em;
-        
         min-height: 8vh;
-        min-width: 30vw;
-        
+        min-width: 30vw;        
         `        
-        
-        const CardStyle = styled(Display)`
-        // background-color: ${props => props.bgToggle === 'new' ? "rgb(62, 50, 32)" : "transparent"};       
-        // background-color: ${props => props.bgToggle === 'new' ? "red" : "transparent"};       
+
+const CardStyle = styled(Display)`        
         box-shadow: 10px 10px 10px rgb(247, 208, 32);    
-        border: 3px solid rgb(247, 208, 36);
-    
-        
-        
-        
-     
+        border: 3px solid rgb(247, 208, 36);    
 `
-
-
-
-
-
-
-
 export default CardStyle
 // export default [Display, CardStyle]
