b7bd2a164ac5daed40b0149fa12eddb5040748a1
Frank Collins <fwc3rd@gmail.com>
2022-12-02 16:55:17 -0500

    [error] import bootstrap [4:54pm]
    i didn't have the:
    /cointainer endpoint set onto the [import from 'react-bootstrap'] API

---
+++ pages/error.tsx
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container';
+// import Card from 'react-bootstrap'
+
+export default function Error () {
+    return ( 
+        
+        <Container>
+              <p> error </p>
+         </Container> 
+        
+        
+    )
+}
