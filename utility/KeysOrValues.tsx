aa0ce13b6dc562a9a924679457b935221fe06ae0
Frank Collins <fwc3rd@gmail.com>
2022-12-03 13:24:06 -0500

    modular expression for returning object endpoints [1:23pm]

---
+++ utility/KeysOrValues.tsx
@@ -0,0 +1,11 @@
+
+export default async function ReturnEndpoints(object:object, method:string) {
+    // if (typeof object === 'object') 
+    let strongstring:string = method.toLowerCase()  // VALUES will work like values
+    console.log('strongstring')
+    console.log(strongstring)
+
+    if (method === 'keys') return Object.keys(object)
+    if (method === 'values') return Object.keys(object)
+
+}
