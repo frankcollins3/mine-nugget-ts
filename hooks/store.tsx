a8c2ff0106c383af832c7ab41806bd791c8a1506
Frank Collins <fwc3rd@gmail.com>
2022-12-03 14:41:37 -0500

    set initial state as state

--- hooks/store.tsx
+++ hooks/store.tsx
@@ -1,20 +1,5 @@
 import {useEffect, useState, useContext, createContext, useMemo} from 'react'
 
-// * index.tsx
-//   const [ pokemon, setPokemon ] = useState('')
-//   const [currentStrain, setCurrentStrain] = useState('')
-//   const [savedStrains, setSavedStrains] = useState('')
-//   const [users, setUsers] = useState([])
-//   const [dbStrains, setDbStrains] = useState([])
-// * strain.tsx
-// const [clickedStrain, setClickedStrain] = useState()
-// const [bgToggle, setBgToggle] = useState('new')
-// const [textState, setTextState] = useState('')
-// const [displayText, setDisplayText] = useState('')
-// * AllStrainContainer
-// const [styleFile, setStyleFile] = useState('')
-// const [nothing, setNothing] = useState()
-// const [apiLen, setApiLen] = useState(0)
 
 let initialstate = {
     pokemon: '',
@@ -34,23 +19,45 @@ let initialstate = {
 
 
 
-export default async function StateStore() {
+export default async function StateStore({children}) {
     // Make a context for the store
-    const context = createContext(null as any);
-  
-    // initialValue provider
-    const Provider = ({ initialValue = {}, children }) => {
-    //   state
-      const [state, setState] = useState(initialValue);
-  
-      // cache/memo for
-      const contextValue = useMemo(() => [state, setState], [state]);
-  
-      // Provide the store to children
-      return <context.Provider value={contextValue}>{children}</context.Provider>;
-    };
+    const Context = createContext(null as any);
+    const [state, setState] = useState(initialstate)
 
+    return (
+        <Context.Provider value={[state, setState]}>{children}</Context.Provider>
+
+    )
+
+}
+
+
+// * index.tsx
+//   const [ pokemon, setPokemon ] = useState('')
+//   const [currentStrain, setCurrentStrain] = useState('')
+//   const [savedStrains, setSavedStrains] = useState('')
+//   const [users, setUsers] = useState([])
+//   const [dbStrains, setDbStrains] = useState([])
+// * strain.tsx
+// const [clickedStrain, setClickedStrain] = useState()
+// const [bgToggle, setBgToggle] = useState('new')
+// const [textState, setTextState] = useState('')
+// const [displayText, setDisplayText] = useState('')
+// * AllStrainContainer
+// const [styleFile, setStyleFile] = useState('')
+// const [nothing, setNothing] = useState()
+// const [apiLen, setApiLen] = useState(0)
+
+// initialValue provider
+    // const Provider = ({ initialValue = {}, children }) => {
+    //   state
+    //   const [state, setState] = useState(initialValue);
+      // cache/memo for
+    //   const contextValue = useMemo(() => [state, setState], [state]);
+      // Provide the store to children
+    //   return <context.Provider value={contextValue}>{children}</context.Provider>;
+    // };
     // A hook to help consume the store
-    const useStore = () => useContext(context);
-    return { Provider, useStore };
-  }
+    // const useStore = () => useContext(context);
+    // return { Provider, useStore };
+//   }
