9a0f3281988e2fa853891436d867f40a2a0da461
Frank Collins <fwc3rd@gmail.com>
2022-12-03 14:55:23 -0500

    swap the location of global-state declaration from hook file to app[2:55pm]
    store.tsx?d661:18 Uncaught (in promise) TypeError: Cannot destructure property 'children' of 'param' as it is undefined.
             redeclare initial state, which is all of the [const state:values === value] all the state variables across the app declared in object without setState 
    
    work-around to that above-stated-error because the function is invoked with no params. the function is:
     {invoked: app.tsx}              &&            {hook/function and initialstate-object declared:  hooks/store}

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,12 +8,31 @@ import '../styles/globals.css'
   import Axios from 'axios';
   import ERROR from 'utility/CatchBlockErr'
   import Link from 'next/link'
+  import StateStore from 'hooks/store'
+  console.log('StateStore')
+  console.log(StateStore)
 
-  console.log('Link from the home page')
-  console.log(Link)
+  let initialstate = {
+    // pokemon:string: '',  cant
+    pokemon: '',
+    currentStrain: '',
+    savedStrains: '',
+    users: [],
+    dbStrains: [],
+    clickedStrain: undefined || null,
+    bgToggle: 'new',
+    textState: '',
+    displayText: '',
+    styleFile: '',
+    nothing: '',
+    apiLen: 0
+}
 
-  // import { themes, ThemeContext} from '.././utility/Context'
-  // import globalContext from   '.././utility/Context'
+  
+
+
+
+
 
   export default function App({ Component, pageProps }: AppProps) {
     
@@ -25,17 +44,24 @@ import '../styles/globals.css'
     const [errAcknowledge, setErrAcknowledge] = useState('')
       
     const testError = async () => {
-      let url = `https://pokeapi.co/api/v2/pokemon/`
-      let url2 = 'hi'
+      // test store of state that it returns the values declared across the app that i manually stored as obj
+      
+      // let url = `https://pokeapi.co/api/v2/pokemon/`
+      console.log("hey whats up!")
+      let globalstate = await StateStore()
+      console.log(StateStore)
+      console.log('globalstate')
+      console.log(globalstate)
+      // let url2 = 'hi'
       try {
         // let testfetch = await Axios.get('https://pokeapi.co/api/v2/pokemon')        
-        let test = await Axios.get('hi')
+        // let test = await Axios.get('hi')
         // console.log('testfetch')
         // console.log(testfetch)
       } 
       catch(err) {              
-        const testError:string = await ERROR(err, setError)
-        setError(testError)
+      //   const testError:string = await ERROR(err, setError)
+      //   setError(testError)
       }
 
     }
@@ -56,8 +82,8 @@ import '../styles/globals.css'
       <button onClick={testError}>
         ERROR TEST 
       </button>
-      
       <p> {error || 'no error'} </p>
+      
       <Component {...pageProps}
       error={error} setError={setError}
       currentStrain={currentStrain} setCurrentStrain={setCurrentStrain}
