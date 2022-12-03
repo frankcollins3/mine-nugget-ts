9e97b5901e75c63029a3482d8ddc65b01b2a0cd3
Frank Collins <fwc3rd@gmail.com>
2022-12-03 11:33:39 -0500

    globalState.config() declare null context as any [11:33am]
    context.Provider Type '{}[]' is not assignable to type 'null'.ts(2322)

--- hooks/store.tsx
+++ hooks/store.tsx
@@ -1,15 +1,18 @@
 import {useEffect, useState, createContext, useMemo} from 'react'
 export default async function StateStore () {
     const [state, setState] = useState('')
-    const context = createContext(null) // a default value has to be provided   
+    const context = createContext(null as any) // a default value has to be provided   
 
     // initialValue provider:
     const Provider = ({ initialValue = {}, children}) => {
         // create state 
         const [state, setState] = useState(initialValue)
 
-        // memoize with hook
+        // memoize 
         const contextvalue = useMemo( () => [state, setState], [state] )
+        
+        // return <context.Provider value={contextvalue}> {children} </context.Provider>;
+        return <context.Provider value={contextvalue}> {children} </context.Provider>;
 
     }
 }
