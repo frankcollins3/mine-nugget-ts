19d5e139312a19378d2f1e2797d786500e9cf365
Frank Collins <fwc3rd@gmail.com>
2022-12-03 16:30:01 -0500

    [app] importing and wrapping global context correctly [4:30pm]
    just under 4 hours!
    1)  import StateStore from 'hooks/store'     
    2) <StateStore> 
    
     i was invoking this function instead and using an explicitly declared test-object that stored all of the APP's (handwritten) state  into its keys
    the {children} in the hook will be the properties of the children that are enclosed by:
    
    <StoreState> 
                              <child-component declared as {children} in the StoreState params> 
    </StoreState>
    
    did not know you could do that and was trying to do this from the documentation without tutorial assistance.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,9 +9,9 @@ import '../styles/globals.css'
   import ERROR from 'utility/CatchBlockErr'
   import Link from 'next/link'
 
-  // import StateStore from 'hooks/store'
-  // console.log('StateStore')
-  // console.log(StateStore)
+  import StateStore from 'hooks/store'
+  // import StateStore from 'components/store'
+  
   
   export default function App({ Component, pageProps }: AppProps) {
     
@@ -31,64 +31,41 @@ import '../styles/globals.css'
       apiLen: 0
   }
 
-  let fakestate = {
-    state: 'fake'
-  }
+  
   
   // const [theme, setTheme] = useState(themes.dark)      this is for context in the example.
   const [state, setState] = useState()
+  let globalshorthand = [state, setState]
   const [url, setUrl] = useState('')    // url:string
   const [currentStrain, setCurrentStrain ] = useState('')
   const [allStrains, setAllStrains] = useState([])
   const [error, setError] = useState()
   const [errAcknowledge, setErrAcknowledge] = useState('')
 
-  const StateStore = (children) => {
-    // console.log('children')
-    // console.log(children)    
-       const Context = createContext(null as any);
-    // const [state, setState] = useState()
-    // export default function StateStore({children}) {        
-      return (
-        <Context.Provider value={[state, setState]}>{children}</Context.Provider> || {my: 'life'}
-        // {children} || {my: 'life'}
-        // <Context.Provider value={[state, setState]}>{}</Context.Provider> || {my: 'life'}
-    )
-  }
+      //  const Context = createContext(null as any);    
+      // <Context.Provider value={ 'he4y'  }> {initialstate} </Context.Provider>       
+
+  // const StateStore = (children) => {    
+  //      const Context = createContext(null as any);    
+  //      return (
+  //         <Context.Provider value={[state, setState]}> {initialstate}</Context.Provider> || {my: 'life'}        
+  //      )
+  // }
+    
+    
+  
         
     const testError = async () => {
-      // test store of state that it returns the values declared across the app that i manually stored as obj
-      
-      // let url = `https://pokeapi.co/api/v2/pokemon/`
-      
-      // let globalstate = await StoreState()
-      // console.log('globalstate')
-      // console.log(globalstate)
-      console.log('initialstate')
-      console.log(initialstate)
-      let globalstate = await StateStore(initialstate)      
-      console.log('globalstate')
-      console.log(globalstate)
-
-      // let url2 = 'hi'
+      let url = `https://pokeapi.co/api/v2/pokemon/`
+      let url2 = 'hi'          
       try {
         // let testfetch = await Axios.get('https://pokeapi.co/api/v2/pokemon')        
-        // let test = await Axios.get('hi')
-        // console.log('testfetch')
-        // console.log(testfetch)
+        let test = await Axios.get(url2)       
       } 
       catch(err) {              
-        // const testError:string = await ERROR(err, setError)
-      //   setError(testError)
+        const testError:string = await ERROR(err, setError)      
       }
-
     }
-
-    // const ThemeContext = createContext(theme)
-    // globalContext()
-    // const appContext = createContext({
-    //   default
-    // })
 
     return (
       <>
@@ -96,19 +73,23 @@ import '../styles/globals.css'
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
 
-      {/* <ThemeContext.Provider value={theme}> */}
+      
       <button onClick={testError}>
         ERROR TEST 
       </button>
       <p> {error || 'no error'} </p>
       
+      {/* <Context.Provider>         */}
+      <StateStore>
       <Component {...pageProps}
       error={error} setError={setError}
       currentStrain={currentStrain} setCurrentStrain={setCurrentStrain}
       url={url} setUrl={setUrl}
       allStrains={allStrains} setAllStrains={setAllStrains}
       />
-    {/* </ThemeContext.Provider> */}
+      </StateStore>      
+      {/* </Context.Provider> */}
+    
     </>
       
     )
