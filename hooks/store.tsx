ee8643aaf0035c1642396c1d992c2bb83cdf4764
Frank Collins <fwc3rd@gmail.com>
2022-12-03 10:50:06 -0500

    function to provide initialValue & create State [10:49am]

--- hooks/store.tsx
+++ hooks/store.tsx
@@ -2,4 +2,12 @@ import {useEffect, useState, createContext} from 'react'
 export default async function StateStore () {
     const [state, setState] = useState('')
     const context = createContext(null) // a default value has to be provided   
+
+    // initialValue provider:
+    const Provider = ({ initialValue = {}, children}) => {
+        // create state 
+        const [state, setState] = useState(initialValue)
+
+        
+    }
 }
