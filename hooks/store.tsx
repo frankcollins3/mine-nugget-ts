e1b98595ebe562fee606ee3fee3b6316dc2e017b
Frank Collins <fwc3rd@gmail.com>
2022-12-03 14:27:34 -0500

    [store] organize all state from all components [2:25pm]
    app is coming along wanted to finish as is.
    want to get some better practices for managing state.
    
    family-tree:
    where the endpoint-data of strain.parents is kept from view through the whole app, besides this game where that very data is provided
    as a way of creating a guessing game of picking strains
    
    time to learn  global state interfearlessly from the rest of the app

--- hooks/store.tsx
+++ hooks/store.tsx
@@ -1,25 +1,43 @@
 import {useEffect, useState, useContext, createContext, useMemo} from 'react'
-export default async function StateStore () {
-    const [state, setState] = useState('')
-    const context = createContext(null as any) // a default value has to be provided   
 
-    // initialValue provider:
-    const Provider = ({ initialValue = {}, children}) => {
-        // create state 
-        const [state, setState] = useState(initialValue)
+// * index.tsx
+//   const [ pokemon, setPokemon ] = useState('')
+//   const [currentStrain, setCurrentStrain] = useState('')
+//   const [savedStrains, setSavedStrains] = useState('')
+//   const [users, setUsers] = useState([])
+//   const [dbStrains, setDbStrains] = useState([])
 
-        // memoize 
-        const contextvalue = useMemo( () => [state, setState], [state] )
-        
-        // return <context.Provider value={contextvalue}> {children} </context.Provider>;
-        return <context.Provider value={contextvalue}> {children} </context.Provider>;
-        }
+// * strain.tsx
+// const [clickedStrain, setClickedStrain] = useState()
+// const [bgToggle, setBgToggle] = useState('new')
+// const [textState, setTextState] = useState('')
+// const [displayText, setDisplayText] = useState('')
 
-        // this hook consumes store:
-        const useStore = useContext(context)
-        console.log('useStore')       
-        console.log(useStore)       
-        return {Provider, useStore}
-        // was returning Provider within the Provider at first.
+// * AllStrainContainer
+// const [styleFile, setStyleFile] = useState('')
+// const [nothing, setNothing] = useState()
+// const [apiLen, setApiLen] = useState(0)
 
-}
+
+
+
+export default async function StateStore() {
+    // Make a context for the store
+    const context = createContext(null as any);
+  
+    // initialValue provider
+    const Provider = ({ initialValue = {}, children }) => {
+    //   state
+      const [state, setState] = useState(initialValue);
+  
+      // cache/memo for
+      const contextValue = useMemo(() => [state, setState], [state]);
+  
+      // Provide the store to children
+      return <context.Provider value={contextValue}>{children}</context.Provider>;
+    };
+
+    // A hook to help consume the store
+    const useStore = () => useContext(context);
+    return { Provider, useStore };
+  }
