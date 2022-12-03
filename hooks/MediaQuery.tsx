454240b1508d7deb5be26a9643263edd73373e8d
Frank Collins <fwc3rd@gmail.com>
2022-12-02 23:47:08 -0500

    reusable resize observer hook [11:46pm]

--- hooks/MediaQuery.tsx
+++ hooks/MediaQuery.tsx
@@ -1,13 +1,26 @@
-import { useState, useEffect} from 'react'
+import { useState, useEffect, useLayoutEffect} from 'react'
+import useResizeObserver from "@react-hook/resize-observer";
 
-    const BreakPointHook = (widthparam:string) => {
-        const [matches, setMatches] = useState('')
 
-        useEffect( () => {
-            const media = window.matchMedia(widthparam)
-            console.log('media')
-            console.log(media)
-        }, [matches])
+function useSize(target) {
+  const [size, setSize] = useState([])
 
-    }
+  useLayoutEffect(() => {
+    target && setSize(target.getBoundingClientRect());
+  }, [target]);
+
+   // magic
+  useResizeObserver(target, (entry) => setSize(entry.contentRect));
+  return size;
+}
+
+
+    // const BreakPointHook = (widthparam:string) => {
+    //     const [matches, setMatches] = useState('')
+    //     useEffect( () => {
+    //         const media = window.matchMedia(widthparam)
+    //         console.log('media')
+    //         console.log(media)
+    //     }, [matches])
+    // }
 
