437ce1955b8af3ec90529003c2e083bb95ab2555
Frank Collins <fwc3rd@gmail.com>
2022-12-02 23:20:47 -0500

    [error] create array and return value   [11:20pm]
    (3) ['800px', '400px', '1200px']         // heres the array 
    Randomizer.ts?6cf0:8 3
    error.tsx?7d40:15 randomSize
    error.tsx?7d40:16 400px                // heres the value retrieved randomly
          
    imported module Randomizer pure-funct: accepts [* from array as param] from which to returns [ await value.Random() ]

--- pages/error.tsx
+++ pages/error.tsx
@@ -2,29 +2,47 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import ErrorCont from 'styles/Error'
-// import Card from 'react-bootstrap'
+import styles from 'styles/Misc.module.sass'
+import Random from 'utility/Randomizer'
 
-export default function Error () {
+
+export default function Error () {    
+    const changeSize = async () => {
+        // window.addEventListener("resize", '1000px');
+        let screenSizeBucket:(string)[] = ['800px', '400px', '1200px']
+        let randomSize:string =  await Random(screenSizeBucket)
+        // import Card from 'react-bootstrap'
+        console.log('randomSize')
+        console.log(randomSize)
+    }
     return ( 
         
         // <Container>
-                <Container className="Column">
+            <Container className="Column">
+                {/* <p> {media.matches } </p> */}
             <ErrorCont>
                 {/* time for an image bank! */}
               <Row>
               <Col>
-              <img src="/img/barrier.png"></img>
+              <img 
+              className={styles.StrainImage}
+              src="/img/barrier.png"></img>
+              </Col>
+              <Col>              
+              <img
+               className={styles.StrainImage}
+               src="/img/dynamite.png"></img>
               </Col>
               <Col>
-              <img src="/img/dynamite.png"></img>
-              </Col>
-              <Col>
-              <img src="/img/barrier.png"></img>
+              <img
+               className={styles.StrainImage}
+               src="/img/barrier.png"></img>
               </Col>
               </Row>
               {/* <img src="/img/pick.png"/> */}
                           
             </ErrorCont>
+            <button onClick={changeSize}></button>
                 </Container>
         //  </Container> 
         
