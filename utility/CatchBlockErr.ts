a96430ac16295d480d344e7eae38989d9baa7096
Frank Collins <fwc3rd@gmail.com>
2022-12-02 22:31:18 -0500

    throw err [10:31pm]
    throw error creates the msg that shows up on browser and in dev tools but not on iterm.
    answers a question about making a framework / bootstrap-spinoff

--- utility/CatchBlockErr.ts
+++ utility/CatchBlockErr.ts
@@ -8,11 +8,18 @@ let url = '/error'
 export default async function ERROR (errorobj, setErrorState) {                                
 // export default async function ERROR (url, errorobj, errorState, setErrorState)
         if (errorobj) {     // also saying if (typof errorobj === 'object || string')            
-            let errorstatus:string = errorobj.response.status              
-            setErrorState(errorstatus)
-        location.replace(url) || location.attr('href','/error')        
+            // function throwErr(): never {
+                ( () => {
+                    console.log("hey atleast were in here")
+                    throw new Error('Picked The Wrong One!');
+                })()
+
+        //     let errorstatus:string = errorobj.response.status              
+        //     setErrorState(errorstatus)
         // location.replace(url) || location.attr('href','/error')        
-        AttrTool(location, href, url)
+        // location.replace(url) || location.attr('href','/error')        
+        // AttrTool(location, href, url)
+
         } else {
             return ''
         }
