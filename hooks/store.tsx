e1d83808f4da60790038c67e0e4408174cf3ba0c
Frank Collins <fwc3rd@gmail.com>
2022-12-03 10:49:31 -0500

    [store] context for global state [10:45pm]
    import invoke and declare defaults ( createContext() )

--- hooks/store.tsx
+++ hooks/store.tsx
@@ -1,5 +1,5 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useState, createContext} from 'react'
 export default async function StateStore () {
     const [state, setState] = useState('')
-    
+    const context = createContext(null) // a default value has to be provided   
 }
