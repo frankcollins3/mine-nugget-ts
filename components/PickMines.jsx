468b81c5359a8d19ab72e4f87ce0491ac8816498
Frank Collins <fwc3rd@gmail.com>
2022-12-05 02:10:34 -0500

    save globalState var as local state [2:10am]

--- components/PickMines.jsx
+++ components/PickMines.jsx
@@ -1,79 +1,80 @@
-import Container from 'react-bootstrap/Container'
+    import Container from 'react-bootstrap/Container'
 
-import styled from 'styled-components'
-import CardStyle from 'styles/StrainDisplay'
-import MineCont from 'styles/PickMines'
-import {useEffect, useState} from 'react'
-
-
-
-let ptag = 'PickMineP' 
-let column = 'Column'
+    import styled from 'styled-components'
+    import CardStyle from 'styles/StrainDisplay'
+    import MineCont from 'styles/PickMines'
+    import {useEffect, useState} from 'react'
 
 
-let double = [column, ptag].join(' ')
-// let tripleclass = [column, ptag, CardStyle].join(' ')
+
+    let ptag = 'PickMineP' 
+    let column = 'Column'
 
 
-export default function PickMines(props) {
-
-    console.log('props in pickmines')
-    console.log(props)
+    let double = [column, ptag].join(' ')
+    // let tripleclass = [column, ptag, CardStyle].join(' ')
 
 
-    let globalstate = props.global
-    let clickedStrain = props.global.clickedStrain
+    export default function PickMines(props) {
 
-    console.log('clickedStrain')
-    console.log(clickedStrain)
-    
-    const [global, setGlobal] = useState()
-    const [save, setSave] = useState(false)
-
-    useEffect( () => {
-        setGlobal(globalstate)
-    }, [])    
-
-    // let globalstate:(string|object|number)  = props.global
+        console.log('props in pickmines')
+        console.log(props)
 
 
-    return (
+        let globalstate = props.global
+        let clickedStrain = props.global.clickedStrain
 
-        <Container
-         style={ {
-             display: 'flex',
-             flexDirection: 'column',
-             justifyContent: 'center',
-             alignItems: 'center',
-             height: '30%',
-             width: '50%'
-         }}
-        >
+        console.log('clickedStrain')
+        console.log(clickedStrain)
+        
+        const [global, setGlobal] = useState()
+        const [save, setSave] = useState(false)
+        const [savedStrain, setSavedStrain] = useState('')
+
+        useEffect( () => {
+            setGlobal(globalstate)
+        }, [])    
+
+        // let globalstate:(string|object|number)  = props.global
+
+
+        return (
+
+            <Container
+            style={ {
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '30%',
+                width: '50%'
+            }}
+            >
+                    
+            {/* <p className={double}> test text </p>  */}
+            <MineCont
+            className={double}        
                 
-         {/* <p className={double}> test text </p>  */}
-         <MineCont
-        className={double}        
-            
-         >
-         <img  
-        onMouseEnter={() => console.log(globalstate)}
-        style={ { height: '5em', width: '7em'}}
-        src="/img/pick.png"/>
+            >
+            <img  
+            onMouseEnter={() => setSavedStrain(props.global.clickedStrain)}
+            style={ { height: '5em', width: '7em'}}
+            src="/img/pick.png"/>
 
-        {save === false ?
-        <h3
-        style={ { 
-            color: 'white',
-            textShadow: '35px 25px 55px white',
-            boxShadow: '35px 25px 55px orange',
-            fontWeight: 'bold',
-        }}
-        > save {props.global.clickedStrain || ''} to the mines?</h3>
-        :
-        <p> ''</p>
-        }
-         </MineCont>
-         </Container>
+            {save === false ?
+            <h3
+            style={ { 
+                color: 'white',
+                textShadow: '35px 25px 55px white',
+                boxShadow: '35px 25px 55px orange',
+                fontWeight: 'bold',
+            }}
+            > save {props.global.clickedStrain || ''} to the mines?</h3>
+            :
+            <p> ''</p>
+            }
+            </MineCont>
+            </Container>
 
-    )
-}
+        )
+    }
