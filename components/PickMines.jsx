b802d1c784817d697a1bf6152f51cd49756fbdf3
Frank Collins <fwc3rd@gmail.com>
2022-12-05 00:53:31 -0500

    state & wrap render in ternary-comparison [12:52am]
    to render nothing upon the falsy block condition being met
    which is based on state
    that will be toggled on the image being clicked confirming saving to the database

--- components/PickMines.jsx
+++ components/PickMines.jsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container'
 import styled from 'styled-components'
 import CardStyle from 'styles/StrainDisplay'
 import MineCont from 'styles/PickMines'
+import {useEffect, useState} from 'react'
 
 
 
@@ -15,10 +16,24 @@ let double = [column, ptag].join(' ')
 
 
 export default function PickMines(props) {
-    console.log('props')
+
+    console.log('props in pickmines')
     console.log(props)
 
+
     let globalstate = props.global
+    let clickedStrain = props.global.clickedStrain
+
+    console.log('clickedStrain')
+    console.log(clickedStrain)
+    
+    const [global, setGlobal] = useState()
+    const [save, setSave] = useState(false)
+
+    useEffect( () => {
+        setGlobal(globalstate)
+    }, [])    
+
     // let globalstate:(string|object|number)  = props.global
 
 
@@ -31,7 +46,7 @@ export default function PickMines(props) {
              justifyContent: 'center',
              alignItems: 'center',
              height: '30%',
-             width: '30%'
+             width: '50%'
          }}
         >
                 
@@ -41,8 +56,22 @@ export default function PickMines(props) {
             
          >
          <img  
-        style={ { height: '5em', width: '5em'}}
+        onMouseEnter={() => console.log(globalstate)}
+        style={ { height: '5em', width: '7em'}}
         src="/img/pick.png"/>
+
+        {save === false ?
+        <h3
+        style={ { 
+            color: 'white',
+            textShadow: '35px 25px 55px white',
+            boxShadow: '35px 25px 55px orange',
+            fontWeight: 'bold',
+        }}
+        > save {props.global.clickedStrain || ''} to the mines?</h3>
+        :
+        <p> ''</p>
+        }
          </MineCont>
          </Container>
 
