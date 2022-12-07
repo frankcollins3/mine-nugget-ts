37cfef6e9e794455990bf4b78b03a4eb797fa13e
Frank Collins <fwc3rd@gmail.com>
2022-12-06 20:25:49 -0500

    constructor class now parses 3rd arg which is data [8:25pm]
    this data is to be used during axios/axios/fetch calls.
    In this instance the data would have the userId (haven't created login yet) and the strainId of the savedStrain which we can test shortly

--- utility/DataCallJS.js
+++ utility/DataCallJS.js
@@ -1,93 +1,78 @@
 import $ from 'jquery'
 import Axios from 'axios'
 
-// export default async function DataCall ( method:string, url:string, data:(string|null)) { 
-//     const ajaxCall = async () => {
-//         if (method === 'ajax') {
-//             let ajax = $.ajax({
-//                 method: 'get',
-//                 url: url,
-//                 data: 'json',
-//                 success: (ajaxdata) => {
-//                     console.log('ajaxdata')
-//                     console.log(ajaxdata)
-//                     return ajaxdata
-//                 }
-//             })
-//             return ajax
-//         }
-//     }
-//         return ajaxCall()
-
-//         if (method === 'axios') {
-
-//         }    
-
-//         }
-
 export default async function DataCall (method, url, data) {
-    console.log('url')
+// export default async function DataCall (method:string, url:string, data:(string|object|number)) {
+    console.log('url in the datacall')
     console.log(url)
 
     class Call {
-
         constructor(url) {        
             this.url = url
         }      
-    // method getters
-    get ajax() {
-        console.log("we are over here")
-        return this.ajaxcall()
-    }
-    
-    get axios() {
-        return this.axioscall()
-    }
-
-    get fetchit() {
-        return this.fetchcall()
-    }
-
-    // methods 
-    async ajaxcall() {            
+        // method getters
+        get ajax() {
+            return this.ajaxcall()
+        }
+        
+        get axios() {
+            return this.axioscall()
+        }
+        
+        get fetchit() {
+            return this.fetchcall()
+        }
+        
+        // methods 
+        async ajaxcall() {                    
             // let hi = {hey: 'hi'} this was a minimum-viable test that steered things correctly
             let datacall = await $.ajax({
                 method: 'get',
                 url: url,
                 data: 'json'
             })            
-            let actualdata = datacall.getdata            
+            let actualdata = datacall.getdata                 
             return actualdata    
     }
 
     async axioscall() {
-        let datacall = await Axios.get(url)
-        console.log('datacall in the axios')
-        console.log(datacall)
-        let data = datacall.data
+        let datacall = await Axios.get(url)        
+        let data = datacall.data        
         return data
     }
 
     async fetchcall() {
         let predata = await fetch(url)
         let actualdata = await predata.json()
-        return actualdata
-        
-
+        return actualdata    
     }
-
 }   // ajax call ending 
 
-        if (method === 'ajax') {
-            console.log("we are over here doing it this way")
-            const returnajaxcall = await new Call(url).ajax
-            return returnajaxcall                        
-        } else if (method === 'axios') {
-            const returnaxiosdata = await new Call(url).axios
-            return returnaxiosdata
+        if (method === 'ajax') {            
+            const returnajaxcall = await new Call(url).ajax            
+            if (data === null) {
+                return returnajaxcall                   
+            } else if (data !== null) {                
+                let dataobject = {returndata: returnajaxcall, data: data}
+                return dataobject
+            }                     
+            
+        } else if (method === 'axios') {            
+            const returnaxiosdata = await new Call(url).axios            
+            if (data === null) {
+                return returnaxiosdata
+            } else if (data !== null) {                
+                let dataobject = {returndata: returnaxiosdata, data: data}
+                return dataobject
+            }
         } else if (method === 'fetch') {
             const fetchdata = await new Call(url).fetchit
-            return fetchdata
+            if (data === null) {
+                return fetchdata                
+            } else if (data !== null) {                
+                let dataobject = {returndata: fetchdata, data: data}
+                return dataobject
+            }                    
             // should be good with else { } but just to be stricter  using else if (stated expression) 
 
         }
