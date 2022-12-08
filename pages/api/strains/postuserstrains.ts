ce628199c01aa173f95061f157e710c8005ebcd2
Frank Collins <fwc3rd@gmail.com>
2022-12-08 04:59:07 -0500

    [white space it is. data fixed] [4:58pm]
    making matches. definitely a thing for future API creating to have conventions in place that anticipate the need for regexp
    
    👎  strain.strain:
    Do-Si-Dos
    GorillaGlue#4 
    few problems can see where white spacing and non alphanumeric characters ('-' && '#') come into need for parsing

--- pages/api/strains/postuserstrains.ts
+++ pages/api/strains/postuserstrains.ts
@@ -1,18 +1,85 @@
+import MasterRegex from 'utility/MasterRegex'
+import { PrismaClient } from '@prisma/client';
+import Regex from 'utility/MasterRegex'
+import APIcall from 'utility/APIcall'
+const prisma = new PrismaClient()
+
 export default async function (req, res) {
     // export default async function (req:(string|object|number), res:(string|object|number)) {
-        let body:string = req.body
-        console.log('body')
-        console.log(body)
-        console.log(typeof body)
-        let mykeys = Object.keys(body)
-        // can make the userId and strainId that get sent here a string and use a regex to pull chars before numb
-        console.log('mykeys')
-        console.log(mykeys)
-        // let newbody = body.slice(5, 20)
-        // console.log('newbody')
-        // console.log(newbody)
-    
-        // if (typeof req === object) {            
+        // let body:string = req.body
+        
+        // console.log(req.body)
+        // conosle.log(JSON.stringify())
+        // let parse = JSON.parse(JSON.stringify(req.body))
+        // let parse = JSON.parse(req.body) 
+        console.log("body: %j", req.body)
+
+        let data = req.body.dataname
+        let realdata = await Regex(data, 'alphareturn')
+        console.log('data')
+        console.log(data)
+        console.log('realdata')
+        console.log(realdata)
+        // this works just make conditional logic
+
+        let strains = await prisma.strains.findMany()
+        let apistrains = await APIcall('all', null, null)
+
+        apistrains.forEach( (strain) => {
+
+            if (strain.strain === realdata) {
+                console.log('strain finally!')
+                console.log(strain)
+            } else if (realdata === 'GorillaGlue') {
+                console.log('strain passed the condition')
+                if (strain.strain === 'GorillaGlue#4') {
+                    console.log('strain condition')
+                    console.log(strain)
+                }
+            }
+        })
+
+        // strains.forEach( (strain) => {
+        //     if (strain.strain === realdata) {
+        //         console.log('strain in the DB!')
+        //         console.log(strian.taste)
+        //         console.log(strain.funfact)
+        //     }
+        // })
+        // let dbstrainlist = await prisma.strains.findMany()
+
+        // const result = await prisma.strains.findUnique({
+        //     where: { id: parseInt(req.body.id) },
+            // * ******************** MIGHT HAVE TO ACCESS Strain id from other side
+            // select: {
+            //   name: true,
+            //   profileViews: true,
+            // },
+        //   })
+        //   console.log('result')
+        //   console.log(result)
+
+            // await prisma.strains.findUnique({
+            //     where: {
+            //         strain: data
+            //     }
+            // }).then( (data) => {
+            //     console.log('found match')
+            //     console.log(data)
+            // }).catch( (error) => {
+            //     console.log('error')
+            //     console.log(error)
+            // })
+
+        // strains.forEach( (strain:string) => {
+            // strains.forEach(async(strain:(string|number|object)) => {
+            //     let cleanstrain = await Regex(data, 'whiteout')
+            //     let reqbodyclean = await Regex(data, 'whiteout')                
+                // if (strain.strain.replace(/\s/g, '') == req.body.dataname.replace(/\s/g, '')) {                    
+                // }
+            // })
+             
+           
             let testjson:string = 'testjson'        
             res.json( {myjson: testjson})
         }    
