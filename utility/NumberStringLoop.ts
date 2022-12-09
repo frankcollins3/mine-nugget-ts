e541f57c772b1846d2db417e9c163247c928d398
Frank Collins <fwc3rd@gmail.com>
2022-12-08 20:18:36 -0500

    set up length limited functionality based on API [8:16pm]

--- utility/NumberStringLoop.ts
+++ utility/NumberStringLoop.ts
@@ -1,18 +1,20 @@
 import Regex from 'utility/MasterRegex'
+import APIcall from 'utility/APIcall'
+
 export default async function NumberStringLoop (stringNum:string, sizeOfLoop:number) {
-        let i:number = 0
+        let i:(number|string) = 0
+        let strains = await APIcall('all', null, null)        
+        let length:number = strains.length;
         let numberAsString = await Regex(stringNum, 'numreturn')
-
-        if (numberAsString.length <= 2) {
-        for (i; i < sizeOfLoop; i++) {
-            // if 
+        if (numberAsString.length <= 2 && numberAsString < length.toString()) {
+        for (i; i < sizeOfLoop; i++) {            
+            if (i.toString() === stringNum) {
+                console.log(`i ${i} string: ${i.toString()} original ${stringNum}`)
+                console.log('i')
+//  this should be the string value but returned as a number because its just the index.
+                console.log(i)
+                return i
+            }
         })    
-        // if (numberAsString.length <= 2 || numberAsString.length <= 3) {
-            
         }
-
-        console.log('stringNum')
-        console.log(stringNum)
-        console.log(sizeOfLoop)
-
 }
