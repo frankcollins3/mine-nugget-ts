5fff04312fd373219ac2b4b7f24e37f5bab2054b
Frank Collins <fwc3rd@gmail.com>
2022-12-09 20:05:06 -0500

    see: flow of effort. param specifies looplength [8:04pm]
    if (i.toString() == stringNum)            v.s.               if (i === parseInt(stringNum)) 
    👎     this doesn't work ^                                                         👍  this works ^^

--- utility/NumberStringLoop.ts
+++ utility/NumberStringLoop.ts
@@ -2,19 +2,28 @@ import Regex from 'utility/MasterRegex'
 import APIcall from 'utility/APIcall'
 
 export default async function NumberStringLoop (stringNum:string, sizeOfLoop:number) {
+
         let i:(number|string) = 0
         let strains = await APIcall('all', null, null)        
+
         let length:number = strains.length;
-        let numberAsString = await Regex(stringNum, 'numreturn')
-        if (numberAsString.length <= 2 && numberAsString < length.toString()) {
-        for (i; i < sizeOfLoop; i++) {            
-            if (i.toString() === stringNum) {
+        let stringAsNumber = await Regex(stringNum, 'numreturn')
+        if (stringAsNumber.length <= 2 && stringAsNumber < length.toString()) {
+        for (i; i < sizeOfLoop; i++) {      
+            console.log('i this is the easy part')      
+            // console.log(i)    
+            console.log(`${i.toString()} ${typeof i.toString()}`)    
+            // console.log(typeof i)
+            console.log(stringNum)
+            console.log(typeof stringNum)
+            console.log(parseInt(stringNum))
+            if (i === parseInt(stringNum)) {
+            // if (i.toString() == stringNum) {
                 console.log(`i ${i} string: ${i.toString()} original ${stringNum}`)
                 console.log('i')
 //  this should be the string value but returned as a number because its just the index.
-                console.log(i)
                 return i
             }
-        })    
+        }  
         }
 }
