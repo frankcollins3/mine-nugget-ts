f79166976bbedd655a6f7e5121bc79b9fabadd61
Frank Collins <fwc3rd@gmail.com>
2022-12-10 20:18:08 -0500

    JOINTABLE.create() || strain.findUnique->data.upDate({tag.create() [8:17pm]
    instead of creating to the join table "UsersOnStrains" 
    i believe i want to update a current Strain and add tags to it to the [Users and UsersOnStrains  data-tables]

--- pages/api/strains/postuserstrains.ts
+++ pages/api/strains/postuserstrains.ts
@@ -2,12 +2,16 @@ import MasterRegex from 'utility/MasterRegex'
 import { PrismaClient } from '@prisma/client';
 import Regex from 'utility/MasterRegex'
 import APIcall from 'utility/APIcall'
+import NumberStringLoop from 'utility/NumberStringLoop'
 const prisma = new PrismaClient()
 
 export default async function (req, res) {
     // export default async function (req:(string|object|number), res:(string|object|number)) {
 
         let data = req.body.dataname
+        let id = req.body.id
+        console.log('id')
+        console.log(id)
         let realdata = await Regex(data, 'alphareturn')
 
 
@@ -27,23 +31,58 @@ export default async function (req, res) {
         //     }
         // })
 
-        strains.forEach( (strain) => {
+        strains.forEach(async(strain) => {
             if (strain.strain === realdata) {
-                let id:(string|number) = strain.strainId                
+                let strainid:(string|number) = strain.strainId   
+                console.log(`typoe strainid ${typeof strainid}`)
+                
+                // CREATE TABLE "UsersOnStrains" (
+                //     "usersId" INTEGER NOT NULL,
+                //     "strainsId" INTEGER NOT NULL,
+                //     CONSTRAINT "UsersOnStrains_pkey" PRIMARY KEY ("usersId","strainsId")
+                // );
+
+                let newNumber = await NumberStringLoop(id, 7)
+                console.log(`typoe new number ${typeof newNumber}`)
+                // let cleanStrain = await NumberStringLoop(strainid, 7)
+                // let resobject = {name: strainid, id: strainid}
+
+                const newstrain = await prisma.UsersOnStrains.create({
+                    data: {
+                      usersId: newNumber,
+                      strainsId: strainid,
+                    },
+                    include: {
+                        users: true,
+                        strains: true
+                    }
+                }).then(async(data) => {
+                    console.log('data success for post route!')
+                    console.log(data)
+                }).catch( (error) => {
+                    console.log('error')
+                    console.log(error)
+                })
+
+                //   usersId | strainsId | assignedAt | assignedBy
+          //   }).then(async(data:object|string|numbers) => {
+
+                res.json( {number: newNumber, id: strainid} )
             } else if (realdata === 'GorillaGlue') {
                 console.log('strain passed the gorillaglue condition')
                 if (strain.strain === 'GorillaGlue#4') {
-                    let id:(string|number) = strain.strainId                                        
+                    let strainid:(string|number) = strain.strainId    
+                    console.log('strainid GG!')                                    
+                    console.log(strainid)                                    
                 }
             } else if (realdata === 'DoSiDos') {
                 if (strain.strain === 'Do-Si-Dos') {
                     console.log("we passed the Do-Si-Dos condition!")
-                    let id:(string|number) = strain.strainId                    
+                    let strainid:(string|number) = strain.strainId                    
                 }
             }
         })           
-            let testjson:string = 'testjson'        
-            res.json( {myjson: testjson})
+            // let testjson:string = 'testjson'        
         }    
     
     
