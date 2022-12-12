277c6036d7169cdd3b6917688dae890510d7b172
Frank Collins <fwc3rd@gmail.com>
2022-12-12 15:02:26 -0500

    added setState as param [2:58pm]
    this is supposed to just be for regular arrays 
    but just in case i reuse w/ [ state && setState() ] im adding a fourth optional parameter which is the setState method (allowed to be typed as null)

--- utility/LoopAndPush.ts
+++ utility/LoopAndPush.ts
@@ -1,4 +1,4 @@
-async function LoopAndPush (LoopArray:(object|string|number), PushArray:(object|string|number), expression:any) {
+async function LoopAndPush (LoopArray:(object|string|number), PushArray:(object|string|number), ArrOrState, setState:(any|null)) {
         
         LoopArray.forEach( (loopitem:(string|object|number)) => {
             console.log('loopitem')
