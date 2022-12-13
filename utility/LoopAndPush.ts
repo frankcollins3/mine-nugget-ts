63192e9221ddc32bd3e08396babe9225806bfa5a
Frank Collins <fwc3rd@gmail.com>
2022-12-12 19:45:58 -0500

    util -> LoopAndPush split param into 2 [7:44pm]

--- utility/LoopAndPush.ts
+++ utility/LoopAndPush.ts
@@ -1,11 +1,13 @@
-async function LoopAndPush (LoopArray:(object|string|number), PushArray:(object|string|number), ArrOrState, setState:(any|null)) {
+async function LoopAndPush (LoopArray:(object|string|number), ArrOrState:(string|object|number), comparison1, comparison2,  setState:(any|null)) {
+        let Array = ArrOrState;
         
-        LoopArray.forEach( (loopitem:(string|object|number)) => {
-            console.log('loopitem')
-            console.log(loopitem)
-            if (expression) {
-                PushArray.push(loopitem)
+        LoopArray.forEach( (loopitem:(string|object|number)) => {            
+            if (comparison1 == comparison2) {                
+                ArrOrState.push(loopitem)                
+            } else {
+                return                
             }
+            return ArrOrState // kind of surprised but kind of not that you cant just return function param
         })
 }
 
