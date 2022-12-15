126b4319b3f0f87aec1532e2eb2abdc87040791f
Frank Collins <fwc3rd@gmail.com>
2022-12-15 09:47:09 -0500

    create new object out of other obj keys [9:46am]
    never mutated an object like this. 
    let newrestaurants = {
            {...restaurants}                         // brackets already created.
    }

--- pages/famtree.tsx
+++ pages/famtree.tsx
@@ -4,9 +4,28 @@ import {decrementCounter, incrementCounter} from '../redux/actions/counterAction
 
 
 
-export default function FamilyTree ({store} ) {
+export default function FamilyTree (props) {
+    console.log('props')
+    console.log(props)
+
+    let restaurants = {
+        burger: 'wendys',
+        shakes: 'wendys',
+        fries: 'wendys'
+    }
+
+    let newrestaurants = {
+        ...restaurants,
+        burger: restaurants.burger.concat(' burgerking'),
+        shakes: restaurants.shakes = ['wendys', 'burgerking'],
+        fries: restaurants.fries += 'burger king'
+    }
+
+    console.log(newrestaurants)
+
+    // console.log({store})
+
     console.log('family tree component!')
-    console.log({store})
     return (
         <div>
             <h1> guessing game test render </h1>
@@ -16,10 +35,10 @@ export default function FamilyTree ({store} ) {
 }
 
 export async function getServerSideProps(context) {              
-        console.log(context)
+        // console.log(context)
   return {
   props: {
-    ''   
+       
   }
   };
   }
