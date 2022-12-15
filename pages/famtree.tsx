89c5380d72efd1dcf7339a5504fa5d4663a4f4c1
Frank Collins <fwc3rd@gmail.com>
2022-12-14 22:30:44 -0500

    redux imports & server-side functionality [10:30pm]
    implementing example in getServerSIdeProps instead of getInitialProps

--- pages/famtree.tsx
+++ pages/famtree.tsx
@@ -1,7 +1,25 @@
-export default function FamilyTree () {
+import React from 'react';
+import {connect} from 'react-redux';
+import {decrementCounter, incrementCounter} from '../redux/actions/counterActions';
+
+
+
+export default function FamilyTree ({store} ) {
+    console.log('family tree component!')
+    console.log({store})
     return (
         <div>
             <h1> guessing game test render </h1>
+            
         </div>
     )
 }
+
+export async function getServerSideProps(context) {              
+        console.log(context)
+  return {
+  props: {
+    ''   
+  }
+  };
+  }
