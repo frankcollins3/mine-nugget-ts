558ee3429247837485e6f90a7d2548902c9136c4
Frank Collins <fwc3rd@gmail.com>
2022-12-16 12:19:04 -0500

    reconfigure imports to undo destructuring [12:17pm]

--- pages/famtree.tsx
+++ pages/famtree.tsx
@@ -3,26 +3,64 @@ import {connect} from 'react-redux';
 
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import reducer from "slices/game/gameslice";
+import { useDispatch, useSelector } from "react-redux";
+import game from 'slices/game/gameslice'
+console.log('game')
+console.log(game)
+console.log(game.actions)
 
-import { play } from 'slices/game/gameslice'
-console.log('play')
-console.log({play})
-
-// reducer: rootReducer
 const store = configureStore({
-    reducer: reducer
-});
+    reducer: game.reducer 
+})
 
-console.log('store in the famtree')
-console.log(store)
 console.log(store.getState())
-store.dispatch( { type: 'playing'})
-console.log(store.getState())
+// store.dispatch( {type: })
 
 
 
-import {decrementCounter, incrementCounter} from '../redux/actions/counterActions';
+
+// const { location } = useSelector(state=>state)
+// const dispatch = useDispatch();
+
+
+// const {  } = useSelector(state=>state)
+// const dispatch = useDispatch();
+
+
+// import reducer from "slices/game/gameslice";
+// const {  } = useSelector(state=>state)
+
+
+// const dispatch = useDispatch();
+// import { play } from 'slices/game/gameslice'
+
+
+// const store = configureStore({    
+//     reducer: reducer
+// });
+
+// console.log(store.getState())
+// store.dispatch( { type: 'playing'})
+// console.log("hey were over here")
+// console.log(store.getState())
+// const numbermethod = number => number + 1
+// let gotmynumber = numbermethod(1)
+// console.log('gotmynumber')
+// console.log(gotmynumber)
+
+
+// const playvalue = state => state.inplay
+// const locationvalue = state => state.location
+// const playval = playvalue(store.getState())
+// const locval = locationvalue(store.getState())
+
+// let newlocation = store.dispatch( { type: 'newlocation'})
+// console.log('newlocation')
+// console.log(newlocation)
+
+// console.log(playval)
+// console.log(locval)
+// import {decrementCounter, incrementCounter} from '../redux/actions/counterActions';
 
 
 
@@ -30,8 +68,13 @@ export default function FamilyTree (props) {
     console.log('props')
     console.log(props)
 
+    
+
     const checkredux = async () => {
-        await store.getState()
+        console.log("this is my redux")
+        // await props.store.getState()
+        // await props.store.displatch( { type: 'playing'})
+        // await props.store.getState()
     }
     // let restaurants = {
     //     burger: 'wendys',
@@ -56,6 +99,13 @@ export default function FamilyTree (props) {
     )
 }
 
+// reducer: rootReducer
 // export async function getServerSideProps(context) {              
+
+//     return {
+//         props: {
+//             store
+//         }
+//     }
 
 //   }
