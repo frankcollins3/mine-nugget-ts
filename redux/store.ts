eb0967e8b2b232ec35cd7847229150e75f6bc860
Frank Collins <fwc3rd@gmail.com>
2022-12-16 18:07:17 -0500

    redux store works [6:07pm]
    also factoring in actions need to be exported not local [ 6:06pm ]

--- redux/store.ts
+++ redux/store.ts
@@ -1,11 +1,37 @@
 import { configureStore } from '@reduxjs/toolkit'
-import game from 'slices/game/gameslice'
+import { combineReducers } from 'redux'
 
+const {createStore} = require("redux")
 
-const store = configureStore({
-  reducer: {
-    game: game.reducer
-  },
+const initState = {
+    counter: 77,
+    inplay: false
+}
+const gameReducer = (state=initState, action) => {
+        if(action.type==="INCREMENT") {
+        state.counter++
+        return state
+    }
+    else if(action.type==="DECREMENT") {
+        state.counter--
+        return state
+    }
+    if (action.type === "PLAYING_GAME") {
+        state.inplay = true
+        return state
+    }
+    if (action.type === "NOT_PLAYING_GAME") {
+        state.inplay = false
+        return state
+    }
+
+    else return state
+}
+
+const rootReducer = combineReducers({
+    gameReducer,
 })
+
+const store = createStore(rootReducer)
 
 export default store
