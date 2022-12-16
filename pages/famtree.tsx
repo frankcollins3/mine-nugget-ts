d32b2dcc62bd5b6ae62719e3d377ba3fcb0ae3e2
Frank Collins <fwc3rd@gmail.com>
2022-12-16 10:59:03 -0500

    redux is linked and working [10:58am]
                                      store is working with state and is returning 
    {liftedStore: {…}, dispatch: ƒ, subscribe: ƒ, getState: ƒ, replaceReducer: ƒ, …}
    ()=> {…}
    dispatch
    getState
    liftedStore
    {dispatch: ƒ, subscribe: ƒ, getState: ƒ, replaceReducer: ƒ, @@observable: ƒ}
    replaceReducer
    ƒ replaceReducer(r)
    subscribe 
    ƒ subscribe(listener)
    
    *****
    this is the console.log(store.getState()) from clientside google browser:
    {inplay: false}
    *****
    
    ******
    this is the code from vs code:
        initialState: {                         // { actions } = game
            inplay: false
            // inplay: false
        },
    ****
    
    inplay key is persisting through import

--- pages/famtree.tsx
+++ pages/famtree.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
 import {connect} from 'react-redux';
+
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import reducer from "slices/game/gameslice";
+
+import { play } from 'slices/game/gameslice'
+console.log('play')
+console.log({play})
+
+// reducer: rootReducer
+const store = configureStore({
+    reducer: reducer
+});
+
+console.log('store in the famtree')
+console.log(store)
+console.log(store.getState())
+store.dispatch( { type: 'playing'})
+console.log(store.getState())
+
+
+
 import {decrementCounter, incrementCounter} from '../redux/actions/counterActions';
 
 
@@ -8,37 +30,32 @@ export default function FamilyTree (props) {
     console.log('props')
     console.log(props)
 
-    let restaurants = {
-        burger: 'wendys',
-        shakes: 'wendys',
-        fries: 'wendys'
+    const checkredux = async () => {
+        await store.getState()
     }
+    // let restaurants = {
+    //     burger: 'wendys',
+    //     shakes: 'wendys',
+    //     fries: 'wendys'
+    // }
 
-    let newrestaurants = {
-        ...restaurants,
-        burger: restaurants.burger.concat(' burgerking'),
-        shakes: restaurants.shakes = ['wendys', 'burgerking'],
-        fries: restaurants.fries += 'burger king'
-    }
-
-    console.log(newrestaurants)
-
-    // console.log({store})
-
+    // let newrestaurants = {
+    //     ...restaurants,
+    //     burger: restaurants.burger.concat(' burgerking'),
+    //     shakes: restaurants.shakes = ['wendys', 'burgerking'],
+    //     fries: restaurants.fries += 'burger king'
+    // }
+    // console.log(newrestaurants)
     console.log('family tree component!')
+
     return (
         <div>
             <h1> guessing game test render </h1>
-            
+            <button onClick={checkredux}></button>
         </div>
     )
 }
 
-export async function getServerSideProps(context) {              
-        // console.log(context)
-  return {
-  props: {
-       
-  }
-  };
-  }
+// export async function getServerSideProps(context) {              
+
+//   }
