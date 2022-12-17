bed863b40b66af01d6749af3f95d0fca237c7c59
Frank Collins <fwc3rd@gmail.com>
2022-12-17 08:38:59 -0500

    came up with organizing system [8:38am]
    don't like seeing sass and .js file together though this is the last page of the app.
    but also wouldn't want to see arranging the style files by file-extension v.s. the pages on which they are used.
    
    /game/components  === <styledcomponents/>
    
    /game/sass === [.scss && .sass]

---
+++ styles/game/components/GameContainer.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+    min-height: 50vh;
+    min-width: 50vw;
+    
+
+`
+
+// * const CardStyle = styled(Display)`    have shadowing on another component
+// box-shadow: 10px 10px 10px rgb(247, 208, 32);    
+// border: 3px solid rgb(247, 208, 36);    
+// `
+
+export default Container
