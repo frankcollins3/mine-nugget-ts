e4b89f99efa07eae080ba79adfc797cc4532cda3
Frank Collins <fwc3rd@gmail.com>
2022-12-17 21:05:14 -0500

    test styled components file path with import [9:05pm]

--- pages/famtree.tsx
+++ pages/famtree.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-
+import Container from 'styles/game/components/GameContainer'
 import { TypedUseSelectorHook, useSelector } from 'react-redux'
 import {Provider} from 'react-redux';
 import withRedux from "next-redux-wrapper";
@@ -46,6 +46,7 @@ await store.getState()
 // export default function FamilyTree (props) {
 
     const [parents, setParents] = useState('')
+    const [int, setInt] = useState('')
     console.log('props')
     console.log(props)
 
@@ -76,27 +77,31 @@ await store.getState()
 
 
     // store.dispatch(increment)
-    setTimeout( () => {
-        console.log("wow no way!")
-        console.log(store.getState())
-    }, 2000 )
+    // setTimeout( () => {
+    //     console.log("wow no way!")
+    //     console.log(store.getState())
+    // }, 2000 )
     
     
 
     const checkredux = async () => {
-        console.log('items')
 
-        await dispatch( {type: "INCREMENT"})
-        // await dispatch( {type: "INCREMENT"})
-        if (gamestate.gameReducer.inplay === true) {
-            setParents(inplay)
-            await dispatch( {type: NOT_PLAYING_GAME})
-        } else if (gamestate.gameReducer.inplay === false) {
-            setParents('magic')
-            await dispatch( {type: PLAYING_GAME})            
-        }
-        console.log('gamestate')
-        console.log(gamestate)
+
+        // let int = await dispatch( {type: "INCREMENT"})
+
+        // // await dispatch( {type: "INCREMENT"})
+        // if (gamestate.gameReducer.inplay === true) {
+        //     await dispatch( { type: "SET_PARENTS"} )
+        //     await dispatch( {type: NOT_PLAYING_GAME} )
+
+        //     await dispatch( { type: "WIN_STREAK"})
+        //     setParents('magic')
+        // } else if (gamestate.gameReducer.inplay === false) {
+        //     // await dispatch( {type: "SET_PARENTS"})
+        //     setParents(counter)
+        //     await dispatch( {type: PLAYING_GAME})            
+        // }
+
     }
         
     
@@ -116,20 +121,26 @@ await store.getState()
     console.log('family tree component!')
 
     return (
-        <div style= {{ backgroundColor: 'dodgerBlue', minHeight: '100vh'}}>
+        <Container>
+        <div    
+        // style= {{ backgroundColor: 'dodgerBlue', minHeight: '100vh'}}
+        >
+        
             <h1> guessing game test render </h1>
             <h1> {parents || 'ayoo'} </h1>
             <button onClick={checkredux}></button>
             
         </div>
+            </Container>
     )
 }
 
-const mapStateToProps = (gamestate) => {
+const mapStateToProps = (game) => {
     return {
          counter: game.counter,
          inplay: game.inplay,
-         parents: game.parents
+         parents: game.parents,
+         winstreak: game.winstreak         
          }
 }
 
