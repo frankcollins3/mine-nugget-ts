eb8d7fc992ebbf83465f4fa5e9adf9726b7899fd
Frank Collins <fwc3rd@gmail.com>
2022-12-17 22:52:48 -0500

    [id] route: accept number only req.query [10:52pm]
    the other data call in the application that utilizes this endpoint has an alpha-character-string attached to the data 
    the elseblock now distinguishes between api calls that have that [set of a-z characters] or not.  // (this committed usecase)

--- pages/api/strains/strain/[strainId].ts
+++ pages/api/strains/strain/[strainId].ts
@@ -3,6 +3,7 @@ import APIcall from 'utility/APIcall'
 import { PrismaClient } from '@prisma/client';
 import Regex from 'utility/MasterRegex'
 let prisma = new PrismaClient()
+let i = 0;
 
 
 export default async function (req:any, res:any) {              // res:string doesn't work
@@ -11,24 +12,21 @@ export default async function (req:any, res:any) {              // res:string do
     
     // let reqStr = req.query
     let twostringkeys = req.query.strainId
+    let dbstrain = await prisma.strains.findMany()
 
     if (twostringkeys.includes('no')) {
         console.log("it includes no!!!!")        
-        let reqstr = await Regex(twostringkeys, 'numreturn')
-        console.log('reqstr')
-        console.log(reqstr)
-        // console.log(typeof reqStr)
-    
-        // let reqStr = parseInt(req.query.strainId)        
-        let dbstrain:(object|string|number)[] = await prisma.strains.findMany()
-        // let dbstrain: { type: any } = await prisma.strains.findMany()
+        let reqstr = await Regex(twostringkeys, 'numreturn')            
+        
         
         const returnRes = () => {
             dbstrain.forEach( (strain) => {            
                 let strainstring:any = strain.strainId
                 if (strainstring.toString() === reqstr) {                
+
                     // strainbucket.push({ strain })
-                    return res.json(  dbstrain  )
+                    // return res({hey: 'hi'})
+                    return res.json( strain )
                 }
             })
         }
@@ -36,6 +34,29 @@ export default async function (req:any, res:any) {              // res:string do
         
     } else if (!twostringkeys.includes('no')) {
         console.log("the data doesn't have that so this same function should return undefined.")
+        let elseid:number = twostringkeys
+        console.log('there should be no data in this part')
+        do {    
+            console.log("hey we are in a do while loop")
+            // * slice next baby!
+            if (dbstrain[i]) {
+                
+                let index = dbstrain[i]
+                let strainid = index.strainId
+                console.log('strainid')
+                console.log(`${strainid}${typeof strainid}`)
+                console.log(`twostrainkeys ${twostringkeys} ${typeof twostringkeys} `)
+                if (index.strainId === parseInt(twostringkeys)) {
+                // if (strainid === parseInt(twostringkeys)) {
+                    console.log('twostringkeys if condition!')
+                    console.log(index)
+                    console.log(twostringkeys)
+                }
+            }
+            i++
+        }
+        while(i < dbstrain.length)
+        return res.json( {hey: 'hi'})
         // return { christmas: 'coal'}
         // return (             cant render react elements in .ts file
         //     <div>
