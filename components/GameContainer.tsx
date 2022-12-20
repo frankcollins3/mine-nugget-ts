10d9520da8d307e17dd1e5f63a3917b8ad63e017
Frank Collins <fwc3rd@gmail.com>
2022-12-19 19:10:37 -0500

    [GameContainer] redux as text [7:10pm]
    few things and also append redux state.
    bootstrap containers as well.

--- components/GameContainer.tsx
+++ components/GameContainer.tsx
@@ -1,6 +1,22 @@
-import ShadowBorder from 'styles/game/components/GameContainer'
+// * dependencies
 import {connect} from 'react-redux';
 import { TypedUseSelectorHook, useSelector } from 'react-redux'
+import styles from 'styles/game/sass/FamilyTree.module.scss'
+// * CSS
+import ShadowBorder from 'styles/game/components/GameContainer'
+
+
+
+// * components:
+import ParentRing from 'components/ParentRing'
+import ParentWatch from 'components/ParentWatch'
+import Container from 'react-bootstrap/Container'
+import Family from 'components/FamilyTreeContainer1'
+import Family2 from 'components/FamilyTreeContainer2'
+
+
+
+
 
 export default  function GameContainer (props) {
     console.log('props from the GameContainer!!!!')
@@ -8,12 +24,48 @@ export default  function GameContainer (props) {
     let parents:string = props.redux
     return (
         <>
+
             <ShadowBorder>
         <div>
-            <p> real game container </p>
-            <h1
-            style= {{ color: 'whitesmoke'}}
-            > {parents || 'nice'} </h1>
+        
+
+
+        <div 
+        className={styles.Row2}>
+        <Family/>
+        <Family/>
+        <Family/>
+        </div>
+        
+        
+
+        <Container className={styles.Row}>
+            {/* <div className={styles.Row}> */}
+            <Container className={styles.ColumnParent}>
+            <ParentRing parents={parents}/>
+            <h1> {parents || ''}</h1>
+            </Container>
+
+            <Container className={styles.ColumnParent}>
+            <ParentWatch parents={parents}/>
+            <h1> {parents || ''}</h1>            
+            </Container>
+            {/* </div> */}
+        </Container>
+        
+
+        {/* <Family2/> */}
+
+        <div 
+        className={styles.Row2}>
+        <Family/>
+        <Family/>
+        <Family/>
+        </div>
+        
+        
+
+
         </div>
             </ShadowBorder>
         </>
