d7ba5d12b8a57626f1eb79b6b4db848b93948c12
Frank Collins <fwc3rd@gmail.com>
2022-12-19 20:08:34 -0500

    almost posted issue. redux config [8:08pm]
    Loading...
    Expected 0 arguments, but got 1.ts(2554)
    
     # const dispatch = useDispatch() 
    // almost posted issue. the storing of the invocation of useDispatch method was done in a line of code by the imports in the top level block and broke app
    
        let parents:string = props.redux   
        let parent1 = props.parent1 
        let parent2:string = props.parent1 
    
    was also having trouble because i was doing this:
    
            // reduxparents = dispatch( { type: "SET_PARENTS_1", payload: { parents: newstr[0]}}
    
    i was using props as state which now as i type shouldn't seem to be problematic. you're not supposed to make dispatches of state that is passed as props
    
    global redux state is to avoid passing props. but have also successfully passed the redux state as props from parent to this container component.

--- components/GameContainer.tsx
+++ components/GameContainer.tsx
@@ -5,8 +5,6 @@ import styles from 'styles/game/sass/FamilyTree.module.scss'
 // * CSS
 import ShadowBorder from 'styles/game/components/GameContainer'
 
-
-
 // * components:
 import ParentRing from 'components/ParentRing'
 import ParentWatch from 'components/ParentWatch'
@@ -14,14 +12,43 @@ import Container from 'react-bootstrap/Container'
 import Family from 'components/FamilyTreeContainer1'
 import Family2 from 'components/FamilyTreeContainer2'
 
+// * redux
+import {PLAYING_GAME, NOT_PLAYING_GAME} from 'redux/actions/gameActions'
+import { playingGame, notPlayingGame } from 'redux/actions/gameActions'
+import wrapper from '../redux/store';
+import store from 'redux/store'
+import { useDispatch } from "react-redux";
+import customReduxSelector from 'redux/reduxselector'
 
+// * utility functions
+import Regex from 'utility/MasterRegex'
 
 
 
 export default  function GameContainer (props) {
     console.log('props from the GameContainer!!!!')
     console.log(props)
-    let parents:string = props.redux
+    const dispatch = useDispatch()
+    let reduxresult = useSelector(state => state)
+    console.log('reduxresult')
+    console.log(reduxresult)
+
+    let parents:string = props.redux   
+    let parent1 = props.parent1 
+    let parent2:string = props.parent1 
+
+    const statechange = async () => {
+        console.log("state change function")
+        let newstr = await Regex(parents, 'stringsplit')
+        console.log('newstr')
+        console.log(newstr)
+    }
+
+    const checkredux = async () => {
+        // reduxparents = dispatch( { type: "SET_PARENTS", payload: { parents: randomparents}}
+    }
+
+
     return (
         <>
 
@@ -44,11 +71,18 @@ export default  function GameContainer (props) {
             <Container className={styles.ColumnParent}>
             <ParentRing parents={parents}/>
             <h1> {parents || ''}</h1>
+            <h1> {parent1 || ''}</h1>
+            <img
+            style={{ height: '50px', width: '50px'}}
+            src="img/gold.png"/>         
             </Container>
 
             <Container className={styles.ColumnParent}>
             <ParentWatch parents={parents}/>
-            <h1> {parents || ''}</h1>            
+            <h1> {parents || ''}</h1>   
+            <img
+            style={{ height: '50px', width: '50px'}}
+            src="img/gold.png"/>         
             </Container>
             {/* </div> */}
         </Container>
@@ -62,12 +96,9 @@ export default  function GameContainer (props) {
         <Family/>
         <Family/>
         </div>
-        
-        
-
-
         </div>
             </ShadowBorder>
+        <button onClick={statechange}></button>
         </>
     )
 }
