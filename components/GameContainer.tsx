9ff5dbf8e91fcae253aa81adcc16920b34d8f0d0
Frank Collins <fwc3rd@gmail.com>
2022-12-22 07:17:06 -0500

    [GameContainer] [7:15am]
    create a cactus that has to be hovered upon to reveal the game.
    the cactus tries to bridge the concept between "tree", and "gold-mine" 
    
    also created minimum-viable local-to-component state that will be swapped out for [ redux & action ]

--- components/GameContainer.tsx
+++ components/GameContainer.tsx
@@ -1,8 +1,10 @@
 // * dependencies
-import {connect} from 'react-redux';
-import { TypedUseSelectorHook, useSelector } from 'react-redux'
-import styles from 'styles/game/sass/FamilyTree.module.scss'
+// import {connect} from 'react-redux';
+import { TypedUseSelectorHook, useSelector, connect } from 'react-redux'
+import { useState, useEffect} from 'react'
+
 // * CSS
+import styles from 'styles/game/sass/FamilyTree.module.scss'
 import ShadowBorder from 'styles/game/components/GameContainer'
 
 // * components:
@@ -30,6 +32,8 @@ import Regex from 'utility/MasterRegex'
 export default  function GameContainer (props) {
     console.log('props from the GameContainer!!!!')
     console.log(props)
+    const [cactusHover, setCactusHover] = useState(false)
+
     const dispatch = useDispatch()
     let reduxresult:any = useSelector(state => state)
     
@@ -54,73 +58,60 @@ export default  function GameContainer (props) {
     return (
         <>
             <ShadowBorder>
-        <div>
+                {cactusHover === false 
+                ?
+                <img onMouseEnter={()=> setCactusHover(true)} src="/img/cactus.png"/>
+                :
+
+                <div>
+                <div 
+                className={styles.Row2}>
+                <Family/>
+                <Family/>
+                <Family/>
+                </div>
+                
+                <div className="Column">
+                <GameChild/>
+                </div>
+                
         
-
-
-        <div 
-        className={styles.Row2}>
-        <Family/>
-        <Family/>
-        <Family/>
-        </div>
+                <Container className={styles.Row}>
         
-        <div className="Column">
-        <GameChild/>
-        </div>
+                    <Container className={styles.ColumnParent}>
+                    <ParentRing parents={parents}/>
+                
+                    <h3 className="ParentContText"
+                    > {parent1 || ''} </h3>
+                    <img
+                    onMouseEnter={checkredux}
+                    style={{ height: '50px', width: '50px'}}
+                    src="img/gold.png"/>         
+                    </Container>
         
-
-        <Container className={styles.Row}>
-            {/* <div className={styles.Row}> */}
-            <Container className={styles.ColumnParent}>
-            <ParentRing parents={parents}/>
-            {/* <h1> {parents || ''}</h1> */}
-            <h3 className="ParentContText"
-            > {parent1 || ''}</h3>
-            <img
-            onMouseEnter={checkredux}
-            style={{ height: '50px', width: '50px'}}
-            src="img/gold.png"/>         
-            </Container>
-
-            {/* <Container
-            style= {{ border: '5px solid papayawhip', boxShadow: 'none'}}
-             className={styles.ColumnParent}>
-            <GameCounter/>  
-            <h2 className="ParentContText"
-            style={{
-                 fontFamily: 'Brush Script MT',
-                 letterSpacing: '0.5em'
-
-            }}> Family Tree </h2>     
-            </Container> */}
-
-            <Container             
-            className={styles.ColumnParent}>
-            <ParentWatch parents={parents}/>
-            {/* <h1> {parents || ''}</h1> */}
-            <h3 className="ParentContText"
-            > {parent2 || ''}</h3>
-            <img
-            style={{ height: '50px', width: '50px'}}
-            src="img/gold.png"/>         
-            </Container>
-
-        </Container>
-            
-            
-            
-            
-
+                    <Container             
+                    className={styles.ColumnParent}>
+                    <ParentWatch parents={parents}/>
+                    {/* <h1> {parents || ''}</h1> */}
+                    <h3 className="ParentContText"
+                    > {parent2 || ''}</h3>
+                    <img
+                    style={{ height: '50px', width: '50px'}}
+                    src="img/gold.png"/>         
+                    </Container>
         
+                </Container>
+                    
+                <div 
+                className={styles.Row2}>
+                <Family/>
+                <Family/>
+                <Family/>
+                </div>
+                </div>
+                
+                }
 
-        <div 
-        className={styles.Row2}>
-        <Family/>
-        <Family/>
-        <Family/>
-        </div>
-        </div>
             </ShadowBorder>
         <button onClick={statechange}></button>
         </>
