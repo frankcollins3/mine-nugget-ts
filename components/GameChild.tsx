189e2638fabe5db841f94a6ac6cee0d226017cae
Frank Collins <fwc3rd@gmail.com>
2022-12-22 22:58:18 -0500

    [GameChild] drag functionality [11:00pm]
    used the modular attribute changing function to set the draggable element to false.

--- components/GameChild.tsx
+++ components/GameChild.tsx
@@ -2,31 +2,63 @@ import styles from 'styles/game/sass/FamilyTree.module.scss'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import { useRef } from 'react'
+import $ from 'jquery'
+import CSS from 'utility/CSStool'
+import AttrTool from 'utility/JqAttr'
  
 
 export default function GameChild () {
 
-    const dragStart = async () => {
+    const dragStart = async (event) => {
         console.log("hey were dragging something")
     }
 
-    // possibly using useRef to distinguish the coin that will have the 
-    // hover on the coin and the label will say what strain it is in small text. then drag and it makes the coin disappear.
+    const dragEnd = async (event) => {
+        console.log('event')
+        console.log(event)
+        let tgt = $(event.target)
+        CSS(tgt, 'opacity', '0.1')
+        AttrTool(tgt, 'draggable', 'false')
+    }
+
+            
+
+    const allowDrop = (ev) => {        
+        ev.preventDefault()
+    }
+
+    function drop(ev) {
+        console.log("we are dropping!")
+        
+        console.log('ev and dataTransfer')
+        console.log(ev)
+        console.log(ev.dataTransfer)
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+      }
 
     return (
         <>
         {/* <div className="Column"> */}
         <div className={styles.GameChild}>
-            <img draggable="true" onDragStart={dragStart}className={styles.coin} src="img/coin.png"/>
+
+            <img
+            className={styles.coin} src="img/coin.png"
+            onDrop={drop}
+            onDragEnd={dragEnd}
+            draggable="true"        
+            />
+
             <img className={styles.coin} src="img/coin.png"/>
             <div className="Column">
             <img
-             style={{ borderRadius: '50%', border: '2px solid papayawhip', boxShadow: '10px 10px 10px papayawhip'}}
-            //  border-radius: 50%;
-            //  border: 2px solid papayawhip;
-            //  box-shadow: 10px 10px 10px papayawhip;
+            onDrop={drop}
+            onDragEnd={dragEnd}
+            draggable="true" 
+             style={{ borderRadius: '50%', border: '2px solid papayawhip', boxShadow: '10px 10px 10px papayawhip'}}            
              className={styles.coin} src="img/mine.png"/>
-             {/* <h1> ayoo</h1> */}
+
             </div>    
             <img className={styles.coin} src="img/coin.png"/>            
             <img className={styles.coin} src="img/coin.png"/>            
