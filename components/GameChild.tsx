6e27560638be57c042373035140f4884d2a1c307
Frank Collins <fwc3rd@gmail.com>
2022-12-27 10:26:47 -0500

    class constructor to centralize modular jq func [10:26pm]
          <Draggable type="coin" data="sourog">            
                <img
                draggable="false"
                onDragStart={handleDragStart}
                className={styles.coin} src="img/coin.png" 
                />
                </Draggable>
    
    **** npm i react-drag-and-drop comes with Encapsulating components with props that set up the dragging and dropping. ****
    
    i already have modular functions that accept an argument [typeof object] to return [ $(obj).siblings() ]        or [  $(obj).children()   ]      
    instead of creating 1 more modular function 
    
    i'm creating a javascript file with an ES6 constructor class that will accept: [ parents | children | siblings ]
    and return the relative elements that correspond to the specified-by-(func-param) object ^ ^ ^ ^ 
    
    this function is purposed to retrieve the parents of the data during the guessing game.

--- components/GameChild.tsx
+++ components/GameChild.tsx
@@ -1,66 +1,29 @@
-
-import styles from 'styles/game/sass/FamilyTree.module.scss'
-import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
-import { useRef } from 'react'
 import $ from 'jquery'
-import CSS from 'utility/CSStool'
-import AttrTool from 'utility/JqAttr'
-import Siblings from 'utility/JqSiblings'
-import Random from 'utility/Randomizer'
-import { Draggable, Droppable } from 'react-drag-and-drop'
 
-export default function GameChild () {
-    
-    const handleDragStart = async () => {
-        console.log('hello!')
+async function Family (familymember) {
+// async function Family (familymember:string) {
+    class Fam {
+        constructor(familymember) {
+            this.familymember = familymember
+        }
 
+        get parents() {
+            return this.objectparents
+        }
+            // * getters 
+        objectparents() {
+            let parents = $(familymember).parents()
+            return parents
+        }
     }
+        
 
-    const handleDrop = () => {
-        console.log("okay were dropping")
+    if (typeof familymember === 'object') {
+        if (familymember === 'parents') {
+            let parent = await new Fam(familymember).parents
+            console.log('parent')
+            console.log(parent)
+            return parent
+        }
     }
-
-
-    return (
-        <>
-        <div className="Column">
-        <div className={styles.GameChild}>
-
-            <Draggable type="coin" data="sourog">
-            
-            <img
-            onDragStart={handleDragStart}
-            className={styles.coin} src="img/coin.png" 
-            />
-            </Draggable>
-
-            <img 
-                 className={styles.coin} src="img/coin.png"
-                 />
-
-            {/* <div className="Column">                 */}
-
-            <img 
-                className={styles.coin} src="img/coin.png" 
-                />            
-
-            <img
-                className={styles.coin} src="img/coin.png" 
-                />            
-        </div>  
-
-            <Droppable types={['coin']} onDrop={() => console.log("hey we are dropping so hard")}>
-            <img            
-            draggable="true" 
-            onDrop={handleDrop}
-            style={{ borderRadius: '50%', border: '2px solid papayawhip', boxShadow: '10px 10px 10px papayawhip'}}            
-            className={styles.mine} src="img/mine.png"/>
-            </Droppable>
-
-        {/* <img style={{ border: '5px solid papayawhip', transform: `scale(0.2)` }} src="/img/mine.png"/> */}
-        </div>
-
-        </>
-    )
 }
