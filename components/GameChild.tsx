ba397b00fd6148786fc1e1f828ecb2bc9a3ebbd8
Frank Collins <fwc3rd@gmail.com>
2022-12-27 22:48:37 -0500

    returning props from encapsulating component [10:48pm]
    NamedNodeMap {0: type, 1: data, 2: draggable, type: type, data: data, draggable: draggable, length: 3}
    type
    1
    data
    2
    draggable
    data
    data
    draggable
    draggable
    type
    type
    3
    NamedNodeMap
    
    ******** this works. the data returned in this object above is the props from the element-encapsulating imported drag and drop components.
    
                <Draggable type="coin" data="sourog">            
                <img
                draggable="true"
                onDragStart={handleDragStart}         
                className={styles.coin} src="img/coin.png" 
                />
                </Draggable>
    
    <draggable type="coin"/>
    
    
    ***************
                <Droppable types={['coin']} onDrop={handleDrop}>
                <img            
                draggable="true" 
                // onDrop={handleDrop}
                style={{ borderRadius: '50%', border: '2px solid papayawhip', boxShadow: '10px 10px 10px papayawhip'}}            
                className={styles.mine} src="img/mine.png"/>
                </Droppable>
    
    <Droppable types={ ['coin'] />
    
    all that has to be done is access that prop data that is the 'coin' string that is the validator for an object to run the expression in the component:
    that is wrapped in the <Droppable/> element that comes imported with the <Draggable/> element

--- components/GameChild.tsx
+++ components/GameChild.tsx
@@ -1,29 +1,95 @@
+
+import styles from 'styles/game/sass/FamilyTree.module.scss'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import { useRef } from 'react'
 import $ from 'jquery'
+import CSS from 'utility/CSStool'
+import AttrTool from 'utility/JqAttr'
+import Siblings from 'utility/JqSiblings'
+import Random from 'utility/Randomizer'
+import { Draggable, Droppable } from 'react-drag-and-drop'
+import Family from 'utility/familyJS'
 
-async function Family (familymember) {
-// async function Family (familymember:string) {
-    class Fam {
-        constructor(familymember) {
-            this.familymember = familymember
+export default function GameChild () {
+    
+    const handleDragStart = async (event) => {
+        console.log('hello!')
+        let target = event.target        
+        console.log( $(target).siblings() )
+        // $(target).css('border', '10px solid pink')
+        let parents = await Family(target, 'parents')                
+        if (parents) {
+            console.log('if parents')
+            console.log(parents[0].attributes)
+            let familytype = parents[0].attributes
+            console.log('familytype')
+            console.log(familytype)
         }
 
-        get parents() {
-            return this.objectparents
-        }
-            // * getters 
-        objectparents() {
-            let parents = $(familymember).parents()
-            return parents
-        }
+        // /console.log('parents in native drop')
+        // console.log(parents)/
+
     }
+
+    const nativeDrop = async (event) => {        
+
+    }
+
+    const handleDrop = async (event) => {
+        console.log("okay were dropping")
+        // console.log('event')
+        // console.log(event)
+        // let target = event.target
+        // console.log('target')
+        // console.log(target)
+        // let parents = await Family(target, 'parents')
         
-
-    if (typeof familymember === 'object') {
-        if (familymember === 'parents') {
-            let parent = await new Fam(familymember).parents
-            console.log('parent')
-            console.log(parent)
-            return parent
-        }
     }
+
+
+    return (
+        <>
+        <div className="Column">
+        <div className={styles.GameChild}>
+
+            <Draggable type="coin" data="sourog">            
+            <img
+            draggable="true"
+            onDragStart={handleDragStart}         
+            // onDragOver={handleDragStart}         
+            // onDragEnd={nativeDrop}
+            // onDragOver={nativeDrop}
+            className={styles.coin} src="img/coin.png" 
+            />
+            </Draggable>
+
+            <img 
+                 className={styles.coin} src="img/coin.png"
+                 />
+
+            {/* <div className="Column">                 */}
+
+            <img 
+                className={styles.coin} src="img/coin.png" 
+                />            
+
+            <img
+                className={styles.coin} src="img/coin.png" 
+                />            
+        </div>  
+
+            <Droppable types={['coin']} onDrop={handleDrop}>
+            <img            
+            draggable="true" 
+            // onDrop={handleDrop}
+            style={{ borderRadius: '50%', border: '2px solid papayawhip', boxShadow: '10px 10px 10px papayawhip'}}            
+            className={styles.mine} src="img/mine.png"/>
+            </Droppable>
+
+        {/* <img style={{ border: '5px solid papayawhip', transform: `scale(0.2)` }} src="/img/mine.png"/> */}
+        </div>
+
+        </>
+    )
 }
