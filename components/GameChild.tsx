8b42ddaa03582429a10e10c200ce97b2feb8314a
Frank Collins <fwc3rd@gmail.com>
2022-12-28 20:21:31 -0500

    [GameChild] this finally works and doesn't activate drop func [8:17pm]
    interesting:
    if an element has draggable="false"
    
    you can change it to draggable="true" internally with jquery, but:
    
     <Droppable types={['coin']}
    
    ***********
    its very interesting this state based approach seems necessary because the 
    "coin" attribute can be changed and persist through yet not validate the:
    
     <Droppable types={['coin']} onDrop={handleDrop}> 'coin' part of this type that is a validating attribute to proceed with drop functionality
    
    you can see the change in google chrome that the change from 'coin' to 'notcoin' works but that still isn't enough to fail the <Droppable types="coin">
    and the [react-element-component-props v.s. DOM changing with jquery expressions] is heeding the greater enforcer of defaults which isn't the DOM

--- components/GameChild.tsx
+++ components/GameChild.tsx
@@ -2,16 +2,19 @@
 import styles from 'styles/game/sass/FamilyTree.module.scss'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
-import { useRef } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import $ from 'jquery'
 import CSS from 'utility/CSStool'
 import AttrTool from 'utility/JqAttr'
 import Siblings from 'utility/JqSiblings'
 import Random from 'utility/Randomizer'
 import { Draggable, Droppable } from 'react-drag-and-drop'
+
 import Family from 'utility/familyJS'
 
 export default function GameChild () {
+
+    const [coin1, setCoin1] = useState(false)
     
     const handleDragStart = async (event) => {
         console.log('hello!')
@@ -23,15 +26,19 @@ export default function GameChild () {
             console.log('if parents')
             console.log(parents[0].attributes)
             let dragParent = parents[0]
-            AttrTool(dragParent, 'type', 'notcoin')
             let familytype = parents[0].attributes
-
+            if (familytype[0].textContent === 'notcoin') {
+                      console.log("hey the type is notcoin")
+            } else {
+                console.log("hey the type is coin")
+                setCoin1(true)
+                AttrTool(dragParent, 'type', 'notcoin')
+                AttrTool(dragParent, 'data', 'nice one')
+                await CSS(target, 'opacity', '0.1')
+            }
             console.log('familytype')
             console.log(familytype)
         }
-
-        // /console.log('parents in native drop')
-        // console.log(parents)/
 
     }
 
@@ -41,13 +48,9 @@ export default function GameChild () {
 
     const handleDrop = async (event) => {
         console.log("okay were dropping")
-        // console.log('event')
-        // console.log(event)
-        // let target = event.target
-        // console.log('target')
-        // console.log(target)
-        // let parents = await Family(target, 'parents')
-        
+        console.log('event')
+        console.log(event)
+        console.log(event.target)                
     }
 
 
@@ -56,13 +59,10 @@ export default function GameChild () {
         <div className="Column">
         <div className={styles.GameChild}>
 
-            <Draggable type="coin" data="sourog">            
+            <Draggable type={coin1 === false ? "coin" : "notcoin"} data="sourog">     
             <img
             draggable="true"
             onDragStart={handleDragStart}         
-            // onDragOver={handleDragStart}         
-            // onDragEnd={nativeDrop}
-            // onDragOver={nativeDrop}
             className={styles.coin} src="img/coin.png" 
             />
             </Draggable>
