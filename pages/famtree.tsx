64ea06957dc25bbebc99b720f15c8ec1bace714e
Frank Collins <fwc3rd@gmail.com>
2022-12-31 17:21:53 -0500

    fixed async / mouseEvent [5:21pm]
    fixed an asynchronous issue where the state doesn't set in time after the redux dispatch changes the state.
    i moved the redux action into a [IIFE && useEffect]
    👍  now upon hover, the string data is appended to the container on the first hover event.

--- pages/famtree.tsx
+++ pages/famtree.tsx
@@ -37,15 +37,17 @@ import Family from 'components/FamilyTreeContainer1'
 
 store.dispatch( { type: "PLAYING_GAME!"})
 store.dispatch( { type: "INCREMENT" })
-store.dispatch( { type: 'SET_PARENTS', payload: { parents: 'white rhino'}})
 store.getState()
+// store.dispatch( { type: 'SET_PARENTS', payload: { parents: 'white rhino'}})
+
 
 
 let i = 0;
  function FamilyTree (props) {
-    
-    // export default function FamilyTree (props) {
+    console.log('props in family tree!')
+    console.log(props)
 
+    
     const [parents, setParents] = useState('')
     const [strains, setStrains] = useState('')
     const [trigger, setTrigger] = useState(0)
@@ -54,61 +56,101 @@ let i = 0;
 
     let counter = props.counter
     let inplay = props.inplay
-    let gamestateparents = props.parents
     let serverdata = props.serverdata
-
+    let gameprops = props.globalstate[0]
+    
     const dispatch = useDispatch()
     let result:any = useSelector(state => state)
     
     let localstore = result.gameReducer
     let reduxparents = result.gameReducer.parents
+    
+    // * meet the parents
+    let isInPlay = gameprops.inplay2[0]
+    let setIsInPlay = gameprops.inplay2[1]
+
+    console.log('isInPlay')
+    console.log(`isInplay ${isInPlay} type: ${typeof isInPlay}`)
+    
+    // * meet the parents
     let parent1 = result.gameReducer.parent1
     let parent2 = localstore.parent2
     
-    const checkState = () => {
-        
-    }
-    
-    
-    useEffect( () => {
-        
-    }, [trigger])
-    
-    useEffect( () => {
-        // setParents(inplay)
-    }, [])
-    
-    const checkredux = async () => {
-        let straindata:(object|string|number) = await APIcall('all', null, null)
-        let randomStrain = await Random(straindata)
-        let randomparents:string = randomStrain.parents
-        
-        let newstr = await Regex(reduxparents, 'stringsplit')
-        parent1 = dispatch( { type: 'SET_PARENTS', payload: { parents: randomparents}})        
-        parent1 = dispatch( { type: 'SET_PARENTS_1', payload: { parent1: newstr[0]}})        
-        parent2 = dispatch( { type: 'SET_PARENTS_2', payload: { parent2: newstr[1]}})        
+    let parent1state = gameprops.contextparents1[0]
+    let setParent1state = gameprops.contextparents1[1]
 
-            let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-            let randomletter:string = await Random(letters)            
-            setTrigger(trigger + 1)
-            await setRedux(true)
+    let parent2state = gameprops.contextparents2[0]
+    let setParent2state = gameprops.contextparents2[1]
+
+    useEffect( () => {
+        (async() => {
+            let straindata:(object|string|number) = await APIcall('all', null, null)
+            let randomStrain = await Random(straindata)
+            let randomparents:string = randomStrain.parents
+            setParents(randomparents)
+            parent1 = dispatch( { type: 'SET_PARENTS', payload: { parents: parents}})        
+        })()
+    })
+
+    const checkredux = async () => {
+            // * pass cactusHoverState up a level and see if img onMouseEnter={parent1 '' to reset container}
+            console.log('parent1state')
+            console.log(parent1state)
+            console.log(parent1state.length)
+
+                
+
+                // parent1 = dispatch( { type: 'SET_PARENTS_1', payload: { parent1: newstr[0]}})                        
+                // parent2 = dispatch( { type: 'SET_PARENTS_2', payload: { parent2: newstr[1]}})                        
+
+                    if (isInPlay.length < 3) {
+                        // await setParent1state('yoo')
+                        // await setParent2state('yeah sure')
+                        let newstr = await Regex(reduxparents, 'stringsplit')
+                        await setParent1state(newstr[0])
+                        await setParent2state(newstr[1])
+                        setTimeout( () => {
+                            setIsInPlay('sure')
+                        }, 1000)
+                    }
+                    // await setIsInPlay('key')
+
+                    
+                    // await setIsInPlay(true)
+ 
+                // inplay = dispatch( { type: 'PLAYING_GAME', payload: { game: 'play'}})                                        
+                //     let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+                //     let randomletter:string = await Random(letters)            
+                //     setTrigger(trigger + 1)
+                //     await setRedux(true)
+            // } 
+        } 
+
+        const changeit = () => {
+            setIsInPlay('state change')
+        }
+        
+        const checkit = () => {
+            console.log('check it')
+            console.log('isInPlay')
+            console.log(isInPlay)
         }
 
         
-    console.log('family tree component!')
 
+        
     return (
-        // <ShadowBorder>
-
-        // <div className={styles.div}  
-        // style= {{ backgroundColor: 'dodgerBlue', minHeight: '100vh'}}
-        // >       
-        <Container className={styles.div}>
-            <GameContainer checkredux={checkredux} redux={reduxparents} parent1={parent1} parent2={parent2}/>
-            {/* <button onClick={checkredux}></button>             */}
-        </Container>
-            
-        // </div>        
+        // <ShadowBorder>        
+        <Container 
+        className={styles.div}>
+            {/* <button onClick={changeit}/>
+            <button onClick={checkit}/> */}
+            <GameContainer    
+            game={gameprops}
+            style={{ minHeight: '80vh', minWidth: '80vw'}} 
+            checkredux={checkredux} redux={reduxparents} parent1={parent1state} parent2={parent2state}
+            />
+        </Container>                
     )   
 }
 
@@ -142,8 +184,6 @@ export async function getServerSideProps(context:any) {
     } 
     // let randomvalue = await Random(arraystate)
     let randomvalue = await randomNumber(index1, indexlast)
-    console.log('randomvalue')
-    console.log(randomvalue)
 
         let predata = await fetch(new URL(`${url}/api/strains/strain/${randomvalue}`))                    
         let serverdata = await predata.json()        
