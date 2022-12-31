b9efacb99d20e3b3b8c313413f06f33129286409
Frank Collins <fwc3rd@gmail.com>
2022-12-30 20:54:15 -0500

    state for guessing game [ 8:53pm ]
    the coins to guess with are labeled with id="coin1", "coin2", "coin3" "coin4"
    setting state for randomNumber between 1 - 4
    
    when you hover and drag the coin to the mine the name of the strain will pop up.
    
    will use regex to remove the id from {,.."coin1" / "coin4}
    
    if the id of the coin matches the randomnumber then it will be the coin with the correct answer

--- components/GameChild.tsx
+++ components/GameChild.tsx
@@ -8,51 +8,78 @@ import CSS from 'utility/CSStool'
 import AttrTool from 'utility/JqAttr'
 import Siblings from 'utility/JqSiblings'
 import Random from 'utility/Randomizer'
+import ReturnRight from 'utility/ReturnRight'
 import { Draggable, Droppable } from 'react-drag-and-drop'
 
 import Family from 'utility/familyJS'
+import { propTypes } from 'react-bootstrap/esm/Image'
 
-export default function GameChild () {
+export default function GameChild (props) {
 
     const [coin1, setCoin1] = useState(false)
     const [coin2, setCoin2] = useState(false)
     const [coin3, setCoin3] = useState(false)
     const [coin4, setCoin4] = useState(false)
+    const [yesNumber, setYesNumber] = useState(0)
+
+    // let i:number = 0
+    let numbers:number[] = [1, 2, 3, 4]
+    const [text, setText] = useState('')
+
+    useEffect( () => {
+        (async() => {
+            let randomnumber:number = await Random(numbers)
+            setYesNumber(randomnumber)
+            console.log('randomnumber')
+            console.log(randomnumber)
+        })()
+
+    }, [])
     
     const handleDragStart = async (event) => {
-        console.log('hello!')
         let target = event.target        
-        console.log( $(target).siblings() )
-        // $(target).css('border', '10px solid pink')
+        let targetid = event.target.id
+        // $(target).css('border', '10px solid pink')        
         let parents = await Family(target, 'parents')                
-        if (parents) {
-            
+        
+        if (parents) {            
             let dragParent = parents[0]
             let familytype = parents[0].attributes
             if (familytype[0].textContent === 'notcoin') {
 
             } else {
                 console.log("hey the type is coin")
-                setCoin1(true)
-                AttrTool(dragParent, 'type', 'notcoin')
-                AttrTool(dragParent, 'data', 'nice one')
-                await CSS(target, 'opacity', '0.1')
-            }
-            console.log('familytype')
-            console.log(familytype)
-        }
 
+                if (targetid === 'coin1') {
+                    await setCoin1(true)       
+                    // ReturnRight()             
+                }
+
+
+                if (targetid === 'coin2') setCoin2(true)
+                if (targetid === 'coin3') setCoin3(true)
+                if (targetid === 'coin4') setCoin4(true)
+                
+                await AttrTool(dragParent, 'type', 'notcoin')
+                await AttrTool(dragParent, 'data', '')                
+                await CSS(target, 'opacity', '0.1')
+                setText('ayee')
+                
+            }            
+        }
     }
 
     const nativeDrop = async (event) => {        
 
     }
 
-    const handleDrop = async (event) => {
-        console.log("okay were dropping")
-        console.log('event')
-        console.log(event)
-        console.log(event.target)                
+    const handleDrop = async (event) => {        
+
+    }
+
+    const mineclick = async () => {
+        console.log('mine click')
+        props.setCactusHover(false)
     }
 
 
@@ -61,6 +88,7 @@ export default function GameChild () {
         <div className="Column">
         <div className={styles.GameChild}>
 
+            {/* <Draggable type={"coin"} data="sourog">      */}
             <Draggable type={coin1 === false ? "coin" : "notcoin"} data="sourog">     
             <img
             id="coin1"
@@ -96,17 +124,26 @@ export default function GameChild () {
             className={styles.coin} src="img/coin.png" 
             />
             </Draggable>
-
      
         </div>  
+
+            <h6
+            style={{ color: 'papayawhip', letterSpacing: '0.75em'}}        
+            > you picked {text}  </h6>
 
             <Droppable types={['coin']} onDrop={handleDrop}>
             <img            
             draggable="true" 
+            onClick={mineclick}
             // onDrop={handleDrop}
             style={{ borderRadius: '50%', border: '2px solid papayawhip', boxShadow: '10px 10px 10px papayawhip'}}            
             className={styles.mine} src="img/mine.png"/>
             </Droppable>
+
+            <button onClick={() => console.log(yesNumber)} />
+
+            
+
 
         {/* <img style={{ border: '5px solid papayawhip', transform: `scale(0.2)` }} src="/img/mine.png"/> */}
         </div>
