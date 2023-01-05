6bea987d6dff5a16fbab2b9f7d63c037b7b90959
Frank Collins <fwc3rd@gmail.com>
2023-01-05 11:05:10 -0500

    type aliases and specified defaults [11:05am]

--- Contexts/GameContext.ts
+++ Contexts/GameContext.ts
@@ -1,4 +1,25 @@
-import { createContext } from 'react'
-const GameContext = createContext( { wow: 'nice' } )
-// const GameContext = createContext( {} )
-export default GameContext
+import { useContext, createContext } from 'react'
+
+type gameContextType = {
+    gameOn: boolean, 
+    // setGameOn: (setGameOn: (value: string) => void) => void,
+}
+
+const gamecontextdefaults: gameContextType = {
+    gameOn: false
+}
+
+const gameContext = createContext<gameContextType>(gamecontextdefaults)
+console.log('gameContext')
+console.log(gameContext)
+
+
+// * gameOn, setGameOn, parents, setParents, parent1, setParent1,
+// * parent2, setParent2, winStreak, setWinStreak, wrongGuess, setWrongGuess
+
+
+
+
+
+
+
