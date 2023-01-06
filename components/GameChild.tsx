ef72663b1be20ddf257f52c286ae56a228acb324
Frank Collins <fwc3rd@gmail.com>
2023-01-06 13:34:26 -0500

    [GameChild] UI now responds to state changes [1:34pm]
    when you drag the coin,
     it successfully picks the correct strain.name thats based on the strain.parents presented as the container displayed options to guess from

--- components/GameChild.tsx
+++ components/GameChild.tsx
@@ -9,6 +9,7 @@ import AttrTool from 'utility/JqAttr'
 import Siblings from 'utility/JqSiblings'
 import Random from 'utility/Randomizer'
 import Regex from 'utility/MasterRegex'
+import Endpoint from 'utility/StrainEndpoint'
 import ReturnRight from 'utility/ReturnRight'
 import { Draggable, Droppable } from 'react-drag-and-drop'
 
@@ -19,30 +20,45 @@ import { propTypes } from 'react-bootstrap/esm/Image'
 import store from 'redux/store'
 import { useDispatch, useSelector } from "react-redux";
 
+// * context
+import { useGame } from 'Contexts/game'
+
+
 export default function GameChild (props) {
+
 
     const [coin1, setCoin1] = useState(false)
     const [coin2, setCoin2] = useState(false)
     const [coin3, setCoin3] = useState(false)
     const [coin4, setCoin4] = useState(false)
     const [yesNumber, setYesNumber] = useState(0)
+    const [guessText, setGuessText] = useState('')
+
+    const[hoverCoin, setHoverCoin] = useState('')
+
+    const {
+        gameOn, playing, notplaying,
+         parents, meetTheParents, 
+         parent1, parent1state, parent2, parent2state 
+         } = useGame()
+// * these context variables are working facilitate guessing with the coins.
+// * the coins will need labels with: [ReturnRight() && ReturnWrong] depending on if coin matches up.
+// * import { useGame } from 'Contexts/game'
+
 
     let reduxstore:any = useSelector(state => state)
-    let rootparents:string = reduxstore.gameReducer.parents
 
-    console.log('reduxstore')
-    console.log(reduxstore)
+    
+    // let parents:string = props.rootparents
+    // let rootparents = parents
 
-    // let i:number = 0
     let numbers:number[] = [1, 2, 3, 4]
-    const [text, setText] = useState('')
+
 
     useEffect( () => {
         (async() => {
             let randomnumber:number = await Random(numbers)
-            setYesNumber(randomnumber)
-            console.log('randomnumber')
-            console.log(randomnumber)
+            setYesNumber(randomnumber)            
         })()
 
     }, [])
@@ -51,13 +67,13 @@ export default function GameChild (props) {
         let target = event.target        
         let targetid = event.target.id
         // $(target).css('border', '10px solid pink')        
-        let parents = await Family(target, 'parents')                
+        let familyparents = await Family(target, 'parents')                
         
-        if (parents) {            
-            let dragParent = parents[0]
+        if (familyparents) {            
+            let dragParent = familyparents[0]
             console.log('dragParent')
             console.log(dragParent)
-            let familytype = parents[0].attributes
+            let familytype = dragParent.attributes
             let coinid = await Regex(targetid, 'numreturn')
             let coinint:number = parseInt(coinid)
             console.log('coinid')
@@ -68,49 +84,65 @@ export default function GameChild (props) {
                 console.log("hey the type is coin")
 
                 if (targetid === 'coin1') {
-                    await setCoin1(true)    
-                    if (coinint === yesNumber) {
-                        let rightparents = await ReturnRight(rootparents)
-                        console.log('rightparents')
-                        console.log(rightparents)
+                    console.log('parents coin1')
+                    console.log(parents)
+                    let newparents = await ReturnRight(parents)                                            
+                    setGuessText(newparents)
+                    
+                    // setGuessText(newparents)
+                    console.log(`newparents ${newparents} typeof newparents ${typeof newparents}`)
 
-                        console.log(` YES coinid ${coinid} ${typeof coinid} yesNumber ${yesNumber} ${typeof yesNumber}`)
+                    await setCoin1(true)    
+                    if (coinint === yesNumber) {                        
+                        
                     } else {
-                        console.log(` NO!!! coinid ${coinid} ${typeof coinid} yesNumber ${yesNumber} ${typeof yesNumber}`)
+                        console.log(`NO!!! coinid ${coinid} ${typeof coinid} yesNumber ${yesNumber} ${typeof yesNumber}`)
                     }
                     // ReturnRight()             
                 }
                 
                 if (targetid === 'coin2') {
+                    console.log('parents coin2 ')
+                    console.log(parents)
                     setCoin2(true)
+                                        
                     if (coinint === yesNumber) {
-                        console.log(` YES coinid ${coinid} ${typeof coinid} yesNumber ${yesNumber} ${typeof yesNumber}`)
+                        let newparents = await ReturnRight(parents)                                            
+                        setGuessText(newparents)                        
+                        // let newparents = await ReturnRight(parents)                                                
                     } else {
                         console.log(` NO!!! coinid ${coinid} ${typeof coinid} yesNumber ${yesNumber} ${typeof yesNumber}`)
                     }
                 }
                 if (targetid === 'coin3') {
                     setCoin3(true)
+                    console.log('parents for coin3')
+                    console.log(parents)
+                    let newparents = await ReturnRight(parents)                                            
+                    console.log(`newparents ${newparents} typeof newparents ${typeof newparents}`)
+
                     if (coinint === yesNumber) {
-                        console.log(` YES coinid ${coinid} ${typeof coinid} yesNumber ${yesNumber} ${typeof yesNumber}`)
+                        let newparents = await ReturnRight(parents)                                            
+                        setGuessText(newparents)                        
                     } else {
                         console.log(` NO!!! coinid ${coinid} ${typeof coinid} yesNumber ${yesNumber} ${typeof yesNumber}`)
                     }
                 }
                 if (targetid === 'coin4') {
+                    let newparents = await ReturnRight(parents)                                            
+                    setGuessText(newparents)                                            
                     setCoin4(true)
+                    console.log(`newparents ${newparents} typeof newparents ${typeof newparents}`)
                     if (coinint === yesNumber) {
-                        console.log(` YES coinid ${coinid} ${typeof coinid} yesNumber ${yesNumber} ${typeof yesNumber}`)
+                        
                     } else {
-                        console.log(` NO!!! coinid ${coinid} ${typeof coinid} yesNumber ${yesNumber} ${typeof yesNumber}`)
+                        console.log(`NO!!! coinid ${coinid} ${typeof coinid} yesNumber ${yesNumber} ${typeof yesNumber}`)
                     }
                 }
                 
                 await AttrTool(dragParent, 'type', 'notcoin')
                 await AttrTool(dragParent, 'data', '')                
-                await CSS(target, 'opacity', '0.1')
-                setText('ayee')
-                
+                await CSS(target, 'opacity', '0.1')                            
             }            
         }
     }
@@ -123,8 +155,7 @@ export default function GameChild (props) {
 
     }
 
-    const mineclick = async () => {
-        console.log('mine click')
+    const mineclick = async () => {        
         props.setCactusHover(false)
     }
 
@@ -151,6 +182,7 @@ export default function GameChild (props) {
             onDragStart={handleDragStart}         
             className={styles.coin} src="img/coin.png" 
             />
+            <h1 style={{ color: 'white'}}></h1>
             </Draggable>
             
             <Draggable type={coin3 === false ? "coin" : "notcoin"} data="sourog">     
@@ -174,8 +206,11 @@ export default function GameChild (props) {
         </div>  
 
             <h6
-            style={{ color: 'papayawhip', letterSpacing: '0.75em'}}        
-            > you picked {text}  </h6>
+            style={ { color: 'papayawhip', letterSpacing: '0.75em'} }        
+            > you picked {guessText}  </h6>
+
+            
+            
 
             <Droppable types={['coin']} onDrop={handleDrop}>
             <img            
@@ -186,7 +221,14 @@ export default function GameChild (props) {
             className={styles.mine} src="img/mine.png"/>
             </Droppable>
 
-            <button onClick={() => console.log(yesNumber)} />
+            {/* <button 
+            style={{ 
+            borderRadius: '50%', backgroundColor: 'limegreen',
+             height: '50px', width: '50px'
+            }}
+            onClick={() => console.log(parents)}></button> */}
+
+            
 
             
 
