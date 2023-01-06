04c5d9a363fa47cf7723b21ae5f125bcc13525a6
Frank Collins <fwc3rd@gmail.com>
2023-01-05 19:40:04 -0500

    APIcall() and Random() modular funcs [7:39pm]
    parents function now accesses the modular:
    API calling function that retrieves strains
    Random() from /utility/Randomizer that accepts an array argument, in this case the data retrieved from the API call,
    
    and then grabs the 
    
    randomstrain.parents
    
    this is the root string that gets parents.split()
    so that 1 part of the text is appended to the one component 
    
    and the other part of the string that is returned from str.split()[1] is appended to the right side of the container so they both show the text as 
    
    displayed options from which the game is created
    
    UI passes the test on text rendered and reacting to state change with change of rendered text as well

--- Contexts/game.tsx
+++ Contexts/game.tsx
@@ -1,15 +1,27 @@
 import { createContext, useContext, ReactNode, useState } from "react";
+import APIcall from 'utility/APIcall'
+import Random from 'utility/Randomizer'
 
 type gameContextType = {
     gameOn: string;
     playing: () => void;
     notplaying: () => void;
+
+    parents: string;
+    meetTheParents: () => void;
 };
+
+//     gameOn: gameOn, setGameOn: setGameOn, parents: parents, setParents: setParents,
+    //     parent1: parent1, setParent1: setParent1, parent2: parent2, setParent2: setParent2,
+    //     winStreak: winStreak, setWinStreak: setWinStreak, wrongGuess: wrongGuess, setWrongGuess: setWrongGuess
 
 const gameDefaults: gameContextType = {
     gameOn: 'not playing',
     playing: () => {},
-    notplaying: () => {}
+    notplaying: () => {},
+
+    parents: 'no parents yet',
+    meetTheParents: () => {}
 };
 
 const GameContext = createContext<gameContextType>(gameDefaults);
@@ -25,6 +37,7 @@ type Props = {
 export function GameProvider({ children }: Props) {
     const [user, setUser] = useState<string>('no user');
     const [gameOn, setGameOn] = useState<string>('not playing');
+    const [parents, setParents] = useState<string>('no parents yet')
 
     const playing = () => {
         setGameOn('playing')
@@ -33,11 +46,29 @@ export function GameProvider({ children }: Props) {
     const notplaying = () => {
         setGameOn('not playing')
     }
+
+    const meetTheParents = async () => {
+        // setParents("oh nice")
+        let strains:(object|string) = await APIcall('all', null, null)
+        let randomstrain:any = await Random(strains)
+        // let randomstrain:(object|string) = await Random(strains)
+        let parents = randomstrain.parents
+        
+        console.log('randomstrain')
+        console.log(randomstrain)
+        
+        console.log('parents')
+        console.log(parents)
+        
+    }
     
     const value = {
         gameOn, 
         playing,
-        notplaying
+        notplaying,
+
+        parents,
+        meetTheParents
     };
 
     return (
