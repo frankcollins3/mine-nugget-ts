c4a80b78829d859be4af6d27e1c8a2f9b25a7733
Frank Collins <fwc3rd@gmail.com>
2023-01-06 10:35:16 -0500

    Union Type/Tuple/2-type-array [10:34pm]
    👎  :(string|object)[] = []
    
    👍  :string || arr:(string)[] = []
    
    exercise in spending a few minutes thinking about how the types would be used would've saved 40 minutes.
    
    typescript is a good bit more than:
    let name:string = data.name

--- utility/ReturnRight.ts
+++ utility/ReturnRight.ts
@@ -3,35 +3,30 @@
         let allstrain = await APIcall('all', null, null)  // let allstrain:(object|string) = await APIcall('all', null, null) 
         console.log('parents in the ReturnRight')
         console.log(parents)
+
+
     
-        let parentarray:(string|object)[] = []
+        let parentarray:(string)[] = []
         // let parentarray:any = []
         const loopandpush = () => {
             allstrain.forEach( (strain) => {
-            // allstrain.forEach( (strain:(object|string)) => {
-        
-                if (strain.parents === parents) {
-        
-                    console.log("it includes parents alright!")
-                    console.log(`strainparents: ${strain.parents} parents: ${parents}`)
-                    console.log(strain)
-                    // return strain
+            // allstrain.forEach( (strain:(object|string)) => {        
+                if (strain.parents === parents) {                            
                     parentarray.push(strain.strain)
                 } else {
-                    console.log('nope')
-                    // return
-                    // console.log("it doesn't include those parents")
-                    // console.log('parents')
-                    // console.log(parents)
+                    console.log('nope')                    
                 }
             })
         }
+        await loopandpush()
+        console.log(`parentarray: ${parentarray} type: ${typeof parentarray[0]}`)
+        return parentarray[0]
         
-        const returnvalue = async () => {
-            await loopandpush()
-            return parentarray[0]
-        }
-        return returnvalue()
+        // const returnvalue = async () => {
+        //     await loopandpush()
+        //     return parentarray[0]
+        // }
+        // return returnvalue()
     }
             // return parentarray
             // let parents:(object|string) = strain.parents        
