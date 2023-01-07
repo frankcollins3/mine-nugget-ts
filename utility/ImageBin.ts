e0c73d27978dcf21324ea298705f013185283350
Frank Collins <fwc3rd@gmail.com>
2023-01-07 14:55:17 -0500

    [🛢imageBin ] just for game state  [2:54pm]
    modular function that accepts a string argument that returns a specified string-img-url
    could have thought of this before

---
+++ utility/ImageBin.ts
@@ -0,0 +1,5 @@
+export default async function Images (img:string) {
+    if (img === 'gold') return '/img/gold.png'
+    if (img === 'ring') return '/img/ring.png'
+    if (img === 'watch') return '/img/watch.png'
+}
