b97cc97f21f12b6f0db41807024e8d2f685177b3
Frank Collins <fwc3rd@gmail.com>
2023-01-07 14:46:15 -0500

    using useEffect almost like post route send state thru [2:46pm]
    aiming to change the background of the images and swap them.
    the Ring and watch representing the parents will become goldbars 
    
    and the goldbars by the text where the options were displayed will become the ring and the watch (if it looks okay)

--- components/ParentRing.tsx
+++ components/ParentRing.tsx
@@ -1,12 +1,55 @@
-
-import { propTypes } from "react-bootstrap/esm/Image";
+import Container from 'react-bootstrap/Container'
+import { useEffect, useState } from 'react'
+import { useGame } from 'Contexts/game'
+import $ from 'jquery'
+import AttrTool from 'utility/JqAttr'
+import Images from 'utility/ImageBin'
 
 export default function ParentRing (props) {
+    const [images, setImages] = useState<string[]>([])
+
+    const {
+        // gameOn, playing, notplaying,
+        //  parents, meetTheParents, 
+        //  parent1, parent1state, parent2, parent2state, clearparent1, clearparent2,
+        //  dontuse, fillbucket, emptybucket,
+         winStreak,
+        //   winstreakincrement, wrongGuess, guesswrongincrement,
+        //   trophy, addTrophy, 
+         } = useGame()
+    let ring = $('#Ring')
+
+    useEffect( () => {
+        (async() => {
+        //  let images = await Images()
+        })()
+    }, [])
+
+    // let goldimg = Images('gold')
+    console.log('goldimg')
+    console.log(goldimg)
+    
+    // AttrTool(ring, 'src', goldimg)
+
+    useEffect( () => {
+        // AttrTool(ring, 'src', 'gold.png')        
+
+    }, [winStreak])
+
     return (
-        <div
+        <Container
+        className="RingWatch"
+        id="Ring"
         style={{ backgroundImage: `url('img/ring.png')`}}
-         className="RingWatch">
-            <p> {props.parents || 'nothing'} </p>
-        </div>
+        // style={{ backgroundImage: `url('img/ring.png')`, backgroundSize: '3%'}}
+        
+        >            
+        {/* <div */}
+        {/* // style={{ backgroundImage: `url('img/ring.png')`}} */}
+        {/* className="RingWatch"> */}
+            {/* <p> {props.parents || 'nothing'} </p> */}
+            
+        {/* </div> */}
+        </Container>
     )
 }
