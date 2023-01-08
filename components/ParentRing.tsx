7c20869ff419d6adb0becc58e3bc927f0c91e38c
Frank Collins <fwc3rd@gmail.com>
2023-01-07 22:22:58 -0500

    [ParentRing] lose img upon loss [10:21pm]
    i kind of like background repeating; it looks more like widespread fire
    
    if (wrongGuess === 2) ---- >
    guess twice and lose: changeImg()

--- components/ParentRing.tsx
+++ components/ParentRing.tsx
@@ -1,60 +1,59 @@
-import Container from 'react-bootstrap/Container'
-import { useEffect, useState } from 'react'
-import { useGame } from 'Contexts/game'
-import $ from 'jquery'
-import AttrTool from 'utility/JqAttr'
-import CSS from 'utility/CSStool'
-import Images from 'utility/ImageBin'
+    import Container from 'react-bootstrap/Container'
+    import { useEffect, useState } from 'react'
+    import { useGame } from 'Contexts/game'
+    import $ from 'jquery'
+    import AttrTool from 'utility/JqAttr'
+    import CSS from 'utility/CSStool'
+    import Images from 'utility/ImageBin'
 
-export default function ParentRing (props) {
-    const [imgbucket, setImgbucket] = useState([])
-    const [gameimg, setGameimg] = useState('')    
+    export default function ParentRing (props) {
+        const [imgbucket, setImgbucket] = useState([])
+        const [gameimg, setGameimg] = useState('')    
 
-    const {
-        // gameOn, playing, notplaying,
-        //  parents, meetTheParents, 
-        //  parent1, parent1state, parent2, parent2state, clearparent1, clearparent2,
-        //  dontuse, fillbucket, emptybucket,
-         winStreak,
-        //   winstreakincrement, wrongGuess, guesswrongincrement,
-        //   trophy, addTrophy, 
-         } = useGame()
-    let ring = $('#Ring')
+        const {
+            // gameOn, playing, notplaying,
+            //  parents, meetTheParents, 
+            //  parent1, parent1state, parent2, parent2state, clearparent1, clearparent2,
+            //  dontuse, fillbucket, emptybucket,
+            winStreak, wrongGuess
+            //   winstreakincrement, wrongGuess, guesswrongincrement,
+            //   trophy, addTrophy, 
+            } = useGame()
+        let ring = $('#Ring')
 
-    useEffect( () => {
-        (async() => {
-        let allimages = await Images('all')
-        // setImgbucket(allimages)
-                
-        let gold = await Images('gold')            
-        setGameimg(gold)
-    })()
-}, [])
+        useEffect( () => {
+            (async() => {
+            let allimages = await Images('all')                    
+            let gold = await Images('gold')            
+        })()
+    }, [])
 
-    //  let imagebucket:string|any[] = [goldbarimg, ringimg, watchimg]
-    // let goldimg = Images('gold')
-    
-    
-    // AttrTool(ring, 'src', goldimg)
+        useEffect( () => {
+            const changeimg = async () => {
+                let gold = await Images("gold")        
+                CSS(ring, 'background-image', `url('${gold}')`)        
+            }
+            changeimg()
+        }, [winStreak])
 
-    useEffect( () => {
-        // AttrTool(ring, 'src', 'gold.png')        
-        
-        // AttrTool(ring, 'src', '/public/img/gold.png')
-        CSS(ring, 'background-image', `url('${img}')`)
-        // $(ring).css('border', '10px solid hotpink')
-        // AttrTool(ring, 'src', currimg)
-    }, [winStreak])
+        useEffect( () => {
+            if (wrongGuess === 2) {
+                const change = async () => {
+                    let firetag = await Images('firetag')
+                    CSS(ring, 'background-image', `url('${firetag}')`)
+                }
+                change()
+            }
+        }, [wrongGuess])
 
-    return (
-        <>
-        <Container
-        className="RingWatch"
-        id="Ring"
-        style={{ backgroundImage: `url('img/ring.png')`}}        
-        >            
-        </Container>
-        <button onClick={() => console.log("hey") }></button>
-        </>
-    )
-}
+        return (
+            <>
+            <Container
+            className="RingWatch"
+            id="Ring"
+            style={{ backgroundImage: `url('img/ring.png')`}}        
+            >            
+            </Container>        
+            </>
+        )
+    }
