d796a0d357fb8290a99391031832fe8ccb6165d1
Frank Collins <fwc3rd@gmail.com>
2023-01-10 17:59:26 -0500

    [💋sass] && styled components [5:58pm]
    For Search page that accesses relational data on a join table between [ User.id && strain.id ]

---
+++ styles/findmine/components/SearchContainer.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+    min-height: 400px;
+    min-width: 400px;
+    border: 5px solid pink;
+`
+export default Container
---
+++ styles/findmine/components/SearchPage.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Page = styled.div`
+    min-height: 100vh;
+    min-width: 100vw;
+    background-color: rgb(62, 50, 32); 
+    border: 15px solid orange;
+ 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+export default Page
