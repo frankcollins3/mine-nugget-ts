71c26f5610042f3406fe022bcf87c50fbabd50db
Frank Collins <fwc3rd@gmail.com>
2023-01-10 21:10:53 -0500

    [FindMine] Search page begun [9:00pm]

--- pages/findmine.tsx
+++ pages/findmine.tsx
@@ -1,7 +1,30 @@
+import Page from 'styles/findmine/components/Searchpage'
+import styles from 'styles/findmine/sass/FindMine.module.scss'
+import Helmet from 'components/Helmet'
+import Magnify from 'components/Magnify'
+
+// components/Helmet.tsx
+
 function FindMine () {
     return (
-        <div> 
-            <h1> hello from Find Mine </h1>
+        <Page>
+        <div className={styles.row}>
+
+        <Helmet/>
+        <Magnify/>
+
         </div>
+        
+        {/* <h1 style={{ color: 'papayawhip', fontFamily: 'papyrus' }}> Find Mine </h1> */}'
+        <div className="Row">
+            <h1 className={styles.h1}> Find </h1>
+            <img src="/img/mine.png"/>
+            <h1 className={styles.h1}> Mine </h1>
+            
+        </div>
+
+        </Page>
     )
 }
+export default FindMine
+
