c3b313b5cacd34da223a5a7768c3c82040295434
Frank Collins <fwc3rd@gmail.com>
2023-01-11 21:54:51 -0500

    [Magnify] access redux store [9:53pm]
    also made global state to hold the values of the redux store. 
    anti-DRY measure but will remove the local-file-state when the boolean toggling redux-data manipulating hover function is working strictly in redux

--- components/Magnify.tsx
+++ components/Magnify.tsx
@@ -3,10 +3,40 @@ import Container from 'react-bootstrap/Container'
 import { useState, useEffect } from 'react'
 import $ from 'jquery'
 
+import store from 'redux/store'
+import { useDispatch } from "react-redux";
+import customReduxSelector from 'redux/reduxselector'
+import {connect} from 'react-redux';
+import { TypedUseSelectorHook, useSelector } from 'react-redux'
+import {Provider} from 'react-redux';
+
+let reduxstore = store.getState()
+
 export default function Magnify () {
     let style = ["style1", "style3", "style4"];
     let tam = ["tam1", "tam1", "tam1", "tam2", "tam3"];
     let opacity = ["opacity1", "opacity1", "opacity1", "opacity2", "opacity2", "opacity3"];
+
+    const [hover, setHover] = useState(false)
+    const [reduxBucket, setReduxBucket] = useState([])
+
+    useEffect( () => {
+        console.log('reduxstore from the useEffect')
+        let gameReducer = reduxstore.gameReducer
+        console.log(reduxstore.gameReducer)
+        setReduxBucket(gameReducer)
+    }, [])
+
+    
+
+        // let reduxstate = useSelector(state => state)
+        // console.log(reduxstate)
+
+    
+    
+    
+
+
   
     function getRandomArbitrary(min, max) {
       return Math.floor(Math.random() * (max - min)) + min;
@@ -23,8 +53,8 @@ export default function Magnify () {
         // let width:number = parseInt($('#Cont').width().toFixed())
 
         night.innerHTML = sparkle;
-        let widthWindow = window.innerWidth
-        let heightWindow = window.innerHeight;
+        let widthWindow = 150;
+        let heightWindow = 150;
         for (let i = 0; i < numsparkles; i++) {
           sparkle += "<span class='sparkle " + style[getRandomArbitrary(0, 4)] + " " + opacity[getRandomArbitrary(0, 6)] + " "
           + tam[getRandomArbitrary(0, 5)] + "' style='animation-delay: ." +getRandomArbitrary(0, 9)+ "s; left: "
@@ -32,18 +62,22 @@ export default function Magnify () {
         }
     }, [])
 
+    const stateHandler = () => {
+        console.log('reduxBucket from the hover function')
+        console.log(reduxBucket)
+    }
+
     return (
-        <>
-        {/* <div className={styles.div}> */}
-        {/* <img src="/img/helmet.png" className={helmet} ></img>
-        <img src="/img/barrier.png" className={barrier}></img>             */}
-        {/* <Container> */}
-        
-        <div id="Cont" className={styles.MagnifyCont}>
-            <div className="night">
-                <div className="constellation"></div>
+        <>        
+        <div id="Cont" onMouseEnter={stateHandler} onMouseLeave={() => setHover(false)}
+        // <div id="Cont" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}
+         className={styles.MagnifyCont}>                          
+            <div style={{ display: hover === true ? 'none' : 'flex'}} className="night">
+                <div  style={{ display: hover === true ? 'none' : 'flex'}}  className="constellation">
+                </div>
             </div>
         </div>
+            
         
         {/* </Container> */}
         {/* </div>             */}
