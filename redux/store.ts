b9c315e00ad929a39432e6855b55017af61edfca
Frank Collins <fwc3rd@gmail.com>
2023-01-11 22:00:37 -0500

    update redux store with new boolean state [10:00pm]
    this is for:
    /pages/FindMine.tsx
    
    hover on the magnifying glass and change the state.
    this state will also control the visible-display-toggling of a label 
    
    this label will inform the user that they can enter a letter to search for strains that they have already saved to the database.

--- redux/store.ts
+++ redux/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { defaultConfig } from 'next/dist/server/config-shared'
 import { combineReducers } from 'redux'
 import APIcall from 'utility/APIcall'
 import Random from 'utility/Randomizer'
@@ -7,11 +8,15 @@ const {createStore} = require("redux")
 
 const initState = {
     counter: 77,
-    inplay: false,
+    inplay: 'false',
     parents: '',
+    parent1: '',
+    parent2: '',
     winstreak: 0,
-    wrongguess: 0
+    wrongguess: 0,  
+    magnifyhover: false  
 }
+    
 const gameReducer = (state=initState, action) => {
         if(action.type === "INCREMENT") {
         console.log('state from the increment')
@@ -25,21 +30,33 @@ const gameReducer = (state=initState, action) => {
         return state
     }
     if (action.type === "PLAYING_GAME") {
-        state.inplay = true
-        return state
+        if (action.payload.game === 'play') {
+            console.log("play in the action dispatch")
+            state.inplay = 'true'
+            return state
+        }
     }
     if (action.type === "NOT_PLAYING_GAME") {
-        state.inplay = false
+        state.inplay = 'true'
         return state
     }
     if (action.type === "SET_PARENTS") {                
                 state.parents = action.payload.parents
-                return state        
+                return state         
     }
     if (action.type === "CLEAR_PARENTS") {
             state.parents = ''
             return state
     }
+    if (action.type === "SET_PARENTS_1") {                        
+        state.parent1 = action.payload.parent1        
+        return state         
+    }
+    if (action.type === "SET_PARENTS_2") {                
+        state.parent2 = action.payload.parent2
+        return state         
+    }
+    
     // return action.type === "SET_PARENTS" ? action.payload.parents : 'hey';
 
     if (action.type === "WIN_STREAK") {
@@ -62,8 +79,15 @@ const gameReducer = (state=initState, action) => {
     if (action.type === "GUESS_RESET") {
         state.wrongguess = 0
         return state
+    }    
+    if (action.type === 'MAGNIFY_ON') {
+        state.magnifyhover = true
+        return state
     }
-
+    if (action.type === 'MAGNIFY_OFF') {
+        state.magnifyhover = false
+        return state
+    }
     else return state
 }
 
@@ -72,5 +96,8 @@ const rootReducer = combineReducers({
 })
 
 const store = createStore(rootReducer)
+// const store = configureStore({
+//     reducer: rootReducer
+// })
 
 export default store
