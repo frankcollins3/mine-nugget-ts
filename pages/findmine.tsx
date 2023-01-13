ab6fbaa379c75f055bf6f1e7095d3deb8ca294dd
Frank Collins <fwc3rd@gmail.com>
2023-01-12 22:29:08 -0500

    [FindMine] serversideprops [10:29pm]
    creating a post route for userstrains again

--- pages/findmine.tsx
+++ pages/findmine.tsx
@@ -6,17 +6,23 @@ import { useState, useEffect } from 'react'
 import $ from 'jquery'
 import Container from 'react-bootstrap/Container'
 import {useGame} from 'Contexts/game'
-// import {useGame} from 'Contexts/find'
 
-// import {UseSearch} from 'Contexts/FINDMINE'
+// * utility
+import ReturnUrl from 'utility/ReturnUrl'
 
-export default function FindMine () {
+export default function FindMine (props) {
+    console.log('props from FindMine')
+    console.log(props)
+    console.log('props.data')
+    console.log(props.data)
+
+
     const { gameOn, playing, searchHover, hoverOnSearch  } = useGame()
     // const {gameOn, playing, searchHover } = useGame()
     
 
     useEffect( () => {
-        const textEdit = () => {        // i would put turn this abstract expression into a modular reusable function but it won't be reused again.
+        const textExit = () => {        // i would put turn this abstract expression into a modular reusable function but it won't be reused again.
             $('.HoverHintHeader')
             .animate({
                 opacity: 0.9
@@ -37,43 +43,45 @@ export default function FindMine () {
                 opacity: 0.0
             }, 250)
         }
+        textExit()
         // }).complete( (elem) => $(elem).hide() )
     }, [])
 
-    const checkit = () => {        
-        console.log('gameOn')
-        console.log(gameOn)
+    const checkit = () => {                
         playing()
         hoverOnSearch()
     }
 
-    return (
-
-        
+    return (        
         <Page>       
-        <Container className={styles.row}>        
-        <Helmet/>
-        {/* <Helmet/> */}
+
+        <Container className={styles.row}>     
         
-        <div className="Column">
+
+        <div className="Column">        
+        <h1 className={styles.h1} style={{ color: 'papayawhip', fontFamily: 'papyrus' }}> Find Mine </h1>'
         <Magnify/>
             <h6 className="HoverHintHeader"
              style={{ color: 'papayawhip'}}> Hover on the Magnifying Glass <br/> Press a letter to search  </h6>
         </div>
-        
-        </Container>
 
-        {/* <div className="Row">
-            <h1 className={styles.h1}> Find </h1>
-            <img src="/img/mine.png"/>
-            <h1 className={styles.h1}> Mine </h1>         
-        </div> */}
-        
-        {/* <h1 style={{ color: 'papayawhip', fontFamily: 'papyrus' }}> Find Mine </h1> */}'
+        </Container>        
         </Page>
     )
-
+        
 }
+
+export async function getServerSideProps(context:any) {              
+    let url:any = await ReturnUrl(context);    
+    // let pokeurl = `https://pokeapi.co/api/v2/pokemon/`    
+    let predata = await fetch(new URL(`${url}/api/strains/strain`))            
+    let data = await predata.json()        
+  return {
+  props: {
+      data    
+    }
+  };
+  }
 
 
 
