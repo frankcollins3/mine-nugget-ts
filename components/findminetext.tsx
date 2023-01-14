34213919c52122986dc5a31a65159ee877d9ccff
Frank Collins <fwc3rd@gmail.com>
2023-01-14 03:27:52 -0500

    [row] considering animation for text[3:27am]
    probably easier from its own component

---
+++ components/findminetext.tsx
@@ -0,0 +1,16 @@
+export default function findminetext () {
+    return (
+        <>
+        <div className="row">
+        <p> F </p>
+        <p> i </p>
+        <p> n </p>
+        <p> d </p>
+        <p> M </p>
+        <p> i </p>
+        <p> n </p>
+        <p> e </p>
+        </div>
+        </>
+    )
+}
