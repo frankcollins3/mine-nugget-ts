a18a890b51710708ee8e602864f866cfe2827cbd
Frank Collins <fwc3rd@gmail.com>
2023-01-14 00:26:38 -0500

    API and first letter searching [12:25am]
    loops against api and returns an array that contains all [strain.name] that share a first letter of the user-inputted search 
    which would be in this modular function parameter

---
+++ utility/firstLetterSearch.ts
@@ -0,0 +1,30 @@
+import APIcall from 'utility/APIcall'
+
+
+export default function FirstLetter (char:string) {
+    let namebucket:string[] = []
+
+    if (char) {
+        const firstLetterLoop = async () => {
+            let allstrains = await APIcall('all', null, null)
+            allstrains.forEach( (strain:any) => {
+            // allstrains.forEach( (strain:(string|object)) => {
+                let strainName:string = strain.strain
+                let firstLetter = strainName.slice(0, 1)
+                console.log(firstLetter)
+                if (firstLetter === char) {
+                    console.log(`firstLetter ${firstLetter} char ${char}`)
+                    namebucket.push(strainName)
+                }
+            })
+        }
+        const returnBucket = () => {
+            return namebucket
+        }
+        const doubleUP = async () => {
+            await firstLetterLoop
+            await returnBucket
+        }
+        doubleUP()
+    } else return
+}
