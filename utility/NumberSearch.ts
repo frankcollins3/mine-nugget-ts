1b692bd7c16fdac31a7ed2f4a6ca8f6ffd703cd3
Frank Collins <fwc3rd@gmail.com>
2023-01-16 17:37:29 -0500

    little more civilized [5:36pm]
    increment limit set by function parameter which would be the number entered on a keyboard.
    it would only be a number because it would survive the Regex from gobbling all alpha numeric character and leaving behind populated or not number string only

--- utility/NumberSearch.ts
+++ utility/NumberSearch.ts
@@ -1,29 +1,54 @@
 import APIcall from 'utility/APIcall'
 
-export default async function NumberSearch (num:string|number) {
+export default async function NumberSearch (num) {
+// export default async function NumberSearch (num:string|number|any) {
     let strains = await APIcall('all', null, null)
     let number;
+    let i:number = 0;
+    let strainbucket = new Array()
+    console.log('num in the utility function')
+    console.log(num)
     if (typeof num === 'string') number = parseInt(num)
-    if (number === 1 || 0) {
-        return strains[0]
+    // if (number === 1) {
+        
+    //     return strains[0]
+    // }
+    // if (number === 2) {
+    //     return [strains[0], strains[1]]
+    // }
+    // if (number === 3) {
+    //     return [strains[0], strains[1], strains[2]]
+    // }
+    // if (number === 4) {
+    //     return [strains[0], strains[1], strains[2], strains[3]]
+    // }
+    // if (number === 5) {
+    //     return [strains[0], strains[1], strains[2], strains[3], strains[4]]
+    // }
+    // if (number === 6) {
+    //     return [strains[0], strains[1], strains[2], strains[3], strains[4], strains[5]]
+    // }
+    // if (number === 7) {
+    //     return [strains[0], strains[1], strains[2], strains[3], strains[4], strains[5], strains[6]]
+    // }    
+    const loopandpush = () => {
+        while (i < number) {
+            let incrementstrain = strains[i]
+            console.log('incrementstrain')
+            console.log(incrementstrain)
+            strainbucket.push(incrementstrain)
+            i++
+        }
     }
-    if (number === 2 || 0) {
-        return [strains[0], strains[1]]
+    const checkAndReturn = async () => {
+        console.log('strainbucket in the checkAndReturn function')
+        console.log(strainbucket)
+        return strainbucket
     }
-    if (number === 3 || 0) {
-        return [strains[0], strains[1], strains[2]]
+    const bothfunctions = async () => {
+        console.log('both functions function')
+        await loopandpush() 
+        return checkAndReturn()
     }
-    if (number === 4 || 0) {
-        return [strains[0], strains[1], strains[2], strains[3]]
-    }
-    if (number === 5 || 0) {
-        return [strains[0], strains[1], strains[2], strains[3], strains[4]]
-    }
-    if (number === 6 || 0) {
-        return [strains[0], strains[1], strains[2], strains[3], strains[4], strains[5]]
-    }
-    if (number === 7 || 0) {
-        return [strains[0], strains[1], strains[2], strains[3], strains[4], strains[5], strains[6]]
-    }
-
+    return bothfunctions()
 }
