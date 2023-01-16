2b13789333c3032eb59080a635d0132d91d77d4f
Frank Collins <fwc3rd@gmail.com>
2023-01-16 14:45:10 -0500

    updated ES6 class to be case sensitive [2:43pm]
        const keyHandler = async (evt) => {
            console.log('firing the keyHandler function!')
            console.log('evt')
            console.log(evt)
            let precode:string = evt.code
            let code:string = evt.code.slice
        let numreturn = await Regex(precode, 'numreturn')
            console.log('numreturn')
            console.log(numreturn)
            // let code:string = evt.code.slice(3)
            console.log(`i just pressed ${code}`)
        }
    
    if not, the keydown string data that gets returned from the event object slips through the escaping of all alphanumeric characters
    (which is unwanted and only the case because of the capitalization)
    
    .replace(/[\/a-z]/g, '')
    there's the regex that capital-alpha characters bypass

--- utility/MasterRegex.js
+++ utility/MasterRegex.js
@@ -21,22 +21,21 @@ export default async function Regex(url, action) {      // already forgot its no
             
             numberexp () {
                 // if (typeof url === 'string') {
-                    let onlynumbers = url.replace(/[/\a-z]/g, '')
+                    let onlynumbers = url.replace(/[/\a-zA-Z]/g, '')
                     return onlynumbers
                 // }
             }
             alphabetexp() {
-                let onlyletters = url.replace(/[^\s/a-zA-Z]+/g, '')       
+                let onlyletters = url.replace(/[^\s/a-zA-Z]+/g, '')                                                              
                 return onlyletters
             }
             whitespace() {
                 let predry = url.replace(/\s/g, '');                
-                // let predry = url.replace(/^\s+|\s+$|\s+(?=\s)/g, "")
+                // let predry = url.replace(/^\s+|\s+$|\s+(?=\s)/g, "")                
                 return predry
             }
             splitstring() {
-                console.log("in the string split method of the regex")
-                let splitit = url.split(', ')
+                let splitit = url.split(', ')                
                 return [ splitit[0], splitit[1] ]                
                 // let splitit:string = url.split(', ')
             }
@@ -53,10 +52,11 @@ export default async function Regex(url, action) {      // already forgot its no
             if (action === 'whiteout') {
                 let cleanWhites = await new Exp(url).whiteout
                 return cleanWhites
+                
             }
             if (action === 'stringsplit') {
-                console.log("were in the if block right here")
-                let getString = new Exp(url).stringsplit         
+                let getString = new Exp(url).stringsplit
+
                 return getString
             }
         }
