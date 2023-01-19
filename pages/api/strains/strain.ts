8018df09344b2e3cc8791d97f73d5fd638fb8e79
Frank Collins <fwc3rd@gmail.com>
2023-01-19 12:08:27 -0500

    backupdata from API in case postgres fails [noon]
    options declared as a logical OR comparison defaulting to the data received from postgres && prisma

--- pages/api/strains/strain.ts
+++ pages/api/strains/strain.ts
@@ -1,12 +1,15 @@
 import Random from 'utility/Randomizer'
 import { PrismaClient } from '@prisma/client';
+import APIcall from 'utility/APIcall'
 
 async function getAllStrain(req:any, res:any) {        
         let prisma = new PrismaClient()
         let { body } = req.body || {fakeKey: 'fakeValue'}
         let allstrains = await prisma.strains.findMany()       
-        let randomStrainToTest = Random(allstrains)                
-         res.json( { getdata: allstrains })        
+        let backupdata = await APIcall('all', null, null)
+        let randomStrainToTest = await Random(allstrains)                
+        let datasafestrains = allstrains || backupdata
+         res.json( { getdata: datasafestrains })        
 }
 
 export default getAllStrain
