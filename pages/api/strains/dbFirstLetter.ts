811e4f217d19e0dac3e856262058e69782efea2e
Frank Collins <fwc3rd@gmail.com>
2023-01-19 17:06:45 -0500

    returns db data [5:06pm]
    just like the API data returning function
    but i want this to be fetching associated data from the join table (User has many strains; Saved Strain can belong to many data.Users)

--- pages/api/strains/dbFirstLetter.ts
+++ pages/api/strains/dbFirstLetter.ts
@@ -1,26 +1,36 @@
-import Random from '../../../utility/Randomizer'
+import Random from 'utility/Randomizer'
 import { PrismaClient } from '@prisma/client';
-import { createNoSubstitutionTemplateLiteral } from 'typescript';
 
 async function dbFirstLetter(req:any, res:any) {        
+
+
         let prisma = new PrismaClient()
-        let firstLetter:string = req.body || {fakeKey: 'fakeValue'}
-        let strainbucket = new Array()
-        console.log('firstLetter in the house')
+        // let firstLetter:string = req.body || {fakeKey: 'fakeValue'}
+        let firstLetter:string = req.body.dataname 
+        // let firstLetter:string = req.body.code || {fakeKey: 'fakeValue'}
+        
+        
+        console.log('req')
+        console.log(req)
+
+        console.log('firstLetter')
         console.log(firstLetter)
+
+        let strainbucket = new Array()
+        // console.log('firstLetter in the house')
+        // console.log(firstLetter)
 
         const pushloop = async () => {
             let allstrains = await prisma.strains.findMany()        // find all strains from
             let randomStrainToTest = Random(allstrains)                
             allstrains.forEach( (strain:any) => {
                 let name:string = strain.strain
-                let firstletter:string = name.charAt(1)
-
-                console.log('strain')
-                console.log(strain)
-
+                let firstletter:string = name.charAt(0)
+                console.log('firstletter name.charAt(1)')
+                console.log(firstletter)
                 if (firstLetter === firstletter) {
-                    console.log(`firstLetter ${firstLetter} firstletter ${firstletter}`)
+                    
+                    console.log(` *  * * * * * * * firstLetter ${firstLetter} firstletter ${firstletter} * * * * * * `)
                     strainbucket.push(strain)
                 }
             })
@@ -29,10 +39,13 @@ async function dbFirstLetter(req:any, res:any) {
             await pushloop()
             return strainbucket
         }
-        let myvalue = await returnvalue()
+        // let myvalue = await returnvalue()
         
-
-        res.json( { getdata: myvalue })        
+        const returnfunction = async () => {
+            let value = await returnvalue()
+             res.json( value)        
+        }
+        returnfunction()
     }
 
 export default dbFirstLetter
