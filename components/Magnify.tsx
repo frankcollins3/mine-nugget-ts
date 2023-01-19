44ae164b9513aa2ef042dfa75c0a6c41d393026c
Frank Collins <fwc3rd@gmail.com>
2023-01-19 16:50:23 -0500

    more of an optimizing testing feature. [4:50pm]
      if (code === 'aleft') return
    to toggle out tabs with [ cmnd, tab ] to bring up the applications, a search result returning all of the strains from the database are returned.
    
    this allows switching out to see the terminal return the server side console.logs() without this redundant search push the intended search data logs up into the terminal

--- components/Magnify.tsx
+++ components/Magnify.tsx
@@ -9,7 +9,11 @@ import $ from 'jquery'
 import {useUrl} from 'Contexts/Url'
 import {useGame} from 'Contexts/game'
 import FirstLetter from 'utility/firstLetterSearch'
+
 import NumberSearch from 'utility/NumberSearch'
+import dbFirstLetter from 'pages/api/strains/dbFirstLetter'
+import GET from 'utility/GETdataJS'
+import POST from 'utility/POSTdataJS'
 
 // let hoverstring:string = magnifyhover.toString()
 
@@ -19,14 +23,15 @@ export default function Magnify (props) {
     let tam = ["tam1", "tam1", "tam1", "tam2", "tam3"];
     let opacity = ["opacity1", "opacity1", "opacity1", "opacity2", "opacity2", "opacity3"];
     let theme:string = props.findMineTheme
+    
 
-    const { allStrain, getSpecifiedStrain, userStrainPost } = useUrl()  //obj destructuring
+    const { allStrain, getSpecifiedStrain, userStrainPost, dbFirstLetter, dbNumber } = useUrl()  //obj destructuring
 
-    const { 
-        gameOn, playing, searchHover, hoverOnSearch, 
-        findMineTheme, toggleTheme, searchChar, searchCharFunc,
-        searchBucket, fillSearchBucket
-        } = useGame()
+const { 
+    gameOn, playing, searchHover, hoverOnSearch, 
+    findMineTheme, toggleTheme, searchChar, searchCharFunc,
+    searchBucket, fillSearchBucket
+    } = useGame()
 
     const [hover, setHover] = useState(false)
     const [reduxBucket, setReduxBucket] = useState([])
@@ -79,41 +84,47 @@ export default function Magnify (props) {
 
     const keyHandler = async (evt) => {        
         let precode:string = evt.code
-        let code:string = evt.code.slice(3).toLowerCase()
-        console.log('code')
-        console.log(code)
+        let code:string = evt.code.slice(3).toLowerCase()        
         // gpmcwd gorilla, pineapple, mimosa, cherry, wid & cake, dosidos
 
         let numreturn = await Regex(precode, 'numreturn')
 
         let regexlength:number = numreturn.length        
         if (regexlength < 1) {
-            console.log('code')            
-            console.log(code)            
+            // let db = await dbFirstLetter('g') // expected 2 arguments but got 1 when you try to invoke as a callback with parameter.
+            // console.log('dbFirstLetter')
+            // console.log(dbFirstLetter)
+            let db = await $.ajax({
+                method: 'GET',
+                url: dbFirstLetter,
+                data: {
+                    code
+                }
+            }).then( (mydata) => {
+                // console.log('mydata')
+                // console.log(mydata)
+            })
+            let searchfor = await POST(dbFirstLetter, 'w')
+            console.log('searchfor')
+            console.log(searchfor)
+            
+            
             if (code === 'g' || code === 'w' || code === 'p' || code === 'm' || code === 'c' || code === 'd') {
                 let searchFor:(string|object|any) = await FirstLetter(code)
-                fillSearchBucket(searchFor)
-                console.log('searchFor')
-                console.log(searchFor)
-                console.log(typeof searchFor)
+                fillSearchBucket(searchFor)                
             } else {
+                if (code === 'aleft') return
                 let allstrains = await APIcall('all', null, null)
-                fillSearchBucket(allstrains)
-                console.log('allstrains')
-                console.log(allstrains)
+                fillSearchBucket(allstrains)                
             }
 
         } 
         else if (regexlength >= 1) {
             if (parseInt(numreturn) <= 6 || parseInt(numreturn) > 6) {                
                 let myStrains = await NumberSearch(numreturn)
-                fillSearchBucket(myStrains)
-                console.log('myStrains')
-                console.log(myStrains)
-            }
-            console.log("there is some regex length this is a number")
-        }
-        console.log(`i just pressed  ${code}`)
+                fillSearchBucket(myStrains)                
+            }            
+        }        
     }
 
     return (
@@ -129,7 +140,6 @@ export default function Magnify (props) {
                 </div>
             </div>
         </div>
-        <button onClick={() => console.log(`well this is the ${searchChar}`)}></button>
     </>       
     )
 }
