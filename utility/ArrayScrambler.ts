5cc478b248606d25785bea4cde9e866c49da197f
Frank Collins <fwc3rd@gmail.com>
2023-01-20 01:00:51 -0500

    [ArrayScrambler] array.filter() [1:00am]

--- utility/ArrayScrambler.ts
+++ utility/ArrayScrambler.ts
@@ -7,11 +7,13 @@ console.log(array)
 let i:number = 0;
 let arrLen:number = array.length
 let indexArray:(string|number|object)[] = []        // need object because push won't work without it.
+let indexLen:number = indexArray.length
 let dontuse = new Array()
-// let indexArray:(string|number|object)[] = []
+let dontuseLen:number = dontuse.length
+
 const populateArray = () => {
-    array.forEach( (indexitem, idx:any) => {
-            indexArray.push(idx)
+    array.forEach( (indexitem, idx:any) => {        
+            indexArray.push(idx)        
     })
 }
 // loop thru array and push the indexes in the array so we have them for reference.
@@ -19,13 +21,32 @@ const populateArray = () => {
 const checkArray = async () => {
 const swapindex = async () => {
     for (i; i < array.length; i++) {
-        let randomIndex = await Random(indexArray)
-        array[randomIndex] = array[i]
-        // kind of crazy that this works. i had to test that: let arr = [1, 2, 3, 4, 5] arr[0] = 0... arr = [0, 2, 3, 4, 5]
+
+        const checkdontuse = (array) => {
+            for (i; i < array.length; i++) {
+                for (i; i < dontuseLen; i++) {
+                    if (array[i] === dontuse[i]) {
+                        continue
+                    } else {
+                        return array[i]
+                    }
+                }
+            }
+        }
+        let randomIndex = indexArray.filter(checkdontuse)        
+
+        console.log('randomIndex')
+        console.log(randomIndex)
+        // let randomIndex = indexArray.filter(async(idx) => !dontuse.includes(idx) ? )        
+        // let randomIndex = await Random(indexArray)        
         
+        // array[randomIndex] = array[i]
+
     }
+    
+    // kind of crazy that this works. i had to test that: let arr = [1, 2, 3, 4, 5] arr[0] = 0... arr = [0, 2, 3, 4, 5]        
 }
-const check = () => {
+const check = () => {    
     return array
 }
 const asyncwaitfunc = async () => {
