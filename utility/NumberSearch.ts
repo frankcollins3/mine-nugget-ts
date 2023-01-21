5bfd18d38b01eb3a5a959ad80bbcdbee4a32c3b4
Frank Collins <fwc3rd@gmail.com>
2023-01-21 11:11:52 -0500

    NumberSearch [11:09am]
    iterates over the scrambled array vs the not-scrambled array.
    now when you press a number after clicking onto the magnifying glass 
    
    (which activates the receptivity of the search input):
    
    when you type a number into the input it returns a random 3 set of strains. 
    i did this so things felt a little less robotic.

--- utility/NumberSearch.ts
+++ utility/NumberSearch.ts
@@ -1,38 +1,25 @@
 import APIcall from 'utility/APIcall'
+import Scrambler from 'utility/ArrayScrambler'
 
 export default async function NumberSearch (num) {
 // export default async function NumberSearch (num:string|number|any) {
     let strains = await APIcall('all', null, null)
+    let scrambled = await Scrambler(strains)
+    console.log('scrambled in NumberSearch')
+    console.log(scrambled)
+
+
     let number;
     let i:number = 0;
     let strainbucket = new Array()    
     if (typeof num === 'string') number = parseInt(num)
     if (num > 6) number = 7
-    // if (number === 1) {
-        
-    //     return strains[0]
-    // }
-    // if (number === 2) {
-    //     return [strains[0], strains[1]]
-    // }
-    // if (number === 3) {
-    //     return [strains[0], strains[1], strains[2]]
-    // }
-    // if (number === 4) {
-    //     return [strains[0], strains[1], strains[2], strains[3]]
-    // }
-    // if (number === 5) {
-    //     return [strains[0], strains[1], strains[2], strains[3], strains[4]]
-    // }
-    // if (number === 6) {
-    //     return [strains[0], strains[1], strains[2], strains[3], strains[4], strains[5]]
-    // }
-    // if (number === 7) {
-    //     return [strains[0], strains[1], strains[2], strains[3], strains[4], strains[5], strains[6]]
-    // }    
+
     const loopandpush = () => {
         while (i < number) {
-            let incrementstrain = strains[i]            
+            let incrementstrain = scrambled[i]      
+            console.log('incrementstrain')     
+            console.log(incrementstrain)     
             strainbucket.push(incrementstrain)
             i++
         }
