461b46927ffdf1d8e25b627e10f22c74fac41f7b
Frank Collins <fwc3rd@gmail.com>
2023-01-22 18:53:08 -0500

    [error] [6:50pm]
    not working on this just had this idea 
    i don't want to have an error page (too late)
    i'd rather have an error component
    
    this <error/> will live in the app.tsx scope
    with: some global state && useContext() 
    any component can toggle parse any error and change the string state to be anything, 
    
    and the <footer> component will render on any component from /app.tsx 
    as i personally made this API .. I'm avoiding any errors being a massively breaking change.
    
    so click on this component, acknowledge the error,
     and move on without redirecting from the user's current /nextAPP/vercel|Aws/mineNugget/approute.tsx location before encountering the error.

---
+++ components/error.tsx
@@ -0,0 +1,11 @@
+export default async function error () {
+    
+    return (
+        <div>
+            <img src="/img/dynamite.png"></img>
+            <h1> You'll Blow up Next Time! For now lets stick to mine</h1>
+            <img src="/img/mine.png"></img>
+            {/* gradient bg? possible acme explosion type of background or that "but wait theres more!" cheesy bannery sales ad */}
+        </div>
+    )
+}
