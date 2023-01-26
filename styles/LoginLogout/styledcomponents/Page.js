7e70d04be0e1d4e2863861aab915a29e800a3bcc
Frank Collins <fwc3rd@gmail.com>
2023-01-26 12:55:57 -0500

    probably the only styled component for this page [12:55pm]

---
+++ styles/LoginLogout/styledcomponents/Page.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Page = styled.div`
+    min-height: 100vh;
+    min-width: 100vw;
+    background-color: rgb(62, 50, 32);
+
+    display: flex; 
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    overflow: hidden;    
+`
+
+export default Page
