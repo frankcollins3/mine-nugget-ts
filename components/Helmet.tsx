305b856fa747881d76f3e2c1a3a6c33261c6a24c
Frank Collins <fwc3rd@gmail.com>
2023-01-26 22:29:21 -0500

    forgot responsive container [10:29pm]

--- components/Helmet.tsx
+++ components/Helmet.tsx
@@ -1,11 +1,20 @@
 import styles from 'styles/findmine/sass/FindMine.module.scss'
+import Container from 'react-bootstrap/Container'
 
 export default function Helmet () {
+    let sassdiv:string = styles.div
+    let helmet:string = styles.helmet
+    let barrier:string = styles.barrier
+
+    // let halfsizediv = [styles.div, ".halfpint"].join(" ")
+
     return (
-        <div className={styles.div}>
-        <img src="/img/helmet.png" className={styles.helmet}></img>
-        <img src="/img/barrier.png" className={styles.barrier}></img>            
-        {/* <p> hey </p> */}
-        </div>            
+                
+        <Container className={styles.div}>
+        <img src="/img/helmet.png" className={helmet} ></img>
+        <img src="/img/barrier.png" className={barrier}></img>            
+        </Container>
+        
+        
     )
 }
