5b3b8fd12fef9c9062c2ee81adb2c40debe8d7f6
Frank Collins <fwc3rd@gmail.com>
2023-01-27 10:43:10 -0500

    check for the box! its a checkbox! [10:43am]
    never used them before this looks nice

--- components/SignupConstraints.tsx
+++ components/SignupConstraints.tsx
@@ -1,17 +1,26 @@
 import styles from 'styles/LoginLogout/LogInOut.module.scss'
+import Container from 'react-bootstrap/Container'
 // possible dictionary API to check for simple syllable words or too easily guessed words?
 // email API to check if its a valid email?
 // possible to make an API and to basically hash that API so that you can't read it but can consist of vulgar words that aren't accepted?
 
-export default async function SignupConstraints() {
-    return (
-        <div>
-            <div> 
-                {/* username constraints: 6-30 characters long */}
-                {/* password constraints: 1 special character, 1 uppercase letter, 7-14 characters */}
-                {/* age constraints: must be atleast 16 years old!  */}
-                {/* valid email constraints: must have an @ in the list. or end in .edu .com etc  */}
-            </div>
-        </div>
-    )
-}
+export default function SignupConstraints(props) {
+        let sty = styles
+
+        return (
+            <Container id={sty.ConstraintGrid}>
+                <div id={sty.ConstraintA}>
+                    <input type="checkbox" className="checkbox" id="UserBox" name="UserBox" value="user"/>
+                    <input type="checkbox" className="checkbox" id="PassBox" name="PassBox" value="password"/>
+                    <input type="checkbox" className="checkbox" id="EmailBox" name="EmailBox" value="email"/>
+                 </div>
+
+                <div id={sty.ConstraintB}>
+                 </div>
+            </Container>
+        )
+    }
+            {/* username constraints: 6-30 characters long */}
+            {/* password constraints: 1 special character, 1 uppercase letter, 7-14 characters */}
+            {/* age constraints: must be atleast 16 years old!  */}
+            {/* valid email constraints: must have an @ in the list. or end in .edu .com etc  */}
