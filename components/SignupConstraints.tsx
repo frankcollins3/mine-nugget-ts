9da32f6cc2a1623fd8521f6c096b0640b83bd7bb
Frank Collins <fwc3rd@gmail.com>
2023-01-27 21:10:41 -0500

    checkbox test && state for cond logic [9:10am]
    quick test to assess checked checkbox <inputs/> and they do.
    
    also created state to restrict 1 clicked checkbox at a time with the ability to toggle between selected checkboxes by unchecking a checked checkbox

--- components/SignupConstraints.tsx
+++ components/SignupConstraints.tsx
@@ -1,22 +1,88 @@
 import styles from 'styles/LoginLogout/LogInOut.module.scss'
 import Container from 'react-bootstrap/Container'
+import {useEffect, useState} from 'react'
+import $ from 'jquery'
 // possible dictionary API to check for simple syllable words or too easily guessed words?
 // email API to check if its a valid email?
 // possible to make an API and to basically hash that API so that you can't read it but can consist of vulgar words that aren't accepted?
 
 export default function SignupConstraints(props) {
+
+        const [checked, setChecked] = useState('')
+
         let sty = styles
+
+        useEffect( () => {
+            // console.log("heres my inputs")
+            // console.log($('input'))
+
+            // $('input')                
+        })
+
+        const inputClick = (event) => {
+            let target = $(event.target)
+            console.log('event')
+            console.log(event)
+            // setChecked('checked')
+        }
+
+        const btnclick = () => {
+            console.log('cb1 - cb3 and checked')
+            console.log( $('#cb1'))           
+            console.log( $('#cb2'))           
+            console.log( $('#cb3'))           
+            setChecked('checked')
+        }
+        
+        const btnclick2 = () => { 
+            console.log('* * * * * cb1 - cb3 * * * * *  ')
+            console.log( $('#cb1'))           
+            console.log( $('#cb2'))           
+            console.log( $('#cb3'))           
+            setChecked('')
+        }
+
+        const btnclick3 = () => {
+            
+
+            let cb1:any = $('#cb1')
+            let cb2:any = $('#cb2')
+            let cb3:any = $('#cb3')
+            
+            console.log('cb1')
+            console.log(cb1)
+            console.log(cb1[0].checked)
+            console.log('cb2')
+            console.log(cb2)
+            console.log(cb2[0].checked)
+            console.log('cb3')
+            console.log(cb3)
+            console.log(cb3[0].checked)
+        }
 
         return (
             <Container id={sty.ConstraintGrid}>
-                <div id={sty.ConstraintA}>
-                    <input type="checkbox" className="checkbox" id="UserBox" name="UserBox" value="user"/>
-                    <input type="checkbox" className="checkbox" id="PassBox" name="PassBox" value="password"/>
-                    <input type="checkbox" className="checkbox" id="EmailBox" name="EmailBox" value="email"/>
-                 </div>
 
+                <div id={sty.ConstraintA} className="DivParent Row">
+
+            <div className={sty.checkboxcontainer}>            
+                <input  type="checkbox" id="cb1"/>
+                <label htmlFor="cb1">username</label>
+            </div>
+            <div className={sty.checkboxcontainer}>
+                <input type="checkbox" id="cb2"/>
+                <label htmlFor="cb2">password</label>
+            </div>
+            <div className={sty.checkboxcontainer}>
+                <input type="checkbox" id="cb3"/>
+                <label htmlFor="cb3">email</label>
+            </div>
+                 </div>
                 <div id={sty.ConstraintB}>
                  </div>
+                 <button style={{ backgroundColor: 'blue', width: '80px'}} onClick={btnclick}></button>
+                 <button style={{ backgroundColor: 'red', width: '80px'}} onClick={btnclick2}></button>
+                 <button style={{ backgroundColor: 'yellow', width: '80px'}} onClick={btnclick3}></button>
             </Container>
         )
     }
