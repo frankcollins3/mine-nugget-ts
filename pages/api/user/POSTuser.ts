80caf7d5559d437ae52c05c77922b0602f6fc282
Frank Collins <fwc3rd@gmail.com>
2023-02-04 23:09:33 -0500

    salt the password. [10:30ish]

--- pages/api/user/POSTuser.ts
+++ pages/api/user/POSTuser.ts
@@ -1,15 +1,25 @@
 import { Prisma, PrismaClient } from '@prisma/client';
 let prisma = new PrismaClient()
 
+import bcrypt from 'bcryptjs'
+console.log('bcrypt')
+console.log(bcrypt)
+    
+
 export default async function POSTroute (req:any, res:any) {
 
     let endpointbody = req.body.data
 
+    const salt = bcrypt.genSaltSync(10)
+
     let username = endpointbody.username
     // let username = req.body.data.username
     let password = endpointbody.password
+    const hashPassword = bcrypt.hashSync(password, '$2a$10$CwTycUXWue0Thq9StjUM0u')
     let email = endpointbody.email
     let age = req.body.data.dataage / 1
+
+
 
     // let numberbucket = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
     // let stringbucket = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30']
@@ -25,10 +35,13 @@ export default async function POSTroute (req:any, res:any) {
     //     })
     // }
 
+
+
         const createuser = await prisma.users.create({
             data: {
                 username: username,
-                password: password,
+                password: hashPassword,
+                // password: password,
                 email: email,
                 age: age
             }
