bbaf6e1e716c7f6199b651fbdf70a4d0fc02279b
Frank Collins <fwc3rd@gmail.com>
2023-02-05 21:29:34 -0500

    fix issue #89 ) usersId has to belong to existing data [9:29pm]

--- pages/api/strains/userstrainpost.ts
+++ pages/api/strains/userstrainpost.ts
@@ -2,10 +2,14 @@ import APIcall from 'utility/APIcall'
 
 import path from 'path';
 import { promises as fs } from 'fs';
+import { PrismaClient } from '@prisma/client';
 let strainJSON = 'utility/strainJSON.json'
+
+// * might grab the user from the URL too to also confirm!
 
 export default async function (req, res) {
 
+    let prisma = new PrismaClient()
     let body = req.body
     let usersId = body.usersId
     let strainName = body.strain
@@ -14,23 +18,31 @@ export default async function (req, res) {
     console.log(req.body)
     console.log('usersId')
     console.log(usersId)
-    
-    let apidata = await APIcall('all', null, null) 
-    const fileContents = await JSON.parse(await fs.readFile('utility/strainJSON.json', 'utf8'))
-    // const fileContents = await fs.readFile(jsonDirectory + 'utility/strainJSON.json', 'utf8');
 
+    let apidata = await APIcall('all', null, null) // backup.
+    const fileContents = await JSON.parse(await fs.readFile('utility/strainJSON.json', 'utf8'))
     let strainsFS = fileContents.strains
 
+    let nonDBstrains = apidata || strainsFS
+
     
-    await strainsFS.forEach( (strain:any) => {
-        if (strain.strain === strainName) {           
-            strainsId = strain.strainId
-
-        }
-        // if (strain.strain === strainName)  strainsId = strain.id        
-    })
 
 
-    res.json( { response: req.body} )
+    let strainsDB = await prisma.strains.findMany()
+    strainsDB.forEach(async (dbstrain) => {
+        console.log('dbstrain')
+        console.log(dbstrain)
+        if (dbstrain.strain === strainName) {
+            const strainId:number = dbstrain.id
+            console.log("condition is met!")
 
-}
+            const newUserStrains = await prisma.usersOnStrains.create({
+                data: {
+                  usersId: 47,
+                  strainsId: strainId,
+                },
+              }).then( (newUserStrain) => {
+                console.log(newUserStrain)
+                res.json( { response: req.body, strainsId: strainId, name: strainName  } )
+            })
+        }})}
