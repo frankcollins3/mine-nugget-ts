8ecc9c340285cac100d31966a785e745b80e1754
Frank Collins <fwc3rd@gmail.com>
2023-02-06 20:31:16 -0500

    wrong before was data.id->data.name now id -> id [8:30pm]

--- pages/api/strains/getIDwithNAME.ts
+++ pages/api/strains/getIDwithNAME.ts
@@ -3,10 +3,11 @@ import { PrismaClient } from '@prisma/client';
 export default async function getIDwithNAME(req, res) {
     let Prisma = new PrismaClient()
     let body:(object|any) = req.body 
-    let name = body.name
+    let name = body.name 
     let strains:any = await Prisma.strains.findMany()
-    let idfilterstrain = await strains.filter(strain => strain.id === name ? strain.id : 'no match' )    
-    let strainId = idfilterstrain[0]
-    
+    let idfilterstrain = await strains.filter(strain => strain.strain === name  )        
+    let strain = idfilterstrain[0]
+    let strainId = strain.id
+        
     res.json( { strain: req.body, id: strainId})
 }
