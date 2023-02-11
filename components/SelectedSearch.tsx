220b01553e9c600303e861ebac74b52c62d3e214
Frank Collins <fwc3rd@gmail.com>
2023-02-11 14:41:59 -0500

    see other user data 1 at a time [2:40pm]
    click the username to reveal a:
    mine icon && shovel icon
    
    if you click mines it will append the review of the strain correspondent to the clicked-upon username.
    
    Before you could click multiple usernames and they would keep popping up mines and shovel images.
    But you could only search and look into 1 review and 1 user at a time, 
    
    but the images seem to indicate that the user can click onto those elements.
    
    Restrict that behavior, showing its a 1 by 1 process.

--- components/SelectedSearch.tsx
+++ components/SelectedSearch.tsx
@@ -1,5 +1,6 @@
 import Axios from 'axios';
 import {useGame} from 'Contexts/game'
+import {useUrl} from 'Contexts/Url'
 import styles from 'styles/findmine/sass/FindMine.module.scss'
 import {useEffect, useState} from 'react'
 import Random from 'utility/Randomizer'
@@ -9,6 +10,11 @@ import CSS from 'utility/CSStool'
 import AllMine from 'components/AllMineBtnStrip'
 import userStrainsForUsersId from 'pages/api/strains/userStrainsForUsersId'
 import mineshovelES6 from 'utility/mineshovelES6'
+import GETuserstrains from 'utility/GETuserstrains'
+import POSTuserstrainsES6 from 'utility/POSTuserstrainsES6'
+
+import Children from 'utility/jqChildren'
+import Siblings from 'utility/JqSiblings'
 
 export default function SelectedSearch(props) {
     const [uiHover, setUiHover] = useState('')
@@ -16,51 +22,42 @@ export default function SelectedSearch(props) {
 
     let getIDurl = props.getIDurl
     let postMINEurl = props.postMINEurl
+    let getALLminesURL = props.getALLminesURL
+    const ALLusersGET = props.ALLusersGET
+    const userStrainPostUrl = props.userStrainPostUrl
     
-
     let allOrMine = styles.allOrMine
     let h1 = styles.h1
     let soClassy:any = [allOrMine, h1].join()
-    let findMineMyStrains = props.findMineMyStrains    
+    let findMineMyStrains = props.findMineMyStrains 
 
+    // const { userStrainPost } = useUrl()
+    
        const { 
         selectedSearch, searchSelector, searchStrainId, searchstrainidset,
         searchType, usersOfSearchStrain, usersofsearchstrainset, 
         currentUser, currentUserName, currentusernameset, currentuseridset, currentUserId, userstrainset, userStrains, 
         allMyStrains, allmystrainset,
-        MineShovelUser, mineshoveluserset, noMineShovel, nomineshovelset,
+        MineShovelUser, mineshoveluserset, MineShovel, mineshovelset, otherUserMineClick, otherusermineclickset,
         searchMinePost, searchminepostset, searchMineRead, searchminereadpost, 
-        myMineReview, myminereviewset, myMineTitle, myminetitleset,
-        
+    myMineReview, myminereviewset, myMineTitle, myminetitleset, allMinesReviews, 
         } = useGame()
+
+    const [saveStrain, setSaveStrain] = useState(selectedSearch)
 
     useEffect( () => {
         console.log('usersOfSearchStrain in selected search component!')
         console.log(usersOfSearchStrain)
     }, [usersOfSearchStrain])   
 
-    useEffect( () => {
-        console.log('selectedSearch in the selectedSearch component')
-        console.log(selectedSearch)
-
+    useEffect( () => {     
+        console.log('allMyStrains from the useEffect from SelectedSearch')   
+        console.log(allMyStrains)   
         const getId = async () => {
-
-            Axios.post(getIDurl, {
-                name: selectedSearch
-            }).then( (id) => {
-                // let returnid:number = id
-                return id        
-            })
-        }
-            // searchstrainidset(1)
-        // let searchID = getId()
-        // setSearchId(searchID)
-            
-
+            Axios.post(getIDurl, { name: selectedSearch
+            }).then( (id) => { return id })}            
     }, [])
 
-
-    
     const quicktest = () => {
         console.log("hey from quicktest!")
     }
@@ -76,52 +73,123 @@ export default function SelectedSearch(props) {
     const shovel = () => {
         console.log('shovel click')
     }
+    let ValueSavingMap = new Map()
+    ValueSavingMap.set('selectedSearch', selectedSearch)
 
-    const usernameClick = (event) => {
-        console.log(event)
-        console.log('event.target')
-        console.log(event.target)
-    let text = event.target.outerText
+    const usernameClick = async (event) => {        
+        let target = $(event.target)
+        let text = event.target.outerText
+        let eventSibs:any = await Siblings(target)
+        let mineimg = eventSibs![0]
+        let shovelimg = eventSibs![1]        
+        let mineimgopacity = mineimg.style.opacity
+        let shovelimgopacity = shovelimg.style.opacity
+        if (mineimgopacity === '0') await CSS(eventSibs, 'opacity', '1.0')
+        if (mineimgopacity === '1') await CSS(eventSibs, 'opacity', '0.0')
         mineshoveluserset(text)
-        console.log('text')
-        console.log(text)
     }
 
-    const onChange = () => {
-        console.log('we are changing the input')
-    }
+    const onChange = () => { console.log('we are changing the input') }
 
     const fakesubmit = async () => {        
-        // let input = $('.shovelMineInput')
         let titleinput:any = document.querySelector('#TitleInput')
         let reviewinput:any = document.querySelector('#ReviewInput')
-
         let valuetitle = titleinput.value
         let titleWithUserId = `${currentUserId}/${valuetitle}`
-        let valuereview = reviewinput.value
-
-        console.log(`titlevalue: ${valuetitle} reviewvalue: ${valuereview}`)
+        let valuereview = reviewinput.value        
         let postobjectDATA = { strainId: searchStrainId, review: valuereview, title: titleWithUserId, usersId: currentUserId };
+
+        console.log('postobjectDATA')
+        console.log(postobjectDATA)
+
         let newminePOST = await mineshovelES6(postMINEurl, postobjectDATA, 'minePOST') 
-        searchMinePost === true ? searchminepostset() : ''
-        console.log('newminePOST')
-        console.log(newminePOST)        
+        searchMinePost === true ? searchminepostset() : ''        
     }
-    
+
+    const mineclick = async (event) => {
+        setSaveStrain(selectedSearch)
+        let target = $(event.target)
+        let siblings = await Siblings(target)
+        let ptagsibling:any = siblings![0]
+        let usertext = ptagsibling.outerText
+
+        let preusers:any = await GETuserstrains(ALLusersGET, 'onlyusers')
+        let ALLusers = preusers!.data.users
+
+        let premines = await mineshovelES6(getALLminesURL, '', 'GETallmines')
+        let ALLmines = premines?.data.allmines
+        let filteredUser = ALLusers.filter( user => user.username === usertext)[0]
+        let clickedUserId = filteredUser.id
+
+        ALLmines.forEach( (mine) => {   
+            let looptitle:string = mine!.title
+        let userId = looptitle.slice(0, looptitle.indexOf('/'))
+            if (userId.toString() === clickedUserId.toString()) {
+                console.log("we are in this condition")
+                if (mine.strainId === searchStrainId) {
+                    console.log('mine.review')
+                    console.log(mine.review)
+                    let reviewmatch = mine.review
+                    console.log('reviewmatch')
+                    console.log(reviewmatch)
+                    // let originalText = selectedSearch
+                    let originalValue = ValueSavingMap.get('selectedSearch')                    
+                    if (MineShovel === reviewmatch) {
+                        if (otherUserMineClick === true) {
+                            mineshovelset('')
+                        otherusermineclickset()
+                        }
+                    } else if (MineShovel !== reviewmatch) {
+                        // otherUserMineClick === false ? mineshovelset(reviewmatch) : ''
+                        if (otherUserMineClick === false)  {
+                            mineshovelset(reviewmatch)
+                            otherusermineclickset()
+                        }
+                    }                    
+                }
+            }
+        })
+    }
+
+    const wannasave = async (event) => {
+        let target = $(event.target)
+        let targetsiblings = await Siblings($(event.target))
+        let sibling = targetsiblings![0]
+        CSS(sibling, 'opacity', '1.0')
+
+        let allusers = await GETuserstrains(ALLusersGET, 'onlyusers')
+        let users = allusers?.data.users
+
+        let filterusers = users.filter( user => user.id.toString() === currentUserId)[0]        
+        let currentuserid = filterusers.id
+        
+        if (currentuserid.toString() === currentUserId) {
+            let UserStrainMap = new Map()
+            UserStrainMap.set('usersId', currentuserid)
+            await UserStrainMap.set('strain', selectedSearch)
+            await UserStrainMap.set('strainsId', 0)
+            let newUserStrain = await POSTuserstrainsES6(userStrainPostUrl, UserStrainMap)                        
+            mineshovelset('Dig For Gold')
+        } else {
+            return 
+        }        
+        let UserSaveMap = new Map()
+        let stringid = window.localStorage.getItem('currentUserId')
+
+         setTimeout( () => {
+            searchSelector('')
+         }, 1000)                
+    }
+
+    const nothing = () => {return}
     
 
     return (
         <>        
         <div         
         id={styles.SelectedSearchBox} className="Column">
-        <div
-        style = {{
-            // backgroundImage: `url('/img/cone.png')`, backgroundSize: '10%', backgroundRepeat: 'no-repeat',
-        }}
-        >
-
+        <div>
                 <div className="Row">
-
                 {
                     searchType === 'All'                     
                     ?
@@ -129,27 +197,29 @@ export default function SelectedSearch(props) {
                      ?
                       usersOfSearchStrain.map( (mapitem, idx) => {
                         return (
-                            <div key={`div${idx}`} className="Row">
-                        <p onClick={usernameClick} key={idx} 
-style={{ color: mapitem === currentUserName ? 'rgb(247, 208, 32)' : 'papayawhip', margin: '0 2em', cursor: mapitem === currentUserName ? 'not-allowed' : 'pointer' }}> 
-        {mapitem === currentUserName ? 'me' : mapitem} </p>                                                                                    
-                            </div>                                
+                            <div key={`column${idx}`} className="Column">                       
+                                <img  id="mineimg" onClick={mineclick} style={{ height: '40px', width: '40px', opacity: '0.0', cursor: 'pointer'}} src="/img/mine.png"/>                                                        
+                             <p  onClick={otherUserMineClick === false ? usernameClick : nothing } key={idx} 
+style={{ color: mapitem === currentUserName ? 'rgb(247, 208, 32)' : 'papayawhip', margin: '0 2em', cursor: mapitem === currentUserName ? 'not-allowed' : 'pointer',
+         pointerEvents: mapitem === currentUserName ? 'none' : 'all' }}> {mapitem === currentUserName ? 'me' : mapitem} </p>   
+                            <img style={{ height: '40px', width: '40px', opacity: '0.0', cursor: 'pointer' }} src="/img/shovel.png"/>                        
+                            </div>                                                                                                           
                             )
                     })
                         :
                         <div> </div>
                         :
-                        <pre></pre>
-                        // <pre style={ {color: 'papayawhip'}}>. . .</pre>
-                        
+                        <pre></pre>                        
                     }
                 </div>       
                 
                 {
                     searchType === 'Mine' 
                     ?
-                    allMyStrains.map( (mystrain, idx) => {  
-                            if (parseInt(mystrain) === searchStrainId) {                                    
+                    allMyStrains.map( (mystrain, idx) => {   
+                        let straincount = 0;                       
+                                if (parseInt(mystrain) === searchStrainId) {            
+                                    straincount ++                        
                                 return (
                                     <div 
                                     key={`${idx}div`}                                    
@@ -157,25 +227,27 @@ style={{ color: mapitem === currentUserName ? 'rgb(247, 208, 32)' : 'papayawhip'
                                     >                                                                                             
                                     <img style={{ height: '4em', width: '4em' }} src="/img/gold.png"/>                                    
                                     </div>
-                                )
-                            }                            
-                    })                                
+                                )                           
+                             }                    
+                              if (straincount === 0) {
+                                <p> straincount</p>
+                              }           
+                    })                                                    
                     :
                     <pre></pre>
                 }          
-
                     {searchType === 'Mine'
                         ?
                         <div className="Column">
                         {searchMineRead === true 
                             ?
                             <>
-                            <p className="myMineText"  style={{ marginTop: '0.4em' }}> <span className="titlespan"> {myMineTitle}  </span> : {myMineReview}  </p>
+                            <p className="myMineText"  style={{ marginTop: '0.4em', display: 'flex', flexDirection: 'column' }}> 
+                            <span className="titlespan"> {myMineTitle}  </span>  {myMineReview}  </p>
                             </>
                             :
                         <pre></pre>
-                        }
-                        
+                        }                        
                         {searchMinePost === true 
                             ?
                         <form action="/" className="Column">
@@ -188,12 +260,11 @@ style={{ color: mapitem === currentUserName ? 'rgb(247, 208, 32)' : 'papayawhip'
                             :
                         <pre></pre>
                         }
-
                         </div>                            
                         :
                         <pre></pre>
-                    }
-                <h2 style={{ color: 'rgb(247, 208, 36)', boxShadow: '2px 10px 2px rgb(247, 208, 36)'}}> {noMineShovel} </h2>                
+                    }                            
+                <h2 onClick={wannasave} style={{ color: 'rgb(247, 208, 36)', boxShadow: '2px 10px 2px rgb(247, 208, 36)', cursor: 'pointer'}}> {MineShovel} </h2>                                                           
              </div>                                       
         </div>
         </>
