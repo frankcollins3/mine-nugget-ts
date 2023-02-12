02f093919e5057cf8bf826c40318cba12090cdf0
Frank Collins <fwc3rd@gmail.com>
2023-02-11 21:59:46 -0500

    fix few login/user.forEach pass-as-props URL context issues[10pm]

--- pages/strain.tsx
+++ pages/strain.tsx
@@ -20,9 +20,24 @@ import Axios from 'axios';
 let relativepath = `/api/getAllStrain.ts`
 import styled from 'styled-components'
 import Display from 'styles/StrainDisplay'
+import {useGame} from 'Contexts/game'
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
 
                     
 export default  function Strain ( props:any, context ) {       
+
+  console.log('props from strain page')
+        console.log(props)
+        console.log('props.appCurrentUser')
+        console.log(props.appCurrentUser)
+        console.log('props.appCurrentUserName')
+        console.log(props.appCurrentUserName)
+        let appCurrentUser = props.appCurrentUser
+        let appCurrentUserName = props.appCurrentUserName
+
+
     let explicitprops = props
 
     let localhost = props.localhost
@@ -35,7 +50,33 @@ export default  function Strain ( props:any, context ) {
     const [textState, setTextState] = useState('')
     const [displayText, setDisplayText] = useState('')
 
+    const {currentUser, currentUserName, currentuseridset, currentUserId, currentusernameset, noUser, nouserset, userStrains, userstrainset } = useGame()
+
+    useEffect( () => {
+      console.log('useEffect.. currentUser from from strain')
+      const checkuser = async () => {
+        let currentusername =   window.localStorage.getItem('currentUserName')
+        let currentuserid =  window.localStorage.getItem('currentUserId')
+        let prestrains = window.localStorage.getItem('myStrains')          
+        let splitId = prestrains?.split(',')        
+        console.log('splitId')
+        console.log(splitId)
+        let filteredId = await splitId?.filter( strainsId => strainsId !== ',' && strainsId.length === 1)      
+        console.log('filteredId these are my strains!')                                    
+        console.log(filteredId)                                    
+        currentusernameset(currentusername)
+        currentuseridset(currentuserid)    
+        userstrainset(filteredId)    
+        
+      }
+      checkuser()
+  }, [currentUser])
+
+
     const globalstrain = props.globalstate
+    let userId = currentUser ? currentUser.id : ''
+    console.log('userId from over here!')
+    console.log(userId)
 
     const classList:string = [styles.Page, 'Column'].join(" ")
     const textClasses:string = [styles.FontSizeTest, styles.BorderTest].join(" ");
@@ -51,13 +92,79 @@ export default  function Strain ( props:any, context ) {
     } // no 1liner?
     returnUrl()
 
+    const usercheck = async () => {
+      console.log('appCurrentUser')
+      console.log(appCurrentUser)
+      console.log('appCurrentUserName')
+      console.log(appCurrentUserName)
+      
+      console.log('userStrain')
+      console.log(userStrains)
+    }
+
+    const LOGOUT = () => {
+      // window.localStorage.setItem('currentUserId', '')
+      // window.localStorage.setItem('currentUserName', '')
+      window.localStorage.setItem('currentUserName', '')            
+      window.localStorage.setItem('currentUserId', '')            
+      currentusernameset('')
+      currentuseridset('')
+    }
+
+    const noUserClickMine = (event) => {
+      
+
+      $(event.target)
+      .animate({
+        opacity: '0.8'  
+      }, 500)
+      .animate({
+        opacity: '0.3'
+      }, 500)
+      .animate({
+        opacity: '0.3'
+      }, 500, () => {
+        setTimeout( () => {
+          location.href = '/LoginLogout'          
+          nouserset(false);
+          $('*').css('cursor', 'pointer' )
+        }, 1000)
+
+      })
+
+      setTimeout( () => {
+        $('*')        
+        .animate({
+          opacity: '0.8'  
+        }, 500)
+        .animate({
+          opacity: '0.3'
+        }, 500)
+        .animate({
+          opacity: '0.3'
+        })
+      }, 2000)
+
+    }
+
     return (
+
+      <>
+        
         
 
           <Container 
           style={ { minWidth: '100%'}}
           className={classList}>
 
+            
+            { noUser === true ? 
+            <div className="Row">
+              <p id="noUserText" > You Must Be Mine To Dig For Gold. </p>
+            </div>
+            :
+            <pre></pre>
+          }
             <AllStrainContainer   
                 let global={explicitprops}
                 strainSave={props.strainSave} setStrainSave={props.setStrainSave}
@@ -71,6 +178,22 @@ export default  function Strain ( props:any, context ) {
                 allStrains={props.allStrains} setAllStrains={props.setAllStrains}
                 currentStrain={props.currentStrain} setCurrentStrain={props.setCurrentStrain}                            
                 />
+            {
+              noUser === true ?
+              <div style={{
+                height: '10em',
+                width: '10em',
+                display: 'flex', justifyContent: 'center', alignItems: 'center', 
+                flexDirection: 'row'
+              }} 
+              >
+                <img 
+                onClick={noUserClickMine}
+                style={{ transform: 'scale(0.10)', boxShadow: '18px 50px 10px papayawhip', borderRadius: '50%', padding: '0', margin: '0' }}  src="/img/mine.png"/>              
+            </div>            
+              : 
+              <div></div>
+            }
 
               <div className={styles.Rows}>
               {props.strainSave === false 
@@ -96,21 +219,22 @@ export default  function Strain ( props:any, context ) {
               :
               ''
             }
-                </div>
-                
-             {/* {props.strainSave === false 
+                </div>                
+             {props.strainSave === false 
                 ? 
                 ''
-                : */}
+              :
               <PickMines
               localhost={localhost}
               url={returnUrl}
               global={explicitprops}
-              let contextprops={context}
+              contextprops={context}
               />
-             {/* } */}
-                 
+              }
+                 <button style={{ backgroundColor: 'red', marginTop: '2em' }} onClick={LOGOUT}></button> 
+                <button onClick={usercheck}></button> 
           </Container>
+          </>
             
               
     )
