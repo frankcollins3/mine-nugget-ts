e78d4952c363950f037787f66a69a732ff22e6c7
Frank Collins <fwc3rd@gmail.com>
2023-02-11 20:58:05 -0500

    minecart login button -> if user? no mine cart. [8:57pm]
    thought i committed
    
    no ternary rendering with window.localStorage ----> useEffect, check local storage, if user? return. 
    
    no user? change state that invalidates the ternary rendering condition that allows the minecart to stay rendered

---
+++ components/NavBar.tsx
@@ -0,0 +1,56 @@
+import styles from 'styles/NavBar.module.scss'
+import Link from 'next/link'
+import {useGame} from 'Contexts/game'
+import Helmet from 'components/Helmet'
+import {useState, useEffect} from 'react'
+
+export default function NavBar () {
+
+    const [isUser, setIsUser] = useState(false)
+
+    useEffect( () => {
+        let currentUsername = window.localStorage.getItem('currentUserName')
+        console.log('currentUsername')
+        console.log(currentUsername)
+        currentUsername ? setIsUser(true) : setIsUser(false)
+    }, [])
+
+    const {   currentuserset, currentUser, currentUserName, currentusernameset, currentuseridset,  currentUserId   } = useGame()
+
+    const logout = () => {
+        currentuserset([])  
+        currentusernameset('')
+        currentuseridset(0)
+        window.localStorage.setItem('currentUserId', '')
+        window.localStorage.setItem('currentUserName', '')
+        window.localStorage.setItem('myStrains', '')
+        setIsUser(false)
+    }
+
+    return (
+        
+        <div id={styles.NavContainer}>            
+
+    
+            <Link href="/strain">
+            <div style={{ backgroundImage: 'url(/img/gold.png)'}} className={styles.NavBtn} id="StrainLink"></div>
+            </Link> 
+
+            <Link href="/findmine">
+            <div style={{ backgroundImage: 'url(/img/magnify.png)'}} className={styles.NavBtn} id="FamTreeLink"></div>        
+            </Link>
+
+        <Link href="/famtree">
+            <div style={{ backgroundImage: 'url(/img/ring.png)'}} className={styles.NavBtn} id="FindMineLink"></div>                    
+            </Link>
+
+            <img
+            onClick={logout}
+            style={{ display: isUser  ? 'block' : 'none'}}
+            // style={{ display: window.localStorage.getItem('currentUserId') ? 'block' : 'none'}}
+            className={styles.cart} src="/img/cart.png"></img>
+             {/* this will be the logout button */}
+
+        </div>        
+    )
+}
