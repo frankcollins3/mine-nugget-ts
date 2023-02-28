b812b13593d731cd17f155eb60697a734968a3e7
Frank Collins <fwc3rd@gmail.com>
2023-02-28 11:33:03 -0500

    forgot to commit this styled components file [11:33am]

---
+++ styles/findmine/components/SearchDisplay.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+
+
+const Searchdisplay = styled.div`
+    min-height: 30vh;
+    min-width: 30vw;
+    background-color: rgb(62, 50, 32);     
+                                    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    overflow: hidden;
+`
+export default Searchdisplay
