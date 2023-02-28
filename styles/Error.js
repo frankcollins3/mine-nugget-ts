22385cb62d7652d67a35da272d4f054533fe08bc
Frank Collins <fwc3rd@gmail.com>
2023-02-28 15:19:25 -0500

    Add files via upload

---
+++ styles/Error.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+
+const ErrorCont = styled.div`
+    min-height: 100vh;
+    min-width: 100vw;
+    background-color: rgb(62, 50, 32);    
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    `
+export default ErrorCont
+
+// const Img = styled.Img`
+// `
+
+
+
