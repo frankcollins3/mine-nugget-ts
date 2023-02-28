92ba59801f184e2af089b6a62cd74b1b59331cc8
Frank Collins <fwc3rd@gmail.com>
2023-02-28 15:41:23 -0500

    null || empty {}

--- utility/LoopAndPush.ts
+++ utility/LoopAndPush.ts
@@ -1,14 +1,16 @@
-async function LoopAndPush (LoopArray:(object|string|number), ArrOrState:(string|object|number), comparison1, comparison2,  setState:(any|null)) {
-        let Array = ArrOrState;
+// async function LoopAndPush (LoopArray:(object|string|number), ArrOrState:(string|object|number), comparison1, comparison2,  setState:(any|null)) {
+//         let Array = ArrOrState;
         
-        LoopArray.forEach( (loopitem:(string|object|number)) => {            
-            if (comparison1 == comparison2) {                
-                ArrOrState.push(loopitem)                
-            } else {
-                return                
-            }
-            return ArrOrState // kind of surprised but kind of not that you cant just return function param
-        })
-}
+//         LoopArray.forEach( (loopitem:(string|object|number)) => {            
+//             if (comparison1 == comparison2) {                
+//                 ArrOrState.push(loopitem)                
+//             } else {
+//                 return                
+//             }
+//             return ArrOrState 
+//             kind of surprised but kind of not that you cant just return function param
+//         })
+// }
 
-export default LoopAndPush
+// export default LoopAndPush
+export {}
