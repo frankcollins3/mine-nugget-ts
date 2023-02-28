f3f6724fbe8cfb5639bf3e7175e4d2d55c03cc33
Frank Collins <fwc3rd@gmail.com>
2023-02-27 21:52:18 -0500

    Update Helmet.tsx

--- components/Helmet.tsx
+++ components/Helmet.tsx
@@ -8,30 +8,16 @@ export default function Helmet () {
     let helmet:string = styles.helmet
     let barrier:string = styles.barrier
 
-    const {url, urlSetter} = useGame()
-    // can do a regex to remove the slash if you want to clean things up. no need. can do strict equality with copy pasted string from dev tools
+    const {url, urlSetter, constraintshow, constraintshowset} = useGame()    
 
-    useEffect( () => {
-        console.log("useEffect is being triggered by clicking the button from LoginLogout.tsx")
-        console.log('url useEffect')
-        console.log(url)
+    useEffect( () => {        
     }, [url])
 
-    // let halfsizediv = [styles.div, ".halfpint"].join(" ")
-
-    console.log('url from Helmet!')
-    console.log(url)
-
-    
-    
-    return (
-                
+    return (            
         <Container 
-        style={{ border: url === '/LoginLogout' ? '9px solid moccasin' : '9px solid rgba(255, 166, 0, 0.85)' }}
-        // style={{ width: url === '/LoginLogout' ? '500px'  '200px' }}
         className={styles.div}>
         <img src="/img/helmet.png" className={helmet} ></img>
-        <img src="/img/barrier.png" className={barrier}></img>            
-        </Container>            
+        <img src="/img/barrier.png" className={barrier}></img>                    
+        </Container>                        
     )
 }
