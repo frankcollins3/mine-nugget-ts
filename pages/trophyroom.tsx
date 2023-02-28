ef60827749de195e8c5d34b34d260f70531cf6f5
Frank Collins <fwc3rd@gmail.com>
2023-02-28 15:27:08 -0500

    Update trophyroom.tsx

--- pages/trophyroom.tsx
+++ pages/trophyroom.tsx
@@ -4,7 +4,7 @@ function TrophyRoom () {
             <h1> This page will have pictures of highlights of both ejs and next.js mine nugget </h1>
             <p> it will feature a [ movie thatre / velvet-rope-post ] feature film theme  </p>
             <p> double takes inspiration from tony hawks pro skater game "SECRET SKATE TAPE" type highlight reels </p>
-            <footer> img.src="trophy.png"> trophy earned across app AFTER 3 WIN streak on family tree </footer>
+            <footer> img.src="trophy.png" trophy earned across app AFTER 3 WIN streak on family tree </footer>
         </div>
     )
 }
