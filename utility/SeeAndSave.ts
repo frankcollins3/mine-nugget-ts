7e970e3298ac572659bdbb74cf857dcda20225cd
Frank Collins <fwc3rd@gmail.com>
2023-02-27 22:44:43 -0500

    cleaning up utility functions for deployment

--- utility/SeeAndSave.ts
+++ utility/SeeAndSave.ts
@@ -1,26 +1,20 @@
 import Strain from "pages/strain"
-import { propTypes } from "react-bootstrap/esm/Image";
 let i = 0; 
-export default async function SeeAndSave(api, apilength, textState:string, setTextState:any ) {
-
-    console.log('api and length')
-    console.log(api)
-    console.log(api.length)
-    // element to change, the string-data-textState that will be the display text, setTextState which will setState for textState()    
+export default async function SeeAndSave(api, apilength, textState:string, setTextState:any ) {    
     if (api.includes('strain') && api.includes('dominant') && api.includes ('taste')) {
         try {            
             let myfilteredData = api.filter(data => {        
                 if (data !== 'parents') {
                     return data                    
                 }
-            })                
+            })                            
             let datalength = myfilteredData.length                                  
                 if (textState.length < 2) {                    
                     await setTextState(myfilteredData[0])
                 } 
                 else if (textState.length > 2) {
                     for (let i = 0; i < myfilteredData.length; i++) {                        
-                        if (myfilteredData[i] === textState) {
+                        if (myfilteredData[i] === textState) {                            
                             let indexVar:string = myfilteredData[i + 1]                        
                             await setTextState('')
                             await setTextState(myfilteredData[i + 1])
@@ -41,17 +35,14 @@ export default async function SeeAndSave(api, apilength, textState:string, setTe
     else if (textState.length > 2) {
         for (let i = 0; i < elsefilter.length; i++) {                        
             if (elsefilter[i] === textState) {
-                console.log('textState in the met condition')
-                console.log(textState)
-                    console.log(elsefilter[i])
-                // let indexVar:string = elsefilter[i + 1]                        
-                // await setTextState('')
+                // continue 
                 if (i < 7) {
                     await setTextState(elsefilter[i + 1])
-                } else { setTextState('')}
+                } else { 
+                    setTextState('')                    
+                }
 
             }  
-            //  else if ()  setTextState('')
         }                    
     }
   }
