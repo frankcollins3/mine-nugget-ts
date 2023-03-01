02299bacffdee925b397c1ff219c16fb2d193a04
Frank Collins <fwc3rd@gmail.com>
2023-02-28 19:57:25 -0500

    Add files via upload

---
+++ styles/PickMines.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import $ from 'jquery'
+
+const MineCont = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    min-height: 4.5em;
+    min-width: 5em;
+    box-shadow: 10px 10px 10px rgb(247, 208, 32);    
+    border: 3px solid rgb(247, 208, 36); 
+    border: 3px solid rgb(247, 208, 36); 
+    margin-top: 2.22em;
+    
+    // background-color: papayawhip;
+    // background-image: 'https://media1.popsugar-assets.com/files/thumbor/aSoGZzLtCfypBW6BH6j0sN6HHh0/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2022/06/07/924/n/1922729/tmp_YKfxF6_3f3adf3f3699b17b_PS22_05_Fitness_Hot-GirlWalks_Thumbnail_1456x1456.jpg';
+    // background-size: cover;
+    // background-repeat: no-repeat;
+`
+
+export default MineCont
