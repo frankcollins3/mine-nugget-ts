54c61dfa459bdf0fc7d211356a7f849e7872cb4b
Frank Collins <fwc3rd@gmail.com>
2023-03-06 12:24:22 -0500

    allstrainsES6 [12:24pm]

--- utility/allstrainsES6.js
+++ utility/allstrainsES6.js
@@ -12,10 +12,21 @@ export default async function allstrainsES6func (url, method) {
                 }
                 
                 async GETallstrains() {
-                    let fetchstrains = await Axios.get(url)               
+                    // let fetchstrains = await Axios.get(url)               
+                    let fetchstrains = await $.ajax({
+                        method: 'post',
+                        url: '/api/strains/strain',
+                        data: {
+                        key: 'all'
+                        }
+                    }).then( (msg) => {
+                        console.log('msg we are in the .then() statement')
+                        console.log(msg)      
+                        return msg
+                     })              
                     console.log('fetchstrains')
                     console.log(fetchstrains)
-                    return fetchstrains
+                    return fetchstrains.getdata
                 }
                 // * * * * * * * * * * * * * * * * * * * * * * * * * * * 
                 
@@ -32,14 +43,17 @@ export default async function allstrainsES6func (url, method) {
                         }).then( (msg) => {
                             console.log('msg we are in the .then() statement')
                             console.log(msg)      
+                         }).catch( (err) => {
+                            console.log('err we are in the error!')
+                            console.log(err)
                          })
                 }
 
             }
             if (method === 'getALL') {
-                let allstrains = await new allstrainsES6(url).allstrains;
-                console.log(allstrains)
-                return allstrains
+                let getstrains = await new allstrainsES6(url).allstrains;
+                console.log(getstrains)
+                return getstrains
             }
             if (method === 'postALL') {
                 let allstrains = await new allstrainsES6(url).postallstrains;
