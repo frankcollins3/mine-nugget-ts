7de5984c7f32d5a26674a4cd59f85f541b065916
Frank Collins <fwc3rd@gmail.com>
2023-08-02 17:35:11 -0400

    Delete redux directory

--- redux/reduxselector.ts
+++
@@ -1,8 +0,0 @@
-import {
-    useSelector as customReduxSelector,
-    TypedUseSelectorHook
-} from 'react-redux'
-import store from 'redux/store'
-
-const useSelector: TypedUseSelectorHook<typeof store> = customReduxSelector
-export default customReduxSelector
