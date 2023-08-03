000403e9cf24e237769e040d6b785f196daa81a3
Frank Collins <fwc3rd@gmail.com>
2023-08-02 22:17:12 -0400

    modularize queries [10:17pm]

---
+++ graphql/queries.ts
@@ -0,0 +1,3 @@
+   // axios.post('/api/graphql', { query: `query { allStrainsGET { strain, strainid, dominant, funfact, parents, taste, smell, gold, nugget, thc, cbd } }`})
+   
+export const allStrainsGETquery = `query { allStrainsGET { strain, strainid, dominant, funfact, parents, taste, smell, gold, nugget, thc, cbd } }`
