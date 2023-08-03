19e769dff1a45dffc5bf16db006be2ae16fb92be
Frank Collins <fwc3rd@gmail.com>
2023-08-02 21:36:57 -0400

    wrap app with redux/toolkit Provider [9:36pm]

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,32 @@
 // @ts-nocheck
+import React, { useState, useContext, useEffect, createContext } from 'react'
+import Container from "react-bootstrap/Container"
+import type { AppProps } from 'next/app'
+import Head from "next/head";
+
+// @reduxjs/toolkit global state management
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from 'redux/store/rootReducer';
+
+
+// styles 
 import '../styles/globals.css'
-  import type { AppProps } from 'next/app'
-  import Head from "next/head";
-  import React, { useState, useContext, useEffect, createContext } from 'react'
-  import Container from "react-bootstrap/Container"
 
 
+  const store = configureStore({
+    reducer: rootReducer,
+  });
   
  export default function App({ Component, pageProps, context }: AppProps) {
 
     return (
       <Container id="App">
+
+      <Provider store={store}>
           <Component {...pageProps}/>
+
+      </Provider>
       </Container>    
     )
   
