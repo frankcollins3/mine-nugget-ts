f405b77eeb89883b4926f6cd6258a1a45bdf1458
Frank Collins <fwc3rd@gmail.com>
2023-08-04 16:46:59 -0400

    leave event to restart UI [4:46pm]

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,79 @@
-import styles from 'styles/Strain.module.scss'
-import Container from 'react-bootstrap/Container';
+import axios from 'axios';
 import React, { useEffect, useState, useContext, createContext } from 'react'
 // import $ from 'jquery'
-// import Axios from 'axios';
 // import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+// @reduxjs/toolkit global state management
+import {useSelector, useDispatch} from 'react-redux'
+import { RootState } from 'redux/store/rootReducer';
+import { SET_VIEW_SELECTED_STRAIN, SET_VIEW_SELECTED_STRAIN_INDEX, SET_ALL_STRAINS } from 'redux/main/mainSlice';
+
+// components and styles
+import Container from 'react-bootstrap/Container';
+import SeeandSaveStrain from "components/SeeandSaveStrain"
+import SeeandSaveStrainCont from "components/SeeandSaveStrainCont"
+import SaveMines from "components/SaveMines"
+import StrainFooter from "components/Footer/StrainFooter"
+// import styles from 'styles/Strain.module.scss'
+
+// utils
+import {usePromise} from "Contexts/Promises"
 import {strains} from "utility/strainJSON.json"
+import {strainsINTERFACE} from "utility/InterfaceTypes"
+import { allStrainsGETquery } from 'graphql/queries';
                     
 export default function Main ( props:any, context ) {       
+    const dispatch = useDispatch()
     // props go here and get spit out in RENDERMAIN
+    const VIEW_SELECTED_STRAIN = useSelector( (state:RootState) => state.main.VIEW_SELECTED_STRAIN)
+
+    const readyToSave = true
+    // readyToSave = false
 
     return (
-      <Container className="Main">      
-        <RENDERMAIN></RENDERMAIN> {/*     <RENDERMAIN props={}></RENDERMAIN>      */}
-      </Container>                          
+      <>
+      {/*    if SAVED_STRAIN scroll: none;    */}
+
+      <Container onMouseLeave={() => dispatch(SET_VIEW_SELECTED_STRAIN('')) } className="Main">      
+      {/* <Container style={{ border: readyToSave ? "none" : "", overflowY: readyToSave ? "hidden" : "scroll" }} className="Main">       */}
+
+        <RENDERMAIN></RENDERMAIN> 
+        {/* <RENDERSAVE></RENDERSAVE> */}
+
+      </Container>       
+
+      <Container className="Footer">
+
+        {/* <StrainFooter></StrainFooter> */}
+        { VIEW_SELECTED_STRAIN.strainValues && VIEW_SELECTED_STRAIN.strainValues.strain.length > 1 && <StrainFooter></StrainFooter> }
+
+        </Container>                   
+      </>
     )
   }
     
   function RENDERMAIN () {                // function RENDERMAIN (props) {
-        const test = () => {
-          console.log('strains', strains)
-        }
+        const { setallstrainsPROMISE } = usePromise()
+        const dispatch = useDispatch()
+        const ALL_STRAINS = useSelector( (state:RootState) => state.main.ALL_STRAINS)
+        
+        // const test = () => {          
+        //   console.log('check yourself');
+        //   console.log('all strains', ALL_STRAINS)                    
+        // }
 
         return (
-          <Container>
-          <pre onClick={test}> hey how are you guys </pre>
-          </Container>
+          <>          
+          {/* <SaveMines/> */}
+            <SeeandSaveStrainCont/>
+          </>
         )
   }
 
-  //  useEffect( () => {
-  //     (async() => {
-  //         // const strainfunc = async () => {
-  //   $.ajax({
-  //     method: 'post',
-  //     url: '/api/strains/allStrain',
-  //     data: {
-  //      key: 'all'
-  //     }
-  //   }).then( (msg) => {
-  //     console.log('msg we are in the .then() statement')
-  //     console.log(msg)      
-  //   })
-  //   })()
-  //  }, [])
+  function RENDERSAVE () {
+    return (
+      <>
+      <SaveMines/>
+      </>
+    )
+  }
