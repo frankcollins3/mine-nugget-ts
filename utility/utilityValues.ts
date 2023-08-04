228d43d2487bdd03ef5e148a3d7d332ab996da61
Frank Collins <fwc3rd@gmail.com>
2023-08-04 00:37:13 -0400

    provide :any as backup type [12:37pm]

--- utility/utilityValues.ts
+++ utility/utilityValues.ts
@@ -1,6 +1,6 @@
 import { strainsINTERFACE } from "./InterfaceTypes";
 
-export function findStrainFromAllStrains(strainToFind:any, allStrains:strainsINTERFACE[]) { return allStrains.find(s => s.strain === strainToFind) }
+export function findStrainFromAllStrains(strainToFind:any, allStrains:strainsINTERFACE[]|any) { return allStrains.find(s => s.strain === strainToFind) }
 // export function findStrainFromArray(strainToFind:strainsINTERFACE, allStrains:strainsINTERFACE[]) { // const clickedStrain:strainsINTERFACE = allStrains.find(s => s.strain === strainToFind)
 
     
