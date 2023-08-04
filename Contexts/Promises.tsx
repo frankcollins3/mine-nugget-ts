433cf21563f21f609f65c55aa8dbecc9d7ee665c
Frank Collins <fwc3rd@gmail.com>
2023-08-04 14:34:46 -0400

    modular promise returns needed endpoints for page 1 [2:34pm]

--- Contexts/Promises.tsx
+++ Contexts/Promises.tsx
@@ -11,6 +11,7 @@ import { SET_ALL_STRAINS } from "redux/main/mainSlice"
 
 // utils
 import { strainsINTERFACE } from "utility/InterfaceTypes";
+import { keysAndValuesFromStrain, nonGenericKeysAndValuesFromStrain } from "utility/utilityValues"
 
 // queries
 import { allStrainsGETquery } from "graphql/queries";
@@ -20,11 +21,13 @@ type PromiseTypes = {
 
     iPROMISEcookies: () => any;
     setallstrainsPROMISE: () => any;
+    deleteEndpointsPROMISE: (strain:any) => any;
 }   
 
 const PromiseDefaults = {
     iPROMISEcookies: () => {},
     setallstrainsPROMISE: () => {},
+    deleteEndpointsPROMISE: (strain:any) => {},
 }
 
 const PromiseContext = createContext<PromiseTypes>(PromiseDefaults)
@@ -73,10 +76,23 @@ export function PromiseProvider({children}:Props) {
         })
     }
 
+    function deleteEndpointsPROMISE(strain:any) {   // cant do generic because the endpoints cant be deleted and won't change returned data from generics because different components need it.
+        return new Promise( (resolve:any, reject:any) => {
+            const keysAndValues = nonGenericKeysAndValuesFromStrain(strain)
+            const keys = keysAndValues.strainKeys
+            const values = keysAndValues.strainValues
+            const strainKeys = { strain: keys[0], dominant: keys[2], funfact: keys[3], taste: keys[5], smell: keys[6], gold: keys[7], nugget: keys[8], thc: keys[9], cbd: keys[10] }
+            const strainValues = { strain: values[0], dominant: values[2], funfact: values[3], taste: values[5], smell: values[6], gold: values[7], nugget: values[8], thc: values[9], cbd: values[10] }            
+            resolve( { strainKeys: strainKeys, strainValues: strainValues} )
+            reject("nothing")
+        })        
+    }
+
 
         const value = {
             iPROMISEcookies,
-            setallstrainsPROMISE
+            setallstrainsPROMISE,
+            deleteEndpointsPROMISE
         }        
 
 
