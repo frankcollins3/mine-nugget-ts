aa628419f048485c6b4a3a0f29c5e3979ead5082
Frank Collins <fwc3rd@gmail.com>
2023-08-07 18:44:33 -0400

    password toggles. no magnifying glass [6:44pm]

--- components/SignupInput/SignupInput.tsx
+++ components/SignupInput/SignupInput.tsx
@@ -1,77 +1,66 @@
-
-// @reduxjs/toolkit
-import {useSelector, useDispatch} from 'react-redux'
-import { RootState } from 'redux/store/rootReducer';
-import { SET_SIGNUP_USERNAME_INPUT, SET_SIGNUP_EMAIL_INPUT, SET_SIGNUP_AGE_INPUT, SET_SIGNUP_PASSWORD_INPUT } from 'redux/loginSignup/loginSignupSlice';
-
-// components and styles
-import Container from "react-bootstrap/Container"
-import styles from "./SignupInput.module.scss"
-
-// utils
-import { SignupInterface } from 'utility/InterfaceTypes';
-
-export default function SignupInput(props: SignupInterface) {
-    return <RENDER inputType={props.inputType} />
-}
+        import {useState} from "react"
 
 
-const RENDER = (props:SignupInterface) => {
-    const inputType = props.inputType
-    const dispatch = useDispatch()
+        // @reduxjs/toolkit
+        import {useSelector, useDispatch} from 'react-redux'
+        import { RootState } from 'redux/store/rootReducer';
+        import { SET_SIGNUP_USERNAME_INPUT, SET_SIGNUP_EMAIL_INPUT, SET_SIGNUP_AGE_INPUT, SET_SIGNUP_PASSWORD_INPUT } from 'redux/loginSignup/loginSignupSlice';
 
-    const signupInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-            if (inputType === "username") { dispatch(SET_SIGNUP_USERNAME_INPUT(event.target.value)) }
-            if (inputType === "email") { dispatch(SET_SIGNUP_EMAIL_INPUT(event.target.value)) }
-            if (inputType === "age") { dispatch(SET_SIGNUP_AGE_INPUT(event.target.value)) }
-            if (inputType === "password") { dispatch(SET_SIGNUP_PASSWORD_INPUT(event.target.value)) }
-    }
+        // components and styles
+        import Container from "react-bootstrap/Container"
+        import styles from "./SignupInput.module.scss"
 
-    // const passwordInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //         let value:string = event.target.value
-    //         // dispatch(SET_LOGIN_PASSWORD_INPUT(value))
-    //         dispatch(SET_SIGNUP_PASSWORD_INPUT(value))
-    // };
+        // utils
+        import { SignupInterface } from 'utility/InterfaceTypes';
 
-    // const emailInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //     // dispatch(SET_LOGIN_EMAIL_INPUT(event.target.value))
-    // }
-
-    // const usernameInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-        
-    // }
-
-    // const ageInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-
-    // }
-
-    const reset = () => {
-            if (inputType === "username") { dispatch(SET_SIGNUP_USERNAME_INPUT('')) }
-            if (inputType === "email") { dispatch(SET_SIGNUP_EMAIL_INPUT('')) }
-            if (inputType === "age") { dispatch(SET_SIGNUP_AGE_INPUT('')) }
-            if (inputType === "password") { dispatch(SET_SIGNUP_PASSWORD_INPUT('')) }
-    }
-
-    const SIGNUP_USERNAME_INPUT = useSelector( (state:RootState) => state.loginSignup.SIGNUP_USERNAME_INPUT)
-    const SIGNUP_EMAIL_INPUT = useSelector( (state:RootState) => state.loginSignup.SIGNUP_EMAIL_INPUT)
-    const SIGNUP_AGE_INPUT = useSelector( (state:RootState) => state.loginSignup.SIGNUP_AGE_INPUT)
-    const SIGNUP_PASSWORD_INPUT = useSelector( (state:RootState) => state.loginSignup.SIGNUP_PASSWORD_INPUT)
-
-
-    return (
-        <input 
-        className={styles.input} 
-        type="text"
-        spellCheck="false"
-        value=
-        {
-            inputType === "username" ? SIGNUP_USERNAME_INPUT :
-            inputType === "email" ? SIGNUP_EMAIL_INPUT : 
-            inputType === "age" ? SIGNUP_AGE_INPUT : 
-            inputType === "password" ? SIGNUP_PASSWORD_INPUT : ""
+        export default function SignupInput(props: SignupInterface) {
+            return <RENDER inputType={props.inputType} />
         }
-        onChange={signupInputHandler}
-        onFocus={reset}
-        />        
-    )
-}
+
+        const RENDER = (props:SignupInterface) => {
+
+            const SIGNUP_PASSWORD_SEE = useSelector( (state:RootState) => state.loginSignup.SIGNUP_PASSWORD_SEE)
+            const SIGNUP_USERNAME_INPUT = useSelector( (state:RootState) => state.loginSignup.SIGNUP_USERNAME_INPUT)
+            const SIGNUP_EMAIL_INPUT = useSelector( (state:RootState) => state.loginSignup.SIGNUP_EMAIL_INPUT)
+            const SIGNUP_AGE_INPUT = useSelector( (state:RootState) => state.loginSignup.SIGNUP_AGE_INPUT)
+            const SIGNUP_PASSWORD_INPUT = useSelector( (state:RootState) => state.loginSignup.SIGNUP_PASSWORD_INPUT)
+
+            const [alreadyFocused, setAlreadyFocused] = useState(false)
+
+            const inputType = props.inputType
+            const dispatch = useDispatch()
+
+            const signupInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+                    if (inputType === "username") { dispatch(SET_SIGNUP_USERNAME_INPUT(event.target.value)) }
+                    if (inputType === "email") { dispatch(SET_SIGNUP_EMAIL_INPUT(event.target.value)) }
+                    if (inputType === "age") { dispatch(SET_SIGNUP_AGE_INPUT(event.target.value)) }
+                    if (inputType === "password") { dispatch(SET_SIGNUP_PASSWORD_INPUT(event.target.value)) }
+            }
+
+            const reset = () => {
+                    if (alreadyFocused === false) {
+                        if (inputType === "username") { dispatch(SET_SIGNUP_USERNAME_INPUT('')) }
+                        if (inputType === "email") { dispatch(SET_SIGNUP_EMAIL_INPUT('')) }
+                        if (inputType === "age") { dispatch(SET_SIGNUP_AGE_INPUT('')) }
+                        if (inputType === "password") { dispatch(SET_SIGNUP_PASSWORD_INPUT('')) }
+                    }
+                    setAlreadyFocused(true)
+            }
+        
+            return (
+                <input 
+                className={styles.input} 
+                type={inputType === "password" ? SIGNUP_PASSWORD_SEE === false ? "password" : "text" : "text"}                
+                spellCheck="false"
+                value=
+                {
+                    inputType === "username" ? SIGNUP_USERNAME_INPUT :
+                    inputType === "email" ? SIGNUP_EMAIL_INPUT : 
+                    inputType === "age" ? SIGNUP_AGE_INPUT : 
+                    inputType === "password" ? SIGNUP_PASSWORD_INPUT : ""
+                }
+                onChange={signupInputHandler}
+                onFocus={reset}
+                />        
+            )
+        }
