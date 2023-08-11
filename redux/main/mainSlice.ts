db23c06a81a13f8fed0ed107bd627c862efd2134
Frank Collins <fwc3rd@gmail.com>
2023-08-11 19:59:09 -0400

    CURRENT_USER with new fields [7:59pm]

--- redux/main/mainSlice.ts
+++ redux/main/mainSlice.ts
@@ -1,39 +1,41 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Strain } from 'utility/InterfaceTypes';
+import { Strain, minersINTERFACE } from 'utility/InterfaceTypes';
 
 interface MainSliceState {
-  // main
-  VIEW_SELECTED_STRAIN: Strain<any> ;  
+  // VIEW_SELECTED_STRAIN: Strain<any> ;      generic won't work because the clicked strain needs to have endpoints remove. cant do in effect because it redeclares object with all endpoints
+  VIEW_SELECTED_STRAIN: any ;   
   VIEW_SELECTED_STRAIN_INDEX: number;
   VIEW_SELECTED_STRAIN_KEY: string,
   VIEW_SELECTED_STRAIN_VALUE: string | number,
+  SELECTED_STRAIN_SAVE_OR_NOT: boolean;
   ALL_STRAINS: any[];
+  CURRENT_PAGE: string,
+  MINE_CANVAS_TOUCH: boolean,
+
+  ALL_USERS: any[];
+  ALL_USERNAMES: any[];
+  ALL_EMAILS: any[];
+  CURRENT_USER: {id: number, username: string, email: string, password: string, age: number, icon: string, wins: number}
+  // CURRENT_USER: minersINTERFACE  no ID moving on since I already used {Strain} with generics and Typescript OMIT
 }
 
 const initialState: MainSliceState = {
-  // main
-  VIEW_SELECTED_STRAIN: { strain: '', strainid: 0, dominant: '', funfact: '', parents: '', taste: '', smell: '', gold: '', nugget: '', thc: '', cbd: '', mines: [], digs: [], miners: [] },
+  VIEW_SELECTED_STRAIN: {},
   VIEW_SELECTED_STRAIN_INDEX: 0,
   VIEW_SELECTED_STRAIN_KEY: '',
   VIEW_SELECTED_STRAIN_VALUE: '',
+  SELECTED_STRAIN_SAVE_OR_NOT: false,
   ALL_STRAINS: [],
+  CURRENT_PAGE: '',
+  MINE_CANVAS_TOUCH: false,
+
+  ALL_USERS: [],
+  ALL_USERNAMES: [],
+  ALL_EMAILS: [],
+  CURRENT_USER: {id: 0, username: '', email: '', password: '', age: 0, icon: '', wins: 0}
 };
 
-// strain: string,
-// strainid: number,
-// dominant: string,
-// funfact: string,
-// parents: string,
-// taste: string,
-// smell: string,
-// gold: string,
-// nugget: string,
-// thc: string,
-// cbd: string,
-// mines: any[] | null | undefined, // mines: minesINTERFACE[] | null | undefined,
-// digs: any[] | null | undefined, // digs: digsINTERFACE[] | null | undefined,
-// miners: any[] | null | undefined // miners: any[] | null | undefined       
-
+                                        
 
 const mainSlice = createSlice({
   name: 'main',
@@ -44,15 +46,25 @@ const mainSlice = createSlice({
     SET_VIEW_SELECTED_STRAIN_KEY: (state, action) => { state.VIEW_SELECTED_STRAIN_KEY = action.payload; },
     SET_VIEW_SELECTED_STRAIN_VALUE: (state, action) => { state.VIEW_SELECTED_STRAIN_VALUE = action.payload; },
     VIEW_SELECTED_STRAIN_KEY: (state, action) => { state.VIEW_SELECTED_STRAIN_KEY = action.payload; },
+    TOGGLE_SELECTED_STRAIN_SAVE_OR_NOT: (state) => { state.SELECTED_STRAIN_SAVE_OR_NOT = !state.SELECTED_STRAIN_SAVE_OR_NOT },
     SET_ALL_STRAINS: (state, action) => { state.ALL_STRAINS = action.payload; },
-    // SET_CURRENT_PAGE: (state, action) => { state.CURRENT_PAGE = action.payload; },    
-
+    SET_CURRENT_PAGE: (state, action) => { state.CURRENT_PAGE = action.payload },
+    TOGGLE_MINE_CANVAS_TOUCH: (state) => { state.MINE_CANVAS_TOUCH = !state.MINE_CANVAS_TOUCH },
+    
+    SET_ALL_USERS: (state, action) => { state.ALL_USERS = action.payload; },
+    SET_ALL_USERNAMES: (state, action) => { state.ALL_USERNAMES = action.payload; },
+    SET_ALL_EMAILS: (state, action) => { state.ALL_EMAILS = action.payload; },
+    SET_CURRENT_USER: (state, action) => { state.CURRENT_USER = action.payload }
+    
   },
 });
 
 export const 
 { 
-    SET_VIEW_SELECTED_STRAIN, SET_VIEW_SELECTED_STRAIN_INDEX, SET_VIEW_SELECTED_STRAIN_KEY, SET_VIEW_SELECTED_STRAIN_VALUE, SET_ALL_STRAINS
+  SET_VIEW_SELECTED_STRAIN, SET_VIEW_SELECTED_STRAIN_INDEX, SET_VIEW_SELECTED_STRAIN_KEY, SET_VIEW_SELECTED_STRAIN_VALUE, SET_ALL_STRAINS, TOGGLE_SELECTED_STRAIN_SAVE_OR_NOT,
+  SET_CURRENT_PAGE, TOGGLE_MINE_CANVAS_TOUCH, 
+
+  SET_ALL_USERS, SET_ALL_USERNAMES, SET_ALL_EMAILS, SET_CURRENT_USER,
 } = mainSlice.actions;
 
 export default mainSlice.reducer;
