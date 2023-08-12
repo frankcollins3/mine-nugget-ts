f015f48488e2612325cd2d906c559719d739a9e3
Frank Collins <fwc3rd@gmail.com>
2023-08-11 20:40:28 -0400

    findMine reducer [8:40pm]

--- redux/store/rootReducer.ts
+++ redux/store/rootReducer.ts
@@ -1,14 +1,15 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import mainReducer, { MainState } from 'redux/main/mainSlice';
-// import logInOutGoogleReducer, { logInOutGoogleState } from 'redux/logInOutGoogle/logInOutGoogleSlice';
-// import iconsReducer, { iconState } from 'redux/icons/iconsSlice';
-// import dashboardReducer, { dashboardState } from 'redux/dashboard/dashboardSlice';
+import loginSignupReducer, { LoginSignupState } from 'redux/loginSignup/loginSignupSlice';
+import familyTreeReducer, { familyTreeState } from 'redux/familyTree/familyTreeSlice';
+import findMineReducer, { findMineState } from 'redux/findMine/findMineSlice';
 
 const rootReducer = combineReducers({
   main: mainReducer,
-//   logInOutGoogle: logInOutGoogleReducer,
-//   icons: iconsReducer,
-//   dashboard: dashboardReducer
+  loginSignup: loginSignupReducer,
+  familyTree: familyTreeReducer,
+  findMine: findMineReducer
+
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
