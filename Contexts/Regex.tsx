ce5732854dbe1b53fd1bc671fbc21622b3ddd4f8
Frank Collins <fwc3rd@gmail.com>
2023-08-23 05:58:55 -0400

    fix issue #235 w/ regex instead of substring [5:58am]

--- Contexts/Regex.tsx
+++ Contexts/Regex.tsx
@@ -11,6 +11,10 @@ type RegexContextType = {
     RhasCaps: RegExp;
     RhasNums: RegExp;
     RhasSpecialChar: RegExp;
+    
+    MemailExtension: RegExp;
+    MletterAfterSlash: RegExp;
+    MstringAfterHost3000: RegExp;
   }
   
   type RegexDefaults = {
@@ -21,16 +25,26 @@ type RegexContextType = {
     RhasCaps: RegExp;
     RhasNums: RegExp
     RhasSpecialChar: RegExp;
+    MemailExtension: RegExp;
+    MletterAfterSlash: RegExp;
+    MstringAfterHost3000: RegExp;
   }
   
   const regexDefaults: RegexDefaults = {
     RstringAfterPeriod: /^.*\.(.*)$/,
     RreturnLettersAthruZ: /[a\-z]/g,
     RreturnAlphaChar: /[A-Za-z]+/g,
+
+    // RreturnAlphaChar: /[A-Za-z]+/g,          /[^0-9]/g
     RreturnNumbers: /[0\-9]/g,
-    RhasCaps: /[A-Z]/g,
-    RhasNums: /[0-9]/g,
+    RhasCaps: /[A-Z\s]/g,
+    
+    RhasNums: /\d+/g,
     RhasSpecialChar: /[!@#$%^&*()?<>,.=+-]/g,
+
+    MemailExtension: /(\.\w+)$/,
+    MletterAfterSlash: /\/(.)/,
+    MstringAfterHost3000: /\/(\w+\/\w+\.\w+)/    
   };
 
       const RegexContext = createContext<RegexContextType>(regexDefaults)
@@ -48,10 +62,13 @@ type RegexContextType = {
         const [RreturnLettersAthruZ, setReturnLetterseAthruZ] = useState<RegExp>(/[a\-z]/g)          // replace
         const [RreturnAlphaChar, setRReturnAlphaChar] = useState<RegExp>(/[A-Za-z]/g)                       // replace
         const [RreturnNumbers, setReturnNumbers] = useState<RegExp>(/[0\-9]/g)                       // replace
-        const [RhasCaps, setHasCaps] = useState<RegExp>(/[A-Z]/g)                                    // replace
-        const [RhasNums, setHasNums] = useState<RegExp>(/[0-9]/g)                                    // replace
+        const [RhasCaps, setHasCaps] = useState<RegExp>(/[A-Z\s]/g)                                    // replace
+        const [RhasNums, setHasNums] = useState<RegExp>(/\d+/g)                                    // replace
         const [RhasSpecialChar, setHasSpecialChar] = useState<RegExp>(/[!@#$%^&*()?<>,.=+-]/g)                                    // replace
 
+        const [MemailExtension, setMemailExtension] = useState<RegExp>(/(\.\w+)$/)                                    
+        const [MletterAfterSlash, setMLetterAfterSlash] = useState<RegExp>(/\/(.)/)                                    
+        const [MstringAfterHost3000, setMStringfterHost3000] = useState<RegExp>(/\/(\w+\/\w+\.\w+)/)
                             
         const value = {
             RstringAfterPeriod,
@@ -60,7 +77,11 @@ type RegexContextType = {
             RreturnAlphaChar,
             RhasCaps,
             RhasNums,
-            RhasSpecialChar
+            RhasSpecialChar,
+
+            MemailExtension,
+            MletterAfterSlash,
+            MstringAfterHost3000
         };
 
         return (
