09cd27e547ee37fcd531e3d4945d195844debc87
Frank Collins <fwc3rd@gmail.com>
2023-08-25 17:59:41 -0400

    fix issue #248 if condition to restrict effect trigger [5:59pm]

--- components/TrophyRoomComponents/Photos/Photos.tsx
+++ components/TrophyRoomComponents/Photos/Photos.tsx
@@ -10,10 +10,11 @@ import styles from "./Photos.module.scss"
 import {RootState} from "redux/store/rootReducer"
 import {useSelector, useDispatch} from "react-redux"
 import { SET_CURRENT_USER, SET_CURRENT_PAGE } from "redux/main/mainSlice"
-import { NESTED_PHOTO_DECREMENT, NESTED_PHOTO_INCREMENT, NESTED_PHOTO_RESET, NESTED_PHOTO_LENGTH_END, PHOTO_ARRAY_INDEX_PUSH } from "redux/trophyRoom/trophyRoomSlice"
+import { NESTED_PHOTO_DECREMENT, NESTED_PHOTO_INCREMENT, NESTED_PHOTO_RESET, NESTED_PHOTO_LENGTH_END, PHOTO_ARRAY_INDEX_PUSH, SET_OUTER_PHOTO_INDEX } from "redux/trophyRoom/trophyRoomSlice"
 
 // utils
 import {useImage} from "Contexts/Img"
+import {usePromise} from "Contexts/Promises"
 
 export default function Photos () {
     return <RENDER/>
@@ -27,57 +28,35 @@ function RENDER () {
     const NESTED_PHOTO_INDEX = useSelector( (state:RootState) => state.trophyRoom.NESTED_PHOTO_INDEX)
     const PHOTO_ARRAY_INDEX_VISITED = useSelector( (state:RootState) => state.trophyRoom.PHOTO_ARRAY_INDEX_VISITED)
 
+    const { clickRightPhotoArrayIncrement, clickLeftPhotoArrayDecrement } = usePromise()
+
     const left = "<"
     const right = ">"
 
-    const clickLeftDecrement = () => {
-        const length: number = PHOTOS_ARRAY[OUTER_PHOTO_INDEX].length - 1
-        if (NESTED_PHOTO_INDEX > 0) {
-            dispatch(NESTED_PHOTO_DECREMENT())
-        } else {
-            dispatch(NESTED_PHOTO_LENGTH_END(length))
+    useEffect( () => {
+        if (OUTER_PHOTO_INDEX !== 3) {
+            if (PHOTO_ARRAY_INDEX_VISITED[0].length === PHOTOS_ARRAY[0].length && PHOTO_ARRAY_INDEX_VISITED[1].length === PHOTOS_ARRAY[1].length && PHOTO_ARRAY_INDEX_VISITED[2].length === PHOTOS_ARRAY[2].length) {
+                dispatch(NESTED_PHOTO_RESET())
+                dispatch(SET_OUTER_PHOTO_INDEX(3))
+            } 
         }
-    }
-    
-    const clickRightIncrement = () => {
-        
-        let length:number = PHOTOS_ARRAY[OUTER_PHOTO_INDEX].length - 1
-        console.log('length of nested array', length)        
-        
-        if (NESTED_PHOTO_INDEX < length) {
-            const incrementPROMISE = new Promise( (resolve:any, reject:any) => {
-                dispatch(NESTED_PHOTO_INCREMENT())
-                resolve(NESTED_PHOTO_INDEX)
-                const index = NESTED_PHOTO_INDEX + 1 
-                dispatch(PHOTO_ARRAY_INDEX_PUSH({outerindex: OUTER_PHOTO_INDEX, innerindex: index}))
-                // dispatch(PHOTO_ARRAY_INDEX_PUSH({OUTER_PHOTO_INDEX, index}))
-            })
-        } else {
-            dispatch(NESTED_PHOTO_RESET())
-        }
-    }
+    }, [PHOTO_ARRAY_INDEX_VISITED])
 
     const test = () => {
         console.log('photoarray', PHOTO_ARRAY_INDEX_VISITED)
+
+        console.log(PHOTO_ARRAY_INDEX_VISITED.length)
+        console.log(PHOTOS_ARRAY.length)
     }
 
     return (
         <Container id={styles.frameCont}>
             <Container id={styles.photoFrame}>
-            <pre onMouseEnter={clickLeftDecrement} style={{ color: 'rgb(247, 208, 32)', fontSize: "30px", cursor: 'none' }}> {left} </pre>        
+            <pre onMouseEnter={() => clickLeftPhotoArrayDecrement()} style={{ color: 'rgb(247, 208, 32)', fontSize: "30px", cursor: 'none' }}> {left} </pre>        
                 <img id={styles.photoImg} src={PHOTOS_ARRAY[OUTER_PHOTO_INDEX][NESTED_PHOTO_INDEX]}/>
-            <pre onMouseEnter={clickRightIncrement} style={{ color: 'rgb(247, 208, 32)', fontSize: "30px", cursor: 'none' }}> {right} </pre>
+            <pre onMouseEnter={() => clickRightPhotoArrayIncrement()} style={{ color: 'rgb(247, 208, 32)', fontSize: "30px", cursor: 'none' }}> {right} </pre>
             </Container>
-            <button style={{ height: '50px', width: '50px', backgroundColor: 'hotpink' }} onClick={test}></button>
+            {/* <button style={{ height: '50px', width: '50px', backgroundColor: 'hotpink' }} onClick={test}></button> */}
         </Container>
     )
 }
-
-/* 
-barrel,  barrels, barrier, cactus, cart, coin, caution, coneHat, vest, desert, dynamite, edit, eraser, firetag, glasses,
-goldBars,gold, goldenTriangle, helmet, litPaper, magnify, magnify2, magnify3, mine, mineCart, mirror, pick, pickaxe, ring, shovel, signUpSigns, trophy, unLitPaper, 
-watch, goldcursor1, goldcursor2, kiss, luckypull,
-
-king, queen, joker, kingspades, queenspades, cards, blackjack, navbardice, wincards, upsidedowncard, deckcards, goldenticket, winoneheart, winthreecards, howmanywinsprofile, kingqueensplit, ilink, welink,
-trophyroom6333icons, moviesPopcorn, redCarpet, cinemaRopes, photos, film, curtain, redCarpetHome, winsCeremony, movieReel, frame
-*/
