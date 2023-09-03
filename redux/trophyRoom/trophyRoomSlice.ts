d13fd12540d345fb24ee3542d1a9aa2cb0ba9bc8
Frank Collins <fwc3rd@gmail.com>
2023-09-03 00:00:27 -0400

    oh wow published wrong state [12:00am]

--- redux/trophyRoom/trophyRoomSlice.ts
+++ redux/trophyRoom/trophyRoomSlice.ts
@@ -1,143 +1,118 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Strain } from 'utility/InterfaceTypes';
-
-interface familyTreeSliceState {
-    PLAYING: boolean
-    // the picked strain object data
-    PLAYING_STRAIN: any
-    // strain.parents split. 1 parent shown
-    PLAYING_PARENT_KING: string;
-    // strain.parents split. 2nd parent shown
-    PLAYING_PARENT_QUEEN: string;
-
-    GAME_PLAYED: string;
-    GAME_TITLE: string;
-    GAME_TEXT: string;
-    GAME_LIVES: any[];   // :number[] doesn't allow for .pop()
-    GAME_OVER: string,
-
-// The "Child" of the KING & QUEEN. child strain can be seen on page 1 but parents can't. Guessing game based on providing parents. guessing child.
-    PLAYING_GUESS_RIGHT: string;
-
-    // these 3 wrong options and the above PLAYING_GUESS_RIGHT correct option create the game.
-    PLAYING_GUESS_WRONG_1: string;
-    PLAYING_GUESS_WRONG_2: string;
-    PLAYING_GUESS_WRONG_3: string
-    WRONG_RIGHT_OPTION_BUCKET: any[]; // cant use string i think this needs a generic to add .pop() but out of time for now just considering for future. string[]
-    CLICKED_CARD: string;
-    // this code triggers the ternary to render UI. was considering just local state
-    TERNARY_RENDER_KING: boolean;
-    TERNARY_RENDER_QUEEN: boolean;
-    TERNARY_RENDER_OPTION_0: boolean;
-    TERNARY_RENDER_OPTION_1: boolean;
-    TERNARY_RENDER_OPTION_2: boolean;
-    TERNARY_RENDER_OPTION_3: boolean;
-
-    DRAGGED_OPTION_0: boolean;
-    DRAGGED_OPTION_1: boolean;
-    DRAGGED_OPTION_2: boolean;
-    DRAGGED_OPTION_3: boolean;
-
-    BONUS_GAME: boolean;
+import { minersINTERFACE } from 'utility/InterfaceTypes';
+// import {useImage} from "Contexts/Img"    // have to hard code the string values 
 
 
-    
+interface TrophyRoomSliceState {
+  // VIEW_SELECTED_STRAIN: Strain<any> ;      generic won't work because the clicked strain needs to have endpoints remove. cant do in effect because it redeclares object with all endpoints
+  VIEW_SELECTED_STRAIN: any;   
+  WALK_INTO_TROPHY_ROOM: boolean;
+  WHICH_IMAGE_ARRAY: string[]
+  WHICH_IMAGE_INDEX: number;
+  CURTAIN_IMAGE_CLICK: string;
+
+  PHOTOS_ARRAY: any[];
+  OUTER_PHOTO_INDEX: number;
+  NESTED_PHOTO_INDEX: number;
+  PHOTO_ARRAY_INDEX_VISITED: any[];
+  STARS: any[];
+  STARS_RANDOM_USER: any;
+  // STARS_SEARCH_TERM: string;
+  // ALL_USERS_SEARCH_TERM_MEET: any[];
+  // STARS_RANDOM_USER: minersINTERFACE;
+  STARS_ARRAY: [];
+  VIDEO_SRC_ARRAY_INDEX: number;
 }
 
-const initialState: familyTreeSliceState = {
-    PLAYING: false,
-    PLAYING_STRAIN: { strainid: 0, strain: '', dominant: '', funfact: '', gold: '', nugget: '', parents: '', smell: '', taste: '', thc: '', cbd: '' },
-    PLAYING_PARENT_KING: '',
-    PLAYING_PARENT_QUEEN: '',
+const initialState: TrophyRoomSliceState = {
+  VIEW_SELECTED_STRAIN: {},
+  WALK_INTO_TROPHY_ROOM: false,
+  WHICH_IMAGE_ARRAY: ['/img/moviesPopcorn.png', '/img/winsCeremony.png', '/img/photos.png'],
+  // WHICH_IMAGE_ARRAY: ['/img/moviesPopcorn.png', '/img/film.png', '/img/photos.png', '/img/winsCeremony.png'],
+  WHICH_IMAGE_INDEX: 0,
+  CURTAIN_IMAGE_CLICK: '',
 
-    GAME_PLAYED: '',
-    GAME_TITLE: '',
-    GAME_TEXT: '',
-    GAME_LIVES: [1, 1, 2, 3, 4, 7],
-    GAME_OVER: '',
+  PHOTOS_ARRAY: [
+    // strains
+    [
+      '/img/pick.png', '/img/barrel.png', '/img/barrels.png', '/img/barrier.png', '/img/cart.png', '/img/minecart.png', '/img/caution.png', 
+      '/img/cone_hat.png',  '/img/kiss.png', '/img/gold.png',
+    ],
+    // strains
+    
+    // familytree    
+    [
+      '/img/ring.png', '/img/watch.png',
+      '/img/cactus.png', '/img/luckypull.png',
+      '/img/king.png', '/img/queen.png', '/img/joker.png', '/img/kingspades.png', '/img/queenspades.png', '/img/cards.png', '/img/blackjack.png',
+      '/img/wincards.png', '/img/upsidedowncard.png', '/img/deckcards.png', '/img/goldenticket.png', '/img/winthreehearts.png',
+      '/img/kingqueensplit.png', '/img/winoneheart.png',
+    ],
 
-    PLAYING_GUESS_RIGHT: '',
-    PLAYING_GUESS_WRONG_1: '',
-    PLAYING_GUESS_WRONG_2: '',
-    PLAYING_GUESS_WRONG_3: '',
-    WRONG_RIGHT_OPTION_BUCKET: [],
-    CLICKED_CARD: '',
+    // findmine
+    [
+      '/img/gold-bars.png', '/img/dynamite.png', '/img/firetag.png', '/img/edit.png', '/img/eraser.png', '/img/glasses.png', '/img/gold.png', 
+      '/img/magnify.png', '/img/magnify2.png', '/img/search.png', '/img/mine.png', '/img/pickaxe.png', '/img/shovel.png',
+      '/img/vest.png', '/img/signupsigns.png', '/img/helmet.png', '/img/ilink.png', '/img/welink.png', '/img/navbardice.png', '/img/coin.png',
+    ],
+    // familytree
 
-    TERNARY_RENDER_KING: false,
-    TERNARY_RENDER_QUEEN: false,
-    TERNARY_RENDER_OPTION_0: false,
-    TERNARY_RENDER_OPTION_1: false,
-    TERNARY_RENDER_OPTION_2: false,
-    TERNARY_RENDER_OPTION_3: false,
+    // trophyroom
+    [
+     '/img/star.png', '/img/royalflush6333.png', '/img/moviesPopcorn.png', '/img/red-carpet.png', '/img/cinemaRopes.png', '/img/photos.png', '/img/film.png', '/img/curtain.png', '/img/redCarpetHome.png',
+      '/img/winsCeremony.png', '/img/moviereel.png', '/img/frame.png',
+    ]
+    // trophyroom
+  ],
+  OUTER_PHOTO_INDEX: 0,
+  NESTED_PHOTO_INDEX: 0,
+  PHOTO_ARRAY_INDEX_VISITED: [ [0], [0], [0], [] ],
+  STARS: [],
+  STARS_RANDOM_USER: { username: '', password: '', email: '', age: 0, wins: 0, icon: '', strains: [] },  
+  STARS_ARRAY: [],
+  VIDEO_SRC_ARRAY_INDEX: 0,
 
-    DRAGGED_OPTION_0: false,
-    DRAGGED_OPTION_1: false,
-    DRAGGED_OPTION_2: false,
-    DRAGGED_OPTION_3: false,
-
-    BONUS_GAME: false,
-  };
-
-                                    
-const familyTreeSlice = createSlice({
-  name: 'main',
+};
+                                      
+const trophyRoomSlice = createSlice({
+  name: 'trophyRoom',
   initialState,
   reducers: {
-    // SET_SEE_LOGIN_OR_SIGNUP: (state, action) => { state.SEE_LOGIN_OR_SIGNUP = action.payload },
-    TOGGLE_PLAYING: (state) => { state.PLAYING = !state.PLAYING },
-    SET_PLAYING_STRAIN: (state, action) => { state.PLAYING_STRAIN = action.payload },
-
-    SET_PLAYING_PARENT_KING: (state, action) => { state.PLAYING_PARENT_KING = action.payload },
-    SET_PLAYING_PARENT_QUEEN: (state, action) => { state.PLAYING_PARENT_QUEEN = action.payload },
-
-
-    SET_PLAYING_GUESS_RIGHT: (state, action) => { state.PLAYING_GUESS_RIGHT = action.payload },
-    SET_PLAYING_GUESS_WRONG_1: (state, action) => { state.PLAYING_GUESS_WRONG_1 = action.payload },
-    SET_PLAYING_GUESS_WRONG_2: (state, action) => { state.PLAYING_GUESS_WRONG_2 = action.payload },
-    SET_PLAYING_GUESS_WRONG_3: (state, action) => { state.PLAYING_GUESS_WRONG_3 = action.payload },
-    SET_WRONG_RIGHT_OPTION_BUCKET: (state, action) => { state.WRONG_RIGHT_OPTION_BUCKET = action.payload},
-    SET_CLICKED_CARD: (state, action) => { state.CLICKED_CARD = action.payload},
-    WRONG_RIGHT_OPTION_BUCKET_POP: (state) => { state.WRONG_RIGHT_OPTION_BUCKET = state.WRONG_RIGHT_OPTION_BUCKET.pop()},
-
-    TOGGLE_TERNARY_RENDER_KING: (state) => { state.TERNARY_RENDER_KING = !state.TERNARY_RENDER_KING },
-    TOGGLE_TERNARY_RENDER_QUEEN: (state) => { state.TERNARY_RENDER_QUEEN = !state.TERNARY_RENDER_QUEEN },
-    TOGGLE_TERNARY_RENDER_OPTION_0: (state) => { state.TERNARY_RENDER_OPTION_0 = !state.TERNARY_RENDER_OPTION_0 },
-    TOGGLE_TERNARY_RENDER_OPTION_1: (state) => { state.TERNARY_RENDER_OPTION_1 = !state.TERNARY_RENDER_OPTION_1 },
-    TOGGLE_TERNARY_RENDER_OPTION_2: (state) => { state.TERNARY_RENDER_OPTION_2 = !state.TERNARY_RENDER_OPTION_2 },
-    TOGGLE_TERNARY_RENDER_OPTION_3: (state) => { state.TERNARY_RENDER_OPTION_3 = !state.TERNARY_RENDER_OPTION_3 },
-
-    TOGGLE_DRAGGED_OPTION_0: (state) => { state.DRAGGED_OPTION_0 = !state.DRAGGED_OPTION_0 },
-    TOGGLE_DRAGGED_OPTION_1: (state) => { state.DRAGGED_OPTION_1 = !state.DRAGGED_OPTION_1 },
-    TOGGLE_DRAGGED_OPTION_2: (state) => { state.DRAGGED_OPTION_2 = !state.DRAGGED_OPTION_2 },
-    TOGGLE_DRAGGED_OPTION_3: (state) => { state.DRAGGED_OPTION_3 = !state.DRAGGED_OPTION_3 },
+    SET_VIEW_SELECTED_STRAIN: (state, action) => { state.VIEW_SELECTED_STRAIN = action.payload; },
+    TOGGLE_WALK_INTO_TROPHY_ROOM: (state) => { state.WALK_INTO_TROPHY_ROOM = !state.WALK_INTO_TROPHY_ROOM },
+    WHICH_IMAGE_INCREMENT: (state) => { state.WHICH_IMAGE_INDEX = state.WHICH_IMAGE_INDEX + 1 },
+    WHICH_IMAGE_DECREMENT: (state) => { state.WHICH_IMAGE_INDEX = state.WHICH_IMAGE_INDEX - 1 },    
+    SET_CURTAIN_IMAGE_CLICK: (state, action) => { state.CURTAIN_IMAGE_CLICK = action.payload; },
     
-    SET_GAME_PLAYED: (state, action) => { state.GAME_PLAYED = action.payload },
-    SET_GAME_TITLE: (state, action) => { state.GAME_TITLE = action.payload },
-    SET_GAME_TEXT: (state, action) => { state.GAME_TEXT= action.payload },
-    DECREMENT_GAME_LIVES: (state) => { state.GAME_LIVES.pop() && state.GAME_LIVES.pop() },
-    // DECREMENT_GAME_LIVES: (state) => { state.GAME_LIVES = state.GAME_LIVES.pop() && state.GAME_LIVES.pop() },
-    RESET_GAME_LIVES: (state) => { state.GAME_LIVES = [1, 1, 2, 3, 4, 7]},
-    SET_GAME_OVER: (state, action) => { state.GAME_OVER = action.payload },
+    // photo footer
+    SET_OUTER_PHOTO_INDEX: (state, action) => { state.OUTER_PHOTO_INDEX = action.payload },
+    NESTED_PHOTO_INCREMENT: (state) => { state.NESTED_PHOTO_INDEX = state.NESTED_PHOTO_INDEX + 1 },
+    NESTED_PHOTO_DECREMENT: (state) => { state.NESTED_PHOTO_INDEX = state.NESTED_PHOTO_INDEX - 1 },    
+    NESTED_PHOTO_RESET: (state) => { state.NESTED_PHOTO_INDEX = 0 },    
+    NESTED_PHOTO_LENGTH_END: (state, action) => { state.NESTED_PHOTO_INDEX = action.payload },
+    
+    PHOTO_ARRAY_INDEX_PUSH: (state, action) => { state.PHOTO_ARRAY_INDEX_VISITED[action.payload.outerindex].push(action.payload.innerindex)},
+    PHOTO_ARRAY_INDEX_VISITED_RESET: (state) => { state.PHOTO_ARRAY_INDEX_VISITED = [ [0], [0], [0], [] ] }, 
 
-    TOGGLE_BONUS_GAME: (state) => { state.BONUS_GAME = !state.BONUS_GAME }
-        
+    SET_STARS: (state, action) => { state.STARS.push(action.payload) }, // SET_STARS: (state, action) => { state.STARS = action.payload },
+    SET_STARS_RANDOM_USER: (state, action) => { state.STARS_RANDOM_USER = action.payload },
+    SET_STARS_ARRAY: (state, action) => { state.STARS_ARRAY = action.payload },    
+    RESET_STARS_ARRAY: (state) => { state.STARS_ARRAY = [] },
+
+    INCREMENT_VIDEO_SRC_ARRAY: (state) => { state.VIDEO_SRC_ARRAY_INDEX = state.VIDEO_SRC_ARRAY_INDEX + 1 },
+    RESET_VIDEO_SRC_ARRAY: (state) => { state.VIDEO_SRC_ARRAY_INDEX = 0 },
+
   },
 });
 
 export const 
 { 
- TOGGLE_PLAYING, SET_PLAYING_STRAIN, SET_PLAYING_PARENT_KING, SET_PLAYING_PARENT_QUEEN, 
- SET_PLAYING_GUESS_RIGHT, SET_PLAYING_GUESS_WRONG_1, SET_PLAYING_GUESS_WRONG_2, SET_PLAYING_GUESS_WRONG_3,
+  SET_VIEW_SELECTED_STRAIN, TOGGLE_WALK_INTO_TROPHY_ROOM, WHICH_IMAGE_INCREMENT, WHICH_IMAGE_DECREMENT, SET_CURTAIN_IMAGE_CLICK,
+  SET_OUTER_PHOTO_INDEX, NESTED_PHOTO_INCREMENT, NESTED_PHOTO_DECREMENT, NESTED_PHOTO_RESET, NESTED_PHOTO_LENGTH_END, PHOTO_ARRAY_INDEX_PUSH, PHOTO_ARRAY_INDEX_VISITED_RESET,
 
- SET_WRONG_RIGHT_OPTION_BUCKET, WRONG_RIGHT_OPTION_BUCKET_POP, SET_CLICKED_CARD,
- TOGGLE_TERNARY_RENDER_KING, TOGGLE_TERNARY_RENDER_QUEEN, TOGGLE_TERNARY_RENDER_OPTION_0, TOGGLE_TERNARY_RENDER_OPTION_1, TOGGLE_TERNARY_RENDER_OPTION_2, TOGGLE_TERNARY_RENDER_OPTION_3, 
- TOGGLE_DRAGGED_OPTION_0, TOGGLE_DRAGGED_OPTION_1, TOGGLE_DRAGGED_OPTION_2, TOGGLE_DRAGGED_OPTION_3,
- SET_GAME_PLAYED, SET_GAME_TITLE, SET_GAME_TEXT,
- DECREMENT_GAME_LIVES, RESET_GAME_LIVES, SET_GAME_OVER,
+  SET_STARS_RANDOM_USER, SET_STARS, SET_STARS_ARRAY, RESET_STARS_ARRAY,
+  INCREMENT_VIDEO_SRC_ARRAY, RESET_VIDEO_SRC_ARRAY
+} = trophyRoomSlice.actions;
 
- TOGGLE_BONUS_GAME
-  
-} = familyTreeSlice.actions;
-
-export default familyTreeSlice.reducer;
-export type familyTreeState = familyTreeSliceState;
+export default trophyRoomSlice.reducer;
+export type trophyRoomState = TrophyRoomSliceState;
