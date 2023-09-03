a337e7c9ed7bed8d478d19946eebc0dc54abdbde
Frank Collins <fwc3rd@gmail.com>
2023-09-03 10:25:09 -0400

    {onClick} props for mobile friendly UX [10:24am]

--- components/FindMineComponents/NoFeedContainer/UserIcons/UserIcons.tsx
+++ components/FindMineComponents/NoFeedContainer/UserIcons/UserIcons.tsx
@@ -1,4 +1,5 @@
 import {useState, useEffect} from "react"
+import axios from 'axios'
 
 // components and styles
 import Container from "react-bootstrap/Container"
@@ -10,8 +11,11 @@ import {useSelector, useDispatch} from "react-redux"
 
 // utils
 import {useImage} from "Contexts/Img"
+import {usePromise} from "Contexts/Promises"
 import {useRegex} from "Contexts/Regex"
 import {nothing} from "utility/utilityValues"
+
+import { getUpdateUserIconStringFunc } from "graphql/queries"
 
 export default function UserIcons() {
     return <RENDER/>
@@ -19,12 +23,16 @@ export default function UserIcons() {
 
 function RENDER () {
 
+    const CURRENT_USER = useSelector( (state:RootState) => state.main.CURRENT_USER)
+
+    const {updateUserIconPROMISE} = usePromise()
+
     const [profileHover, setProfileHover] = useState(false)
     let [index, setIndex] = useState(8)
     const [dontChangeIndex, setDontChangeIndex] = useState(false)
 
     const [localSearchTerm, setLocalSearchTerm] = useState('')
-    const { MletterAfterSlash } = useRegex()
+    const { MletterAfterSlash, MstringAfterHost3000 } = useRegex()
     const { mine, barrel, firetag, winoneheart, king, queen, joker, kiss, shovel, ring, watch, barrier, cactus, signUpSigns, glasses, cart, coin, dynamite, } = useImage()
     const heart = "img/winoneheart.png"
     const optionArray:string[] = [ barrel, barrier, cactus, cart, coin, dynamite, firetag, glasses, joker, king, kiss, queen, ring, shovel, signUpSigns, watch, heart, ]
@@ -34,9 +42,7 @@ function RENDER () {
     useEffect( () => {
         optionArray.forEach( (item:any, index:number) => {      // has to type as :any for the regex but there's probably a way to throw a generic or some kind of type method-permittable type?
             let firstChar:any = item.match(MletterAfterSlash)
-            firstChar = firstChar[1]         
-            console.log('firstChar', firstChar)
-            console.log('index', index)
+            firstChar = firstChar[1]                     
             if (localSearchTerm === 'h') {
                 setIndex(16)
                 setDontChangeIndex(true)
@@ -44,9 +50,17 @@ function RENDER () {
                 setIndex(15)
                 setDontChangeIndex(true)
             }
-            if (localSearchTerm === firstChar) {
+            else if (localSearchTerm === 't') {
+                setIndex(14)
+                setDontChangeIndex(true)
+            }
+            else if (localSearchTerm === 's') {
+                setDontChangeIndex(true)
+                setIndex(13)
+            }
+            else if (localSearchTerm === firstChar) {
                 console.log(`we have a match: searchTerm: ${localSearchTerm} firstChar: ${firstChar}`)
-                if (localSearchTerm !== 'w') {
+                if (localSearchTerm !== 'w' && localSearchTerm !== 't' && localSearchTerm !== 'h' && localSearchTerm !== 's') {
                     setIndex(index)
                     setDontChangeIndex(true)
                 }
@@ -68,11 +82,6 @@ function RENDER () {
         }
     }
 
-    const test = () => {
-        console.log('index', index)
-        console.log('searchTerm', localSearchTerm)
-    }
-
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {        
         let value:string = event.target.value
         setLocalSearchTerm(value)
@@ -80,9 +89,38 @@ function RENDER () {
 
     const imageHover = () => setDontChangeIndex(false)
 
+    const updateUserIcon = (event:any) => {
+        console.log("hey were updating");
+        const src:string = event.target.src
+        console.log('src', src)
+        console.log('CURRENT_USER', CURRENT_USER)
+
+        // const regex:RegExp = /\/(\w+\/\w+\.\w+)/
+        const imgSrc:any|null = src.match(MstringAfterHost3000)
+        if (imgSrc !== 'null') {
+            let str:string = imgSrc[1]
+            console.log('str', str)
+
+            updateUserIconPROMISE(str)
+            .then( (updatedUser) => {
+                
+                console.log('updatedUser', updatedUser)
+            })
+
+            // const query = getUpdateUserIconStringFunc(CURRENT_USER.username, str)
+            // axios.post("/api/graphql", { query: `${query}` })
+            // .then( (userIconUpdate:any) => {
+            //     console.log('userIconUpdate', userIconUpdate)
+            // })
+
+        } else {
+        }        
+
+    }
+
 
     return (
-        <Container onClick={test} onMouseEnter={() => setProfileHover(true)} onMouseLeave={() => setProfileHover(false)} id={styles.cont}>
+        <Container onMouseEnter={() => setProfileHover(true)} onMouseLeave={() => setProfileHover(false)} id={styles.cont}>
             
         {
             profileHover === true 
@@ -91,9 +129,9 @@ function RENDER () {
 
             <input maxLength={1} id={styles.input} onChange={handleInputChange} type="text"/>
 
-            <pre onMouseEnter={dontChangeIndex ? nothing : decrementState} className={styles.ghost}> {Left} </pre>
-             <img onMouseEnter={dontChangeIndex ? imageHover : nothing} id={styles.img} src={optionArray[index]}/>
-             <pre onMouseEnter={dontChangeIndex ? nothing : incrementState} className={styles.ghost}> {Right} </pre>
+            <pre onClick={dontChangeIndex ? nothing : decrementState} className={styles.ghost}> {Left} </pre>
+             <img onClick={updateUserIcon} onMouseEnter={dontChangeIndex ? imageHover : nothing} id={styles.img} src={optionArray[index]}/>
+             <pre onClick={dontChangeIndex ? nothing : incrementState} className={styles.ghost}> {Right} </pre>
             </Container>
             : 
             <pre id={styles.text}> Profile </pre>
