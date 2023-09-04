d61eed914708b47b889a33ec29fbb3ecff4b1fa1
Frank Collins <fwc3rd@gmail.com>
2023-09-03 23:00:15 -0400

    twice bitten. {400px} not {300px} [11:00pm]

--- components/TrophyRoomComponents/Photos/Photos.tsx
+++ components/TrophyRoomComponents/Photos/Photos.tsx
@@ -1,62 +1,62 @@
-import {useEffect, useState} from "react"
-import axios from 'axios'
-
-// components and styles
-import Container from "react-bootstrap/Container"
-import styles from "./Photos.module.scss"
-
-
-// @reduxjs/toolkit
-import {RootState} from "redux/store/rootReducer"
-import {useSelector, useDispatch} from "react-redux"
-import { SET_CURRENT_USER, SET_CURRENT_PAGE } from "redux/main/mainSlice"
-import { NESTED_PHOTO_DECREMENT, NESTED_PHOTO_INCREMENT, NESTED_PHOTO_RESET, NESTED_PHOTO_LENGTH_END, PHOTO_ARRAY_INDEX_PUSH, SET_OUTER_PHOTO_INDEX } from "redux/trophyRoom/trophyRoomSlice"
-
-// utils
-import {useImage} from "Contexts/Img"
-import {usePromise} from "Contexts/Promises"
-
-export default function Photos () {
-    return <RENDER/>
+#frameCont {
+    min-height: 50vh !important;
+    min-width: 50vw !important;
+    display: grid;
+    grid-area: main; 
+    justify-content: center;
+    align-items: center;
+    // background: url('/img/frame.png') no-repeat center/cover;
+    // background-size: cover;
 }
 
-function RENDER () {
-    const dispatch = useDispatch()
+#photoFrame {
+    height: 600px;
+    width: 600px;
+    background: url('/img/frame.png') no-repeat center/cover;
+    background-size: cover;
 
-    const PHOTOS_ARRAY = useSelector( (state:RootState) => state.trophyRoom.PHOTOS_ARRAY)
-    const OUTER_PHOTO_INDEX = useSelector( (state:RootState) => state.trophyRoom.OUTER_PHOTO_INDEX)
-    const NESTED_PHOTO_INDEX = useSelector( (state:RootState) => state.trophyRoom.NESTED_PHOTO_INDEX)
-    const PHOTO_ARRAY_INDEX_VISITED = useSelector( (state:RootState) => state.trophyRoom.PHOTO_ARRAY_INDEX_VISITED)
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row; 
+}
 
-    const { clickRightPhotoArrayIncrement, clickLeftPhotoArrayDecrement } = usePromise()
+#photoImg {
+    height: 200px;
+    width: 200px;
+}
 
-    const left = "<"
-    const right = ">"
-
-    useEffect( () => {
-        if (OUTER_PHOTO_INDEX !== 3) {
-            if (PHOTO_ARRAY_INDEX_VISITED[0].length === PHOTOS_ARRAY[0].length && PHOTO_ARRAY_INDEX_VISITED[1].length === PHOTOS_ARRAY[1].length && PHOTO_ARRAY_INDEX_VISITED[2].length === PHOTOS_ARRAY[2].length) {
-                dispatch(NESTED_PHOTO_RESET())
-                dispatch(SET_OUTER_PHOTO_INDEX(3))
-            } 
-        }
-    }, [PHOTO_ARRAY_INDEX_VISITED])
-
-    const test = () => {
-        console.log('photoarray', PHOTO_ARRAY_INDEX_VISITED)
-
-        console.log(PHOTO_ARRAY_INDEX_VISITED.length)
-        console.log(PHOTOS_ARRAY.length)
+@media only screen and (max-width: 600px) {
+    #photoFrame {
+        height: 400px;
+        width: 400px;
     }
 
-    return (
-        <Container id={styles.frameCont}>
-            <Container id={styles.photoFrame}>
-            <pre onClick={() => clickLeftPhotoArrayDecrement()} style={{ color: 'papayawhip', fontSize: "40px", cursor: 'none', fontWeight: 'bolder' }}> {left} </pre>        
-                <img id={styles.photoImg} src={PHOTOS_ARRAY[OUTER_PHOTO_INDEX][NESTED_PHOTO_INDEX]}/>
-            <pre onClick={() => clickRightPhotoArrayIncrement()} style={{ color: 'papayawhip', fontSize: "40px", cursor: 'none', fontWeight: 'bolder' }}> {right} </pre>
-            </Container>
-            {/* <button style={{ height: '50px', width: '50px', backgroundColor: 'hotpink' }} onClick={test}></button> */}
-        </Container>
-    )
+    // #photoImg { 
+    //     height: 
+    // }
+
+    #img { height: 100px; width: 100px; }
+}
+
+@media only screen and (max-width: 400px) {
+    #photoFrame {
+        height: 300px;
+        width: 300px;
+    }
+    #img { height: 75px; width: 75px; }
+}
+
+@media only screen and (max-width: 400px) {
+    #photoFrame {
+        height: 200px;
+        width: 200px;
+    }
+
+    #photoImg {
+        height: 125px;
+        width: 125px;
+    }
+
+    #img { height: 50px; width: 50px; }
 }
