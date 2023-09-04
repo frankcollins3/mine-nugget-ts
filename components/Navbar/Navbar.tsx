32287763bd2a1553cde7e14edb8793e62899e1c3
Frank Collins <fwc3rd@gmail.com>
2023-09-03 22:37:05 -0400

    now updates if user wins guess game to unlock bonus features [10:36pm]

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import $ from 'jquery'
 
 // @reduxjs/toolkit
@@ -8,7 +8,8 @@ import {useSelector, useDispatch} from "react-redux"
 //     SET_VIEW_SELECTED_STRAIN_KEY, SET_VIEW_SELECTED_STRAIN_VALUE, SET_VIEW_SELECTED_STRAIN_INDEX, SET_VIEW_SELECTED_STRAIN, TOGGLE_SELECTED_STRAIN_SAVE_OR_NOT,
 //     SET_ALL_USERS, SET_ALL_USERNAMES, SET_ALL_EMAILS,
 // } from "redux/main/mainSlice"
-import { TOGGLE_SHOW_ICONS } from 'redux/findMine/findMineSlice'
+import { SET_CURRENT_USER } from 'redux/main/mainSlice'
+import { TOGGLE_SHOW_ICONS, TOGGLE_USER_ICON_SAVE_ERROR } from 'redux/findMine/findMineSlice'
 import { SET_CURTAIN_IMAGE_CLICK, SET_OUTER_PHOTO_INDEX, NESTED_PHOTO_RESET, PHOTO_ARRAY_INDEX_VISITED_RESET  } from 'redux/trophyRoom/trophyRoomSlice'
 
 
@@ -21,12 +22,15 @@ import styles from "./Navbar.module.scss"
 // utils
 import {useImage} from "Contexts/Img"
 import {usePromise} from "Contexts/Promises"
+import { clearCookie } from 'utility/utilityValues'
 
 export default function Navbar() {
     return <RENDER></RENDER>
 }
 
 function RENDER() {
+
+    const [showLogout, setShowLogout] = useState(false)
     const dispatch = useDispatch()
 
     const CURRENT_USER = useSelector( (state:RootState) => state.main.CURRENT_USER)
@@ -36,13 +40,18 @@ function RENDER() {
     const CURRENT_PAGE = useSelector( (state:RootState) => state.main.CURRENT_PAGE)
     const CURRENT_USER_STRAINS = useSelector( (state:RootState) => state.findMine.CURRENT_USER_STRAINS)
     const SHOW_ICONS = useSelector( (state:RootState) => state.findMine.SHOW_ICONS)
+    const USER_ICON_SAVE_ERROR = useSelector( (state:RootState) => state.findMine.USER_ICON_SAVE_ERROR)
+
+    const GAME_OVER = useSelector( (state:RootState) => state.familyTree.GAME_OVER)
 
     const WALK_INTO_TROPHY_ROOM = useSelector( (state:RootState) => state.trophyRoom.WALK_INTO_TROPHY_ROOM)
     const CURTAIN_IMAGE_CLICK = useSelector( (state:RootState) => state.trophyRoom.CURTAIN_IMAGE_CLICK)
 
-    const { magnify, gold, cactus, mine, navbardice, vest, signUpSigns, redCarpetHome, curtain, magnify3, spotlight, trophy } = useImage()
+    
 
-    const { curtainResetPROMISE, cookieFunc } = usePromise()
+    const { magnify, gold, cactus, mine, navbardice, vest, signUpSigns, redCarpetHome, curtain, magnify3, spotlight, trophy, logout } = useImage()
+
+    const { curtainResetPROMISE, cookieFunc, logoutPROMISE } = usePromise()
 
     useEffect( () => {
         cookieFunc() 
@@ -50,6 +59,15 @@ function RENDER() {
             console.log('currentuser', currentuser)
         })
     }, [])
+
+    useEffect( () => {
+        if (GAME_OVER === 'win') {
+            cookieFunc()
+            .then( (currentuser) => {
+                dispatch(SET_CURRENT_USER(currentuser))
+            })
+        }
+    }, [GAME_OVER])
 
     
     useEffect( () => {    
@@ -80,7 +98,7 @@ function RENDER() {
         if (CURRENT_PAGE === '/findmine' && CURRENT_USER.age > 0) {
             if (CURRENT_USER.wins && CURRENT_USER.wins > 0) {
                 dispatch(TOGGLE_SHOW_ICONS())
-                $(event.target).removeClass('hover')
+                $(event.target).removeClass('hover')                
             }            
         }
     }
@@ -107,7 +125,16 @@ function RENDER() {
         }
     }
 
-    
+    const logoutFunc = (event:any) => {
+        let src:string = event.target.src
+        if (src.includes('logout')) {
+            console.log("hey the logout");
+            logoutPROMISE()
+            setTimeout( () => window.location.href = "/")            
+        } else {
+            console.log("you are mine");
+        }
+    }
 
     const toggleBackToCurtain = () => { curtainResetPROMISE() }
 
@@ -117,7 +144,11 @@ function RENDER() {
 
             <Container>
             { CURRENT_PAGE === "/trophyroom" && CURTAIN_IMAGE_CLICK === "popcorn" && <img className={styles.img} src={spotlight}/> }
-            { CURRENT_PAGE !== "/familytree" && CURRENT_PAGE !== "/findmine" && CURRENT_PAGE !== "/trophyroom" && <img style={{ cursor: 'pointer' }} className={styles.img} id={styles.mine} src={mine}/> }
+            
+            <Container onMouseLeave={() => setShowLogout(false)}>
+            {CURRENT_PAGE !== "/familytree" && CURRENT_PAGE !== "/findmine" && CURRENT_PAGE !== "/trophyroom" && <img onClick={logoutFunc} onMouseEnter={() => setShowLogout(true)} style={{ cursor: 'pointer' }} className={styles.img} id={styles.mine} src={showLogout ? logout : mine}/>}
+            </Container> 
+            
             {/* { CURRENT_PAGE === "/" || CURRENT_PAGE === "/strain" && <img style={{ cursor: 'pointer' }} className={styles.img} id={styles.mine} src={mine}/> } */}
             { CURRENT_PAGE === "/familytree" && <img onClick={leftIconClick} style={{ cursor: 'pointer' }} className={styles.img} id={styles.mine} src={CURRENT_USER.wins && CURRENT_USER.wins > 2 ? trophy : navbardice}/> }
             { CURRENT_PAGE === "/findmine" && <img id="iconVest" onClick={findMineClickForIcons} style={{ cursor: 'pointer', borderRadius: '0%' }} className={styles.img} src={CURRENT_USER_STRAINS.length > 1 ? vest : signUpSigns}/> }
@@ -147,23 +178,3 @@ function RENDER() {
         </Container>
     )
 }
-
-{/* <Container id={styles.Cont}>
-            {WALK_INTO_TROPHY_ROOM && <img className={styles.img} src={redCarpetHome}/>}
-            <Container id={styles.multiIconCont}>
-            {WALK_INTO_TROPHY_ROOM && <p> hey </p>}
-            </Container>
-            </Container> */}
-
-// interface IconVestElement extends HTMLElement {
-//     onClick: (event: MouseEvent) => void;
-// }      
-//   const iconVest: IconVestElement | null | any = document.getElementById('iconVest');      
-//   if (iconVest) {
-//     iconVest.onClick = (event) => {
-//       console.log('Element clicked:', event);
-//     };
-  
-//     // Attach the event listener
-//     iconVest.addEventListener('click', iconVest.onClick);
-//   }
